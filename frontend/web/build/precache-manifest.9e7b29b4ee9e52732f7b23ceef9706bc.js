e5d36f49f67a6fae19251c6d1e09cd8bc644aedc
Jason Mars <mars.ninja@gmail.com>
2020-10-22 13:30:43 -0400

    Build fixed maybe?

---
+++ frontend/web/build/precache-manifest.9e7b29b4ee9e52732f7b23ceef9706bc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2120e1afb647bccb1f64a062a08e7ef5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5d8932617116096ee77",
+    "url": "/static/css/2.dcaccb7f.chunk.css"
+  },
+  {
+    "revision": "4902f1d60ab33d51dc5c",
+    "url": "/static/css/main.2eb2c03f.chunk.css"
+  },
+  {
+    "revision": "c5d8932617116096ee77",
+    "url": "/static/js/2.33635b1d.chunk.js"
+  },
+  {
+    "revision": "ae69a686e1f284d78b6ec2bbf4577426",
+    "url": "/static/js/2.33635b1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4902f1d60ab33d51dc5c",
+    "url": "/static/js/main.0c94bc45.chunk.js"
+  },
+  {
+    "revision": "5121232dfbf456b4921d",
+    "url": "/static/js/runtime-main.451c0d4d.js"
+  },
+  {
+    "revision": "c7f6f1a6adcb2ffd37cd510f24ae6d0e",
+    "url": "/static/media/logo_notext.c7f6f1a6.png"
+  }
+]);
