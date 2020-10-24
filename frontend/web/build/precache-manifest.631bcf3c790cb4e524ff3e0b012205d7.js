027566fe4aec4e0629d2631823575ebfeb4f0133
Jason <jason@lifelogify.com>
2020-10-23 20:26:52 -0400

    Hotfixing the hotfix

---
+++ frontend/web/build/precache-manifest.631bcf3c790cb4e524ff3e0b012205d7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a8f7458c0baf1c89588d77ce532ad51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13f8eee9c3fcd0b22b67",
+    "url": "/static/css/2.dcaccb7f.chunk.css"
+  },
+  {
+    "revision": "c5735aa301bffae5d00a",
+    "url": "/static/css/main.64c0de8f.chunk.css"
+  },
+  {
+    "revision": "13f8eee9c3fcd0b22b67",
+    "url": "/static/js/2.0693a24e.chunk.js"
+  },
+  {
+    "revision": "ae69a686e1f284d78b6ec2bbf4577426",
+    "url": "/static/js/2.0693a24e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5735aa301bffae5d00a",
+    "url": "/static/js/main.da79920d.chunk.js"
+  },
+  {
+    "revision": "5121232dfbf456b4921d",
+    "url": "/static/js/runtime-main.451c0d4d.js"
+  },
+  {
+    "revision": "c7f6f1a6adcb2ffd37cd510f24ae6d0e",
+    "url": "/static/media/logo_notext.c7f6f1a6.png"
+  }
+]);
