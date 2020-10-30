a18dde476593b754b4c0e9682d10da5339725c28
Jason <jason@lifelogify.com>
2020-10-29 22:26:54 -0400

    Prod push

---
+++ frontend/web/build/precache-manifest.cb4ca31d570266cfd5f8d560891e01c5.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "328736765813ffa556f28bc4ecd77feb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5ac169514d95f554b76",
+    "url": "/static/css/2.dcaccb7f.chunk.css"
+  },
+  {
+    "revision": "653f8bac3bfa36b1ede3",
+    "url": "/static/css/main.e82906f6.chunk.css"
+  },
+  {
+    "revision": "a5ac169514d95f554b76",
+    "url": "/static/js/2.8874a9a5.chunk.js"
+  },
+  {
+    "revision": "a3c24c7ea1511f5bff3fa45a1e05f27f",
+    "url": "/static/js/2.8874a9a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "653f8bac3bfa36b1ede3",
+    "url": "/static/js/main.8ee6efef.chunk.js"
+  },
+  {
+    "revision": "5121232dfbf456b4921d",
+    "url": "/static/js/runtime-main.451c0d4d.js"
+  },
+  {
+    "revision": "a1360668f16ac6d5c088b51e04f823b7",
+    "url": "/static/media/background_tree_blur.a1360668.jpg"
+  },
+  {
+    "revision": "d0b5e409c66a87e1024dfa4e0fbd2abc",
+    "url": "/static/media/day_example.d0b5e409.png"
+  },
+  {
+    "revision": "375ce80dfb7798db3e268a90ad9c7cdf",
+    "url": "/static/media/day_move_workette.375ce80d.png"
+  },
+  {
+    "revision": "e23ad9f58f3c720c0541dfc4b9f382a4",
+    "url": "/static/media/day_rename.e23ad9f5.png"
+  },
+  {
+    "revision": "090765cfa6f079be9f3f75b408d42b74",
+    "url": "/static/media/day_ritual.090765cf.png"
+  },
+  {
+    "revision": "5d30656be9abafd05e40cb02996353cf",
+    "url": "/static/media/gs_completing.5d30656b.png"
+  },
+  {
+    "revision": "925181521f1d07693aa074533899cd1a",
+    "url": "/static/media/gs_create.92518152.png"
+  },
+  {
+    "revision": "d194f317f2a8a19fe373d600e34ee173",
+    "url": "/static/media/gs_leftview.d194f317.png"
+  },
+  {
+    "revision": "4a9a68275d679562bcbbbbe5428e6529",
+    "url": "/static/media/gs_workettes.4a9a6827.png"
+  },
+  {
+    "revision": "71eb08bc8c9b642c78d6ec4df77c79bf",
+    "url": "/static/media/gs_worksets.71eb08bc.png"
+  },
+  {
+    "revision": "c7f6f1a6adcb2ffd37cd510f24ae6d0e",
+    "url": "/static/media/logo_notext.c7f6f1a6.png"
+  },
+  {
+    "revision": "619e5943711f3987350c9dcb12007c9c",
+    "url": "/static/media/overview_example.619e5943.png"
+  },
+  {
+    "revision": "eeb9c1d063a5d23e6a8ef95a8be5d85b",
+    "url": "/static/media/release-notes.eeb9c1d0.md"
+  },
+  {
+    "revision": "d888a57df7fc7a0079b8be5ff4905504",
+    "url": "/static/media/transparent_white_logo.d888a57d.png"
+  }
+]);
--- frontend/web/build/service-worker.js
+++ frontend/web/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.67c8640064ce2f690be72da917295cc8.js"
+  "/precache-manifest.cb4ca31d570266cfd5f8d560891e01c5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
