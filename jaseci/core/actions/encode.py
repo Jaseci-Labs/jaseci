b1da3512142a91912ca26577c533e4517826dd1a
Jason <jason@lifelogify.com>
2021-01-16 10:26:10 -0500

    TF tweak

---
+++ jaseci/core/actions/encode.py
@@ -0,0 +1,4 @@
+"""Built in actions for Jaseci"""
+
+
+from .module.use_actions import *  # noqa
---
+++ jaseci/core/actions/module/use_actions.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+import tensorflow_hub as hub
+import numpy as np
+import tensorflow_text
+
+questions = ["What is your age?"]
+responses = ["I am 20 years old.", "good morning"]
+response_contexts = ["I will be 21 next year.", "great day."]
+
+module = hub.load('https://tfhub.dev/google/universal-sentence-encoder-multilingual-qa/3')
+
+question_embeddings = module.signatures['question_encoder'](
+            tf.constant(questions))
+response_embeddings = module.signatures['response_encoder'](
+        input=tf.constant(responses),
+        context=tf.constant(response_contexts))
+
+print(np.inner(question_embeddings['outputs'], response_embeddings['outputs'])))
