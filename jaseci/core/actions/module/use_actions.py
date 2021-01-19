333230cbd53a03b1bb3dd796ee8b149a1d6210ef
Jason Mars <mars.ninja@gmail.com>
2021-01-19 13:37:36 -0500

    lets see

--- jaseci/core/actions/module/use_actions.py
+++ jaseci/core/actions/module/use_actions.py
@@ -1,4 +1,3 @@
-"""
 import tensorflow as tf
 import tensorflow_hub as hub
 import numpy as np
@@ -8,13 +7,13 @@ questions = ["What is your age?"]
 responses = ["I am 20 years old.", "good morning"]
 response_contexts = ["I will be 21 next year.", "great day."]
 
-module = hub.load('https://tfhub.dev/google/universal-sentence-encoder-multilingual-qa/3')
+module = hub.load(
+    'https://tfhub.dev/google/universal-sentence-encoder-multilingual-qa/3')
 
 question_embeddings = module.signatures['question_encoder'](
-            tf.constant(questions))
+    tf.constant(questions))
 response_embeddings = module.signatures['response_encoder'](
-        input=tf.constant(responses),
-        context=tf.constant(response_contexts))
+    input=tf.constant(responses),
+    context=tf.constant(response_contexts))
 
 print(np.inner(question_embeddings['outputs'], response_embeddings['outputs']))
-"""
