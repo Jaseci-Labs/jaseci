09251919f5e0f4324024ea28b90e5cfd013aac26
Jason Mars <mars.ninja@gmail.com>
2021-01-20 14:59:09 -0500

    Step toward tfjaseci

--- jaseci/base/apps.py
+++ jaseci/base/apps.py
@@ -1,5 +1,36 @@
+import tensorflow_text
+import numpy as np
+import tensorflow_hub as hub
+import tensorflow as tf
 from django.apps import AppConfig
 
 
 class CoreApiConfig(AppConfig):
     name = 'base'
+
+    class USEBase():
+        module = hub.load(
+            'https://tfhub.dev/google/universal-sentence-encoder-multilingual-qa/3')
+
+        def use_question_encode(self, q):
+            if(isinstance(q, list)):
+                return self.module.signatures['question_encoder'](tf.constant(q))
+            elif (isinstance(q, str)):
+                return self.module.signatures['question_encoder'](tf.constant([q]))
+
+        def use_answer_encode(self, a, context=None):
+            if(context is None):
+                context = a
+            if(isinstance(a, list)):
+                return self.module.signatures['response_encoder'](
+                    input=tf.constant(a),
+                    context=tf.constant(context))
+            elif(isinstance(a, str)):
+                return self.module.signatures['response_encoder'](
+                    input=tf.constant([a]),
+                    context=tf.constant([context]))
+
+        def use_qa_dot(self, q, a):
+            print(q, a)
+            # return np.inner(question_embeddings['outputs'], response_embeddings['outputs'])
+    USE = USEBase()
--- jaseci/core/actions/module/use_actions.py
+++ jaseci/core/actions/module/use_actions.py
@@ -1,19 +1,26 @@
-import tensorflow as tf
-import tensorflow_hub as hub
-import numpy as np
-import tensorflow_text
+# import tensorflow as tf
+# import tensorflow_hub as hub
+# import numpy as np
+# import tensorflow_text
 
-questions = ["What is your age?"]
-responses = ["I am 20 years old.", "good morning"]
-response_contexts = ["I will be 21 next year.", "great day."]
+# questions = ["What is your age?"]
+# responses = ["I am 20 years old.", "good morning"]
+# response_contexts = ["I will be 21 next year.", "great day."]
 
-module = hub.load(
-    'https://tfhub.dev/google/universal-sentence-encoder-multilingual-qa/3')
+# module = hub.load(
+#     'https://tfhub.dev/google/universal-sentence-encoder-multilingual-qa/3')
 
-question_embeddings = module.signatures['question_encoder'](
-    tf.constant(questions))
-response_embeddings = module.signatures['response_encoder'](
-    input=tf.constant(responses),
-    context=tf.constant(response_contexts))
+# question_embeddings = module.signatures['question_encoder'](
+#     tf.constant(questions))
+# response_embeddings = module.signatures['response_encoder'](
+#     input=tf.constant(responses),
+#     context=tf.constant(response_contexts))
 
-print(np.inner(question_embeddings['outputs'], response_embeddings['outputs']))
+# print(np.inner(question_embeddings['outputs'], response_embeddings['outputs']))
+
+# from base.apps import USEConfig
+
+# qe = USEConfig.USE.use_question_encode("How old are you?")
+# ae = USEConfig.USE.use_answer_encode("I'm 100 years old.")
+
+# print(USEConfig.USE.use_qa_dot(qe, ae))
