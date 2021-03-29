6d5c4b5d76d43cdc7eb3882a9ba8ca8407b7f382
Jason <jason@lifelogify.com>
2021-03-29 17:10:47 -0400

    Silly url patterns

--- jaseci/user_api/serializers.py
+++ jaseci/user_api/serializers.py
@@ -25,20 +25,21 @@ def send_activation_email(request, email):
 
 
 @receiver(reset_password_token_created)
-def password_reset_token_created(sender, instance, reset_token, *args, **kwargs):
+def password_reset_token_created(sender, instance, reset_password_token, *args, **kwargs):
 
     email_msg = "{}?token={}".format(
-        reverse('password_reset:reset-password-request'), reset_token.key)
+        reverse('user_api:password_reset:reset-password-request'), reset_password_token.key)
 
     send_mail(
         # title:
-        "Password Reset for {title}".format(title="Jaseci"),
+        "Password Reset for {title}".format(title="Jaseci Account"),
         # message:
         email_msg,
         # from:
         "noreply@jaseci.org",
         # to:
-        [reset_token.user.email]
+        [reset_password_token.user.email],
+        fail_silently=False,
     )
 
 
