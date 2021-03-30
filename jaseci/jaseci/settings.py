d16d9f8afa88d085c0ddbe18fba923f0323c261a
Jason <jason@lifelogify.com>
2021-03-30 16:58:18 -0400

    incremental perma-fix for lingjia

--- jaseci/jaseci/settings.py
+++ jaseci/jaseci/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework.authtoken',
+    # 'rest_framework.authtoken',
     'knox',
     'django_rest_passwordreset',
     'drf_yasg',
