b25cfe2740a51784837c648bc2aaa92733375ae0
Jason <jason@lifelogify.com>
2021-04-05 23:38:21 -0400

    Finalized

--- jaseci/core/master.py
+++ jaseci/core/master.py
@@ -24,10 +24,7 @@ class master(element):
         Get or create then return application sentinel and graph pairing
         Code must be encoded in base64
         """
-        # TODO: Do better recompilation here
-        self.sentinel_ids.destroy_obj_by_name(name)
-        snt = None
-        # snt = self.sentinel_ids.get_obj_by_name(name, True)
+        snt = self.sentinel_ids.get_obj_by_name(name, True)
         gph = self.graph_ids.get_obj_by_name(name, True)
         if (not snt):
             self.api_create_sentinel(name)
