490bb217c7d6ad255d3070ac0d656ee26de365e2
Jason Mars <jason@mars.ninja>
2021-08-26 15:11:22 -0400

    Requests passthrough library complete

---
+++ jaseci_core/jaseci/actions/module/request_actions.py
@@ -0,0 +1,127 @@
+import requests
+
+
+def get(param_list, meta):
+    """
+    Issue request
+    Param 1 - url
+    Param 2 - data
+    Param 3 - header
+
+    Return - response object
+    """
+    url = param_list[0]
+    data = param_list[1]
+    header = param_list[2]
+    res = requests.get(url, data=data, headers=header)
+    ret = {'status_code': res.status_code}
+    try:
+        ret['response'] = res.json()
+    except Exception:
+        ret['response'] = res.text
+    return ret
+
+
+def post(param_list, meta):
+    """
+    Issue request
+    Param 1 - url
+    Param 2 - data
+    Param 3 - header
+
+    Return - response object
+    """
+    url = param_list[0]
+    data = param_list[1]
+    header = param_list[2]
+    res = requests.post(url, data=data, headers=header)
+    ret = {'status_code': res.status_code}
+    try:
+        ret['response'] = res.json()
+    except Exception:
+        ret['response'] = res.text
+    return ret
+
+
+def put(param_list, meta):
+    """
+    Issue request
+    Param 1 - url
+    Param 2 - data
+    Param 3 - header
+
+    Return - response object
+    """
+    url = param_list[0]
+    data = param_list[1]
+    header = param_list[2]
+    res = requests.put(url, data=data, headers=header)
+    ret = {'status_code': res.status_code}
+    try:
+        ret['response'] = res.json()
+    except Exception:
+        ret['response'] = res.text
+    return ret
+
+
+def delete(param_list, meta):
+    """
+    Issue request
+    Param 1 - url
+    Param 2 - data
+    Param 3 - header
+
+    Return - response object
+    """
+    url = param_list[0]
+    data = param_list[1]
+    header = param_list[2]
+    res = requests.delete(url, data=data, headers=header)
+    ret = {'status_code': res.status_code}
+    try:
+        ret['response'] = res.json()
+    except Exception:
+        ret['response'] = res.text
+    return ret
+
+
+def head(param_list, meta):
+    """
+    Issue request
+    Param 1 - url
+    Param 2 - data
+    Param 3 - header
+
+    Return - response object
+    """
+    url = param_list[0]
+    data = param_list[1]
+    header = param_list[2]
+    res = requests.head(url, data=data, headers=header)
+    ret = {'status_code': res.status_code}
+    try:
+        ret['response'] = res.json()
+    except Exception:
+        ret['response'] = res.text
+    return ret
+
+
+def options(param_list, meta):
+    """
+    Issue request
+    Param 1 - url
+    Param 2 - data
+    Param 3 - header
+
+    Return - response object
+    """
+    url = param_list[0]
+    data = param_list[1]
+    header = param_list[2]
+    res = requests.options(url, data=data, headers=header)
+    ret = {'status_code': res.status_code}
+    try:
+        ret['response'] = res.json()
+    except Exception:
+        ret['response'] = res.text
+    return ret
---
+++ jaseci_core/jaseci/actions/request.py
@@ -0,0 +1,4 @@
+"""Built in actions for Jaseci"""
+
+
+from .module.request_actions import *  # noqa
