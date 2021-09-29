c9fdbc4e2042ec9013579e9d4bad7334f8da3220
cristopher.evangelista <cristopher.evangelista@bcstechnology.com.au>
2021-09-30 03:51:04 +0800

    added api for payment method

--- jaseci_core/jaseci/api/stripe_api.py
+++ jaseci_core/jaseci/api/stripe_api.py
@@ -51,6 +51,60 @@ class stripe_api():
         except Exception as e:
             return {"message": str(e)}
 
+    def admin_api_stripe_retrieve_customer_info(self, customerId: str):
+        """ retrieve customer information """
+        try:
+            customer = stripe.Customer.retrieve("cus_KH2zTwcvguOfGd")
+            return {"data": customer}
+        except Exception as e:
+            return {"message": str(e)}
+
+    def admin_api_stripe_add_customer_payment_methods(self, paymentMethodId: str, customerId: str):
+        """ add customer payment method """
+        try:
+            paymentMethods = self.admin_api_stripe_retrieve_customer_payment_methods(customerId)
+
+            paymentMethod = stripe.PaymentMethod.attach(paymentMethodId, customer=customerId)
+
+            if(len(paymentMethods["data"].data) == 0):
+                self.admin_api_stripe_update_default_payment_method(customerId, paymentMethodId)
+            
+            return {"data": paymentMethod, "is_default": len(paymentMethods["data"].data) == 0}
+
+        except Exception as e:
+            return {"message": str(e)}
+
+    def admin_api_stripe_remove_customer_payment_methods(self, paymentMethodId: str):
+        """ add customer payment method """
+        try:
+            paymentMethod = stripe.PaymentMethod.detach(paymentMethodId)
+            
+            return {"data": paymentMethod}
+
+        except Exception as e:
+            return {"message": str(e)}
+
+    def admin_api_stripe_retrieve_customer_payment_methods(self, customerId: str):
+        """ get customer list of payment methods """
+        try:
+            paymentMethods = stripe.PaymentMethod.list(
+                customer=customerId,
+                type="card",
+            )
+
+            return {"data": paymentMethods}
+        except Exception as e:
+            return {"message": str(e)}
+
+    def admin_api_stripe_update_default_payment_method(self, customerId: str, paymentMethodId: str):
+        """ update default payment method of customer """
+        try:
+            customer = stripe.Customer.modify(customerId, invoice_settings={"default_payment_method": paymentMethodId})
+            
+            return {"data": customer}
+        except Exception as e:
+            return {"message": str(e)}
+
     def admin_api_stripe_customer_subscription(self, customerId: str, priceId: str):
         """ create customer subscription """
         try:
