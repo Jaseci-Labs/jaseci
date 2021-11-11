02c17f18255bf1bb66d23f7a9708cfefebc0e88d
Jason Mars <jason@mars.ninja>
2021-11-11 10:38:04 -0500

    Changing mysqlclient version

--- jaseci_serv/setup.py
+++ jaseci_serv/setup.py
@@ -12,7 +12,7 @@ setup(
         'drf-yasg>=1.20.0,<1.21.0',
         'markdown>=3.2.1,<3.3.0',
         'psycopg2-binary>=2,<3',
-        'mysqlclient>=2.0.3,<2.1.0',
+        'mysqlclient',
         'sphinx>=2.4.3,<2.5.0',
         'django-cors-headers',
         'tblib'
