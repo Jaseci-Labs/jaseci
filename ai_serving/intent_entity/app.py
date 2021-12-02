febf53c7e65825360a55cf4f3e798c059e472918
AshishMahendra <ashishmahendra@live.com>
2021-12-02 19:29:56 +0530

    1.Created API's using fast API 2. added readme and requirements file

--- ai_serving/intent_entity/app.py
+++ ai_serving/intent_entity/app.py
@@ -1,10 +1,25 @@
+from pydantic import BaseModel
+from typing import List,Optional
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 from transformers import pipeline
 from transformers import AutoTokenizer, AutoModelForTokenClassification
+
+#initializing the FastApi object
+app = FastAPI()
+
+#declaring the request body content
+class ParseText(BaseModel):
+    text: str 
+    labels: List
+    is_multi_label: Optional[bool] = False
 
 #zero-Shot_model
 zs_model_name="joeddav/bart-large-mnli-yahoo-answers"
 zs_pipeline = pipeline("zero-shot-classification", model=zs_model_name)
 print("Label Classification Model Loaded\n")
+
 #NER-Model
 ner_model_name = "dbmdz/bert-large-cased-finetuned-conll03-english"
 tokenizer = AutoTokenizer.from_pretrained(ner_model_name)
@@ -12,22 +27,22 @@ model = AutoModelForTokenClassification.from_pretrained(ner_model_name)
 ner_pipeline = pipeline("ner", model=model, tokenizer=tokenizer)
 print("NER Model Loaded\n")
 
-def intentClassification(text):
-    candidate_labels = ["News", "Travel", "Weather", "Greet"]
-    zs_results = zs_pipeline(text, candidate_labels, multi_label=True)
+#defining the api for intentclassification
+@app.post("/intentclassification")
+def intentClassification(request_data : ParseText):
+    print(request_data.text)
+    zs_results = zs_pipeline(request_data.text, request_data.labels, multi_label=request_data.is_multi_label)
     print("Label Classification Score :\n")
     print(f'Text : {zs_results["sequence"]} \n\t\tIntents\t\t\t\t\t\tScores\n{zs_results["labels"]} : {zs_results["scores"]}')
+    json_compatible_data = jsonable_encoder(zs_results)
+    return JSONResponse(content=json_compatible_data,status_code=200)
 
-def entityDetection(text):
+#defining the api for entitydetection
+@app.post("/entitydetection")
+def entityDetection(text : str):
     ner_results = ner_pipeline(text)
-    print("\nEntity Classification Score :\n")
-    print(ner_results)
-
-if __name__ == "__main__":
-    import argparse
-    parser = argparse.ArgumentParser(description='Need text for NLP')
-    parser.add_argument('--text', metavar='text', required=True,
-                        help='input to the model')
-    args = parser.parse_args()
-    intentClassification(text=args.text)
-    entityDetection(text=args.text)
+    entities=[]
+    for results in ner_results:
+        entities.append({"entity" : results["entity"],"conf_score":float(results["score"]),"word":results["word"]})
+    json_compatible_data = jsonable_encoder(entities)
+    return JSONResponse(content=json_compatible_data,status_code=200)
