9a35e6300ed11870b60ef642fb7bcefc65df2ba7
Jason Mars <jason@mars.ninja>
2021-12-08 13:10:27 -0500

    First version 1.3 done!

--- jaseci_core/jaseci/utils/utils.py
+++ jaseci_core/jaseci/utils/utils.py
@@ -72,6 +72,15 @@ def is_jsonable(x):
         return False
 
 
+def json_out(val):
+    if(type(val) == str):
+        return val
+    try:
+        return json.dumps(val)
+    except Exception:
+        return val
+
+
 def get_all_subclasses(cls):
     """Return list of all subclasses of cls"""
     return set(cls.__subclasses__()).union(
