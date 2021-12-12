75a1c111ee0ddccafe4c84ab247b11532e0514e6
Jason Mars <jason@mars.ninja>
2021-12-12 14:53:07 -0500

    import appears to be working with test

--- jaseci_core/jaseci/jac/ir/ast.py
+++ jaseci_core/jaseci/jac/ir/ast.py
@@ -4,9 +4,10 @@ AST for Jac
 from antlr4 import InputStream, CommonTokenStream, ParseTreeListener
 from antlr4.error.ErrorListener import ErrorListener
 
-from jaseci.utils.utils import logger
+from jaseci.utils.utils import logger, parse_str_token
 from jaseci.jac.jac_parse.jacLexer import jacLexer
 from jaseci.jac.jac_parse.jacParser import jacParser, ParseTreeWalker
+import os
 
 
 class ast():
@@ -17,17 +18,21 @@ class ast():
     TODO: Error handling if jac program has errors
     """
 
-    def __init__(self, jac_text=None, parse_errors=None, start_rule='start'):
+    def __init__(self, jac_text=None, mod_name=None, imported=None,
+                 parse_errors=None, start_rule='start'):
         self.name = 'unparsed'
         self.kind = 'unparsed'
         self.context = {}
         self.parse_errors = parse_errors if parse_errors else []
         self.start_rule = start_rule
+        self.mod_name = mod_name if mod_name is not None else "@text_str"
         self.line = 0
         self.column = 0
         self.kid = []
         if(jac_text):
-            self.parse_jac_str(jac_text)
+            if imported is None:
+                imported = []
+            self.parse_jac_str(jac_text, imported)
 
     def is_rule(self):
         """Returns true if node is a rule"""
@@ -54,13 +59,7 @@ class ast():
         if(self.is_terminal()):
             return self.token()['symbol']
 
-    def parse_jac_file(self, jac_fn):
-        """Parse language and build ast from jac file"""
-        with open(jac_fn, 'r') as file:
-            jac_str = file.read()
-        self.parse_jac_str(jac_str)
-
-    def parse_jac_str(self, jac_str):
+    def parse_jac_str(self, jac_str, imported):
         """Parse language and build ast from string"""
         input_stream = InputStream(jac_str)
         lexer = jacLexer(input_stream)
@@ -70,7 +69,7 @@ class ast():
         parser.removeErrorListeners()
         parser.addErrorListener(errors)
         tree = getattr(parser, self.start_rule)()
-        builder = self.jac_tree_builder(self)
+        builder = self.jac_tree_builder(self, imported)
         walker = ParseTreeWalker()
         walker.walk(builder, tree)
 
@@ -114,9 +113,37 @@ class ast():
     class jac_tree_builder(ParseTreeListener):
         """Converter class from Antlr trees to Jaseci Tree"""
 
-        def __init__(self, tree_root):
+        def __init__(self, tree_root, imported):
             self.tree_root = tree_root
             self.node_stack = []
+            self.imported = [tree_root.mod_name]+imported
+
+        def run_import_module(self, jac_ast):
+            """
+            import_module: KW_IMPORT STRING SEMI;
+            """
+            kid = jac_ast.kid
+            fn = parse_str_token(kid[1].token_text())
+            mod_name = os.path.basename(fn)
+            logger.info(f"{self.imported}")
+            if(mod_name in self.imported):
+                logger.error(
+                    f"Already imported {mod_name}, may "
+                    f"be circular from {self.tree_root.mod_name}")
+            elif (os.path.isfile(fn)):
+                with open(fn, 'r') as file:
+                    jac_text = file.read()
+                self.imported.append(mod_name)
+                return filter(lambda x:
+                              x.name == 'element',
+                              ast(jac_text=jac_text,
+                                  mod_name=mod_name,
+                                  imported=self.imported).kid)
+            else:
+                err = f"Module not found for import! {mod_name} from" +\
+                    f" {self.tree_root.mod_name}"
+                self.tree_root.parse_errors.append(err)
+            return []
 
         def enterEveryRule(self, ctx):
             """Visits every node in antlr parse tree"""
@@ -130,13 +157,15 @@ class ast():
             new_node.line = ctx.start.line
             new_node.column = ctx.start.column
 
-            if(len(self.node_stack)):
+            if(len(self.node_stack) and new_node.name != 'import_module'):
                 self.node_stack[-1].kid.append(new_node)
             self.node_stack.append(new_node)
 
         def exitEveryRule(self, ctx):
             """Overloaded function that visits every node on exit"""
-            self.node_stack.pop()
+            top = self.node_stack.pop()
+            if(top.name == 'import_module'):
+                self.node_stack[-1].kid += self.run_import_module(top)
 
         def visitTerminal(self, node):
             """Visits terminals as walker walks, adds ast node"""
@@ -164,6 +193,6 @@ class ast():
                         line, column, msg, e):
             """Add error to error list"""
             self.tree_root.parse_errors.append(
-                f"{self.tree_root}: line {str(line)}:"
-                f"{str(column)} - {msg}"
+                f"{str(self.tree_root.mod_name)}: line {str(line)}:"
+                f"{str(column)} - {self.tree_root} - {msg}"
             )
