0e80cfb898c19c40a17c2e01c5920acd965668e5
Jason Mars <jason@mars.ninja>
2022-01-23 16:14:21 -0500

    Manual testing passed

--- jaseci_core/jaseci/api/actions_api.py
+++ jaseci_core/jaseci/api/actions_api.py
@@ -2,6 +2,7 @@
 Admin Global api functions as a mixin
 """
 from jaseci.api.interface import interface
+import jaseci.actions.live_actions as lact
 
 
 class actions_api():
@@ -9,10 +10,11 @@ class actions_api():
     Admin global APIs
     """
 
-    @interface.admin_api()
-    def actions_register(self, name: str, value: str):
+    @interface.admin_api(cli_args=['file'])
+    def actions_register(self, file: str):
         """
         """
+        lact.load_actions(file)
 
     @interface.admin_api()
     def actions_get(self, name: str, value: str):
@@ -20,9 +22,14 @@ class actions_api():
         """
 
     @interface.admin_api()
-    def actions_list(self, name: str, value: str):
+    def actions_list(self, name: str = ''):
         """
         """
+        actions = list(lact.live_actions.keys())
+        if(not len(name)):
+            return actions
+        else:
+            return list(filter(lambda a: a.startswith(name), actions))
 
     @interface.admin_api()
     def actions_delete(self, name: str, value: str):
