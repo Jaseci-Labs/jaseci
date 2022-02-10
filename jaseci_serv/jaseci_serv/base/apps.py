b6abddcf75e7c366d364517a069f7d97286e9961
Jason Mars <jason@mars.ninja>
2022-02-10 18:28:26 -0500

    improved approach of loading config on demand from within core

--- jaseci_serv/jaseci_serv/base/apps.py
+++ jaseci_serv/jaseci_serv/base/apps.py
@@ -3,13 +3,3 @@ from django.apps import AppConfig
 
 class CoreApiConfig(AppConfig):
     name = 'base'
-
-    def ready(self):
-        from jaseci.actions.live_actions import load_preconfig_actions
-        from jaseci_serv.base.orm_hook import orm_hook
-        from jaseci_serv.base.models import JaseciObject, GlobalVars
-        hook = orm_hook(
-            objects=JaseciObject.objects,
-            globs=GlobalVars.objects
-        )
-        load_preconfig_actions(hook)
