4fa5cb46702dd5e50ec8d83313b37a2fa34cc805
Ashish Mahendra <ashishmahendra@live.com>
2022-03-24 21:24:43 +0530

    Updates for entity extraction for RNN model (#155)

--- jskit/entity_extraction/test_data.py
+++ jskit/entity_extraction/test_data.py
@@ -8,18 +8,24 @@ test_entity_detection_response = {
     "entities": [
         {
             "entity_text": "Humboldt University of Berlin",
-            "entity_value": "University",
-            "conf_score": 0.9999905824661255
+            "entity_value": "ORG",
+            "conf_score": 0.9708927571773529,
+            "start_pos": 4,
+            "end_pos": 33
         },
         {
             "entity_text": "Berlin",
-            "entity_value": "City",
-            "conf_score": 0.9999960660934448
+            "entity_value": "LOC",
+            "conf_score": 0.9977847933769226,
+            "start_pos": 49,
+            "end_pos": 55
         },
         {
             "entity_text": "Germany",
-            "entity_value": "Country",
-            "conf_score": 0.999994158744812
+            "entity_value": "LOC",
+            "conf_score": 0.9997479319572449,
+            "start_pos": 57,
+            "end_pos": 64
         }
     ]
 }
@@ -36,11 +42,26 @@ test_entity_detection_request_fail_text = {
 
 
 test_entity_training_pass = {
-    "text": "Who is Shaka Khan?",
+    "text": "The Humboldt University of Berlin is situated in Berlin, Germany",
     "entity": [
         {
-            "entity_value": "Shaka Khan",
-            "entity_name": "PERSON"
+            "entity_value": "Humboldt University of Berlin",
+            "entity_name": "University",
+            "start_index": 4,
+            "end_index": 34
+        },
+        {
+            "entity_value": "Berlin",
+            "entity_name": "City",
+            "start_index": 49,
+            "end_index": 55
+        },
+        {
+            "entity_value": "Germany",
+            "entity_name": "Country",
+            "start_index": 58,
+            "end_index": 67
+
         }
     ]
 }
@@ -61,4 +82,19 @@ test_entity_training_fail2 = {
             "entity_name": "PERSON"
         }
     ]
+}
+
+test_entity_config_setup_ner = {
+    "ner_model": "ner",
+    "model_type": "LSTM"
+}
+
+test_entity_config_setup_trf = {
+    "ner_model": "distilbert-base-uncased",
+    "model_type": "TRFMODEL"
+}
+
+test_entity_config_setup_blank = {
+    "ner_model": "None",
+    "model_type": "LSTM"
 }
