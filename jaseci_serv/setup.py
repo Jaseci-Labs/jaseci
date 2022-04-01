aa0aada60076ba0d279a6d80905f3c40fe6dca09
Jason Mars <jason@mars.ninja>
2022-04-01 11:10:41 -0400

    Tests pass, non-det error with parallel build exposed

--- jaseci_serv/setup.py
+++ jaseci_serv/setup.py
@@ -5,7 +5,7 @@ setup(
     packages=find_packages(include=['jaseci_serv', 'jaseci_serv.*']),
     install_requires=[
         'jaseci',
-        'Django>=3.1.0,<3.2.0',
+        'Django>=3.2.0,<3.3.0',
         'djangorestframework>=3.11.0,<3.12.0',
         'django-rest-knox>=4.1.0,<4.2.0',
         'django-rest-passwordreset>=1.1.0,<1.2.0',
