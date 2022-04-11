d933052dfb36fb54baf2397d5208787a423464b6
Jason Mars <jason@mars.ninja>
2022-04-10 22:17:19 -0400

    Added pandas just in case

--- jaseci_jskit/setup.py
+++ jaseci_jskit/setup.py
@@ -10,6 +10,7 @@ setup(
         'tensorflow-text >= 2.7.3, < 3.0.0',
         'transformers >= 4.16.2, < 5.0.0',
         'torch >= 1.10.2, < 2.0.0',
+        'pandas>=1.4.1,<2.0.0'
     ],
     package_data={"": ["*.ini"], },
 )
