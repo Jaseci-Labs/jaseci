da165c86bf65775a024d536d9e17aba0ed2b6315
Jason Mars <jason@mars.ninja>
2022-04-11 01:00:47 -0400

    JSKit installabiliyt done I think

--- jaseci_jskit/setup.py
+++ jaseci_jskit/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name='jaseci_jskit',
-    version='1.3.2.11',
+    version='1.3.3.0',
     packages=find_packages(include=['jaseci_jskit', 'jaseci_jskit.*']),
     install_requires=[
         'jaseci',
