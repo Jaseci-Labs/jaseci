b90e325446b0320a40002198cb6def1354bd0605
Jason Mars <jason@mars.ninja>
2022-04-21 08:26:54 -0400

    Changelog update and tagging up

--- jaseci_kit/setup.py
+++ jaseci_kit/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='jaseci_kit',
-    version='1.3.3.6',
+    version='1.3.3.7',
     packages=find_packages(include=['jaseci_kit', 'jaseci_kit.*']),
     install_requires=[
         'jaseci',
