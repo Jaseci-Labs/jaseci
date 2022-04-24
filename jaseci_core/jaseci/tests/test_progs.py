57caf7587b341ad280651ba6b4d83bec8cf64f1c
Jason Mars <jason@mars.ninja>
2022-04-24 11:04:20 -0400

    gettign tests normalized

--- jaseci_core/jaseci/tests/test_progs.py
+++ jaseci_core/jaseci/tests/test_progs.py
@@ -79,7 +79,6 @@ class jac_tests(TestCaseHelper, TestCase):
         self.assertIn("line 3", report['errors'][0])
 
     def test_strange_ability_bug(self):
-        self.logger_on()
         mast = master(h=mem_hook())
         mast.sentinel_register(
             name='test', code=jtp.strange_ability_bug)
