fe7409610a7c1fd19e71581cdbc1ecc54147eae5
Jason Mars <jason@mars.ninja>
2022-04-28 16:40:07 -0400

    changelog and tag up

--- jaseci_core/setup.py
+++ jaseci_core/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="jaseci",
-    version="1.3.3.14",
+    version="1.3.3.15",
     packages=find_packages(include=["jaseci", "jaseci.*"]),
     install_requires=[
         "click>=8.1.0,<8.2.0",
