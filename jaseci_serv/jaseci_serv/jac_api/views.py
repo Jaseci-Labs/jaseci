5515617aedd4b8e21715e4bad2ce7932dd1ac294
Jason Mars <jason@mars.ninja>
2022-05-14 09:12:30 -0400

    Tweak on get support for allowed_methods

--- jaseci_serv/jaseci_serv/jac_api/views.py
+++ jaseci_serv/jaseci_serv/jac_api/views.py
@@ -33,6 +33,17 @@ class AbstractJacAPIView(APIView):
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
 
+    def get(self, request, **kwargs):
+        """
+        General GET function that parses api signature to load parms
+        SuperSmart GET - can read signatures of master and process
+        bodies accordingly
+        """
+        self.proc_request(request, **kwargs)
+        api_result = self.caller.general_interface_to_api(self.cmd, type(self).__name__)
+        self.log_request_stats()
+        return self.issue_response(api_result)
+
     def post(self, request, **kwargs):
         """
         General post function that parses api signature to load parms
