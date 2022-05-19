f1f873d7cff007ef47df3ef063cc3d1618e255a8
Patrick Li <1543162420@qq.com>
2022-05-19 16:52:03 -0400

    USTABLE

--- support/locust/run_jac.py
+++ support/locust/run_jac.py
@@ -46,21 +46,39 @@ class SeqTask(SequentialTaskSet):
         json_var = response.json()
         self.user_token = json_var["token"]
 
+    # @task
+    # def get_global_sentinel(self):
+    #     req = {
+    #             "snt": SNT
+    #             }
+    #     response = self.client.post(
+    #             '/js/sentinel_active_global',
+    #             headers = {"authorization": f"Token {self.user_token}"},
+    #             )
+    #     print(response.text)
     @task
-    def get_global_sentinel(self):
-        req = {
-                "snt": SNT
-                }
+    def get_sentinel(self):
+        self.snt = prepare.registerSentinel(self.user_token)
+        prepare.load_actions(self.user_token)
+
+    @task 
+    def create_graph(self):
+
+        headers={"authorization": f"Token {self.user_token}"}
         response = self.client.post(
-                '/js/sentinel_active_global',
-                headers = {"authorization": f"Token {self.user_token}"},
+                "/js/graph_create",
+                headers = headers,
+                json = {
+                    "set_active" : True
+                    }
                 )
         print(response.text)
+        self.graph_id = response.json()['jid']
 
     @task
     def walker_run(self):
         for walkerName in load_config(TEST_PATH)["walkers"]:
-            req = {"name": walkerName, "snt": SNT}
+            req = {"name": walkerName, "snt": self.snt}
             # print(f"Walker {walkerName} running.")
             response = self.client.post(
                 "/js/walker_run",
@@ -68,6 +86,18 @@ class SeqTask(SequentialTaskSet):
                 json=req,
             )
             print(f"Walker {walkerName} finished. {response.text}")
+
+    @task
+    def delete_graph(self):
+
+        headers={"authorization": f"Token {self.user_token}"}
+        response = self.client.post(
+                "/js/graph_delete",
+                headers = headers,
+                json = {
+                    "gph" : self.graph_id
+                    }
+                )
 
 
 class addJac(HttpUser):
@@ -78,8 +108,8 @@ class addJac(HttpUser):
 
 
 
-token = prepare.login(userID = 0)
-# global SNT
-SNT = prepare.registerSentinel(token)
-prepare.setSentinelGlobal(token = token, snt = SNT)
-prepare.load_actions(token)
+# token = prepare.login(userID = 0)
+# # global SNT
+# SNT = prepare.registerSentinel(token)
+# prepare.setSentinelGlobal(token = token, snt = SNT)
+# prepare.load_actions(token)
