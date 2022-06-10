b6b4b97a4e59863b02fe7a3a853d66b263e74efd
Patrick Li <1543162420@qq.com>
2022-06-10 14:06:09 -0400

    feat(support.monitoring.promon): cpu utilization done
    Add two methods for cpu utilization. Core / Percentage.

--- support/monitoring/promon/__main__.py
+++ support/monitoring/promon/__main__.py
@@ -1,7 +1,28 @@
+from numpy import double
 from prometheus_api_client import PrometheusConnect
 
-prom = PrometheusConnect(url = "http://clarity31.eecs.umich.edu:8082", disable_ssl=True)
+class Promon:
+  def __init__(self, url: str):
+    self.prom = PrometheusConnect(url = url, disable_ssl=True)
+  def all_metrics(self)->list:
+    return self.prom.all_metrics()
+  def cpu_utilization_core(self)->dict:
+    util = self.prom.get_current_metric_value("sum(irate(node_cpu_seconds_total{mode!=\"idle\"}[10m])) by (node)")
+    res = {}
+    for node in util:
+      nodeName = node["metric"]["node"]
+      nodeUtil = node["value"][1]
+      res[nodeName] = nodeUtil
+    return res
+  
+  def cpu_utilization_percentage(self)->dict:
+    util = self.prom.get_current_metric_value("(sum(irate(node_cpu_seconds_total{mode!=\"idle\"}[10m])) by (node)) / (sum(irate(node_cpu_seconds_total{mode!=\"\"}[10m])) by (node)) * 100")
+    res = {}
+    for node in util:
+      nodeName = node["metric"]["node"]
+      nodeUtil = node["value"][1]
+      res[nodeName] = nodeUtil
+    return res
 
-print(prom.all_metrics())
-
-print(prom.get_current_metric_value("count(kube_node_info)"))
+p = Promon("http://clarity31.eecs.umich.edu:8082")
+print(p.cpu_utilization_percentage(), p.cpu_utilization_core())
