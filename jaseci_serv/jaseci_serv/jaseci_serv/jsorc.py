90029b4edf3ede0bc56b2774ce8a3659f1e2894b
Patrick Li <1543162420@qq.com>
2022-06-15 15:31:33 -0400

    refactor(jsorc): changed the code format a little bit

--- jaseci_serv/jaseci_serv/jaseci_serv/jsorc.py
+++ jaseci_serv/jaseci_serv/jaseci_serv/jsorc.py
@@ -5,14 +5,13 @@ config.load_kube_config()  # or config.load_kube_config()
 
 configuration = client.Configuration()
 
-with client.ApiClient(configuration) as api_client:
-    api_instance = client.CoreV1Api()
+api_instance = client.CoreV1Api()
 
-    namespace = "default"  # str | see @Max Lobur's answer on how to get this
-    name = "jaseci-redis-5f98d98bc4-dmxch"  # str | Pod name, e.g. via api_instance.list_namespaced_pod(namespace)
+namespace = "default"  # str | see @Max Lobur's answer on how to get this
+name = "jaseci-redis-5f98d98bc4-dmxch"  # str | Pod name, e.g. via api_instance.list_namespaced_pod(namespace)
 
-    try:
-        api_response = api_instance.delete_namespaced_pod(name, namespace)
-        print(api_response)
-    except ApiException as e:
-        print("Exception when calling CoreV1Api->delete_namespaced_pod: %s\n" % e)
+try:
+    api_response = api_instance.delete_namespaced_pod(name, namespace)
+    print(api_response)
+except ApiException as e:
+    print("Exception when calling CoreV1Api->delete_namespaced_pod: %s\n" % e)
