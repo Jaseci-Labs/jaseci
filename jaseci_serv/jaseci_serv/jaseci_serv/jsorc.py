ade1f38cc2fef11334f55de66fe6d2e173b84e0b
Patrick Li <1543162420@qq.com>
2022-06-16 15:56:15 -0400

    refactor(jsorc): wrap things into a class

--- jaseci_serv/jaseci_serv/jaseci_serv/jsorc.py
+++ jaseci_serv/jaseci_serv/jaseci_serv/jsorc.py
@@ -2,37 +2,41 @@ from kubernetes import client, config
 from kubernetes.client.rest import ApiException
 import yaml
 
-# Configs can be set in Configuration class directly or using helper utility
-config.load_kube_config()
-api_instance = client.CoreV1Api()
-app_api = client.AppsV1Api()
 
+class KubeController:
+    # Configs can be set in Configuration class directly or using helper utility
+    config.load_kube_config()
+    api_instance = client.CoreV1Api()
+    app_api = client.AppsV1Api()
 
-def get_pod_list():
-    print("Listing pods with their IPs:")
-    ret = api_instance.list_pod_for_all_namespaces(watch=False)
-    res = []
-    for i in ret.items:
-        print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
-        res.append(i.metadata)
-    return res
+    def get_pod_list(self):
+        print("Listing pods with their IPs:")
+        ret = self.api_instance.list_pod_for_all_namespaces(watch=False)
+        res = []
+        for i in ret.items:
+            print(
+                "%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name)
+            )
+            res.append(i.metadata)
+        return res
 
-
-def create_jaseci_redis_deployment(config: dict, namespace: str = "default"):
-    print(config)
-    return app_api.create_namespaced_deployment(namespace=namespace, body=config)
-
-
-def kill_jaseci_redis_pod(name: str, namespace: str = "default"):
-    try:
-        api_response = app_api.delete_namespaced_deployment(
-            name=name, namespace=namespace
+    def create_jaseci_redis_deployment(self, config: dict, namespace: str = "default"):
+        print(config)
+        return self.app_api.create_namespaced_deployment(
+            namespace=namespace, body=config
         )
-        print(api_response)
-    except ApiException as e:
-        print("Exception when calling CoreV1Api->delete_namespaced_pod: %s\n" % e)
+
+    def kill_jaseci_redis_pod(self, name: str, namespace: str = "default"):
+        try:
+            api_response = self.app_api.delete_namespaced_deployment(
+                name=name, namespace=namespace
+            )
+            print(api_response)
+        except ApiException as e:
+            print("Exception when calling CoreV1Api->delete_namespaced_pod: %s\n" % e)
 
 
 if __name__ == "__main__":
-    create_jaseci_redis_deployment(yaml.safe_load(open("jaseci.yaml", "r")))
-    # kill_jaseci_redis_pod("jaseci-redis")
+    k = KubeController()
+    k.create_jaseci_redis_deployment(yaml.safe_load(open("jaseci.yaml", "r")))
+    # k.kill_jaseci_redis_pod("jaseci-redis")
