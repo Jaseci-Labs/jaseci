d401d375d5537ddf7424e5aee47eabaa24dd340c
Alexie (Boyong) Madolid <maex@tuta.io>
2022-09-06 13:24:18 +0800

    [UNITTEST]: Added some minor logs to monitor inconsistent failure

--- jaseci_core/jaseci/jac/interpreter/tests/test_interp.py
+++ jaseci_core/jaseci/jac/interpreter/tests/test_interp.py
@@ -22,6 +22,9 @@ class interpreter_test(core_test):
             ["sentinel_register", {"code": self.load_jac("lang_features.jac")}],
         )
         ret = self.call(self.mast, ["walker_run", {"name": "deref_adaptive"}])
+        # this unit test is inconsistent. It throws failed sometimes but success after some retrigger
+        # adding this print for now just to monitor once it throws error again
+        print(ret)
         self.assertTrue(ret["report"][0].startswith("junk and stuff"))
         self.assertEqual(ret["report"][1], {"name": "node0"})
 
