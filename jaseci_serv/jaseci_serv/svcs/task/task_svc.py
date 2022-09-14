672d7b32fc3922ced2f7d2a0e16c9244581ebad7
Alexie (Boyong) Madolid <maex@tuta.io>
2022-09-14 16:34:59 +0800

    [ENHANCEMENT]: Change get task by id implementation
    CHANGES:
     - Use AsyncResult as the main getter for results
     - Add initial AsyncResult call for connection testing

--- jaseci_serv/jaseci_serv/svcs/task/task_svc.py
+++ jaseci_serv/jaseci_serv/svcs/task/task_svc.py
@@ -1,5 +1,3 @@
-import json
-from django_celery_results.models import TaskResult
 from jaseci_serv.jaseci_serv.settings import TASK_CONFIG
 from jaseci.svcs.task.task_svc import task_svc as ts
 
@@ -10,19 +8,5 @@ from jaseci.svcs.task.task_svc import task_svc as ts
 
 
 class task_svc(ts):
-    def get_by_task_id(self, task_id, hook=None):
-        task = self.app.AsyncResult(task_id)
-
-        ret = {"status": task.state}
-
-        if task.ready():
-            task_result = TaskResult.objects.get(task_id=task_id).result
-            try:
-                ret["result"] = json.loads(task_result)
-            except ValueError:
-                ret["result"] = task_result
-
-        return ret
-
     def get_config(self, hook) -> dict:
         return hook.build_config("TASK_CONFIG", TASK_CONFIG)
