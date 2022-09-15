b1b559dc772924d5377efbe68a6d69612d75bce3
Alexie (Boyong) Madolid <maex@tuta.io>
2022-09-15 15:24:12 +0800

    [UNIT-TEST]: Add missing trigger for skip_without_redis decoration

--- jaseci_serv/jaseci_serv/utils/test_utils.py
+++ jaseci_serv/jaseci_serv/utils/test_utils.py
@@ -11,5 +11,6 @@ def skip_without_redis(test):
     def skipper(*args, **kwargs):
         if not RedisService().is_running():
             raise unittest.SkipTest("No Redis!")
+        test(*args, **kwargs)
 
     return skipper
