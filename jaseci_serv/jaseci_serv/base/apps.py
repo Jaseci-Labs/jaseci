4e01096ac0613d46055cb3befaf4bd751bd87251
Patrick Li <1543162420@qq.com>
2022-09-15 23:09:20 -0400

    fix(jsorc): move jsorc to jac_api

--- jaseci_serv/jaseci_serv/base/apps.py
+++ jaseci_serv/jaseci_serv/base/apps.py
@@ -1,9 +1,5 @@
 from django.apps import AppConfig
-from .jsorc.jsorc import jsorc_start
 
 
 class CoreApiConfig(AppConfig):
     name = "jaseci_serv.base"
-
-    def ready(self):
-        jsorc_start()
--- jaseci_serv/jaseci_serv/jac_api/apps.py
+++ jaseci_serv/jaseci_serv/jac_api/apps.py
@@ -1,5 +1,9 @@
 from django.apps import AppConfig
+from .jsorc.jsorc import jsorc_start
 
 
 class JacApiConfig(AppConfig):
     name = "jaseci_serv.jac_api"
+
+    def ready(self):
+        jsorc_start()
