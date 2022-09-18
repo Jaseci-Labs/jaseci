63ef705943d3c6a7f8d71a1bae79a83da927fc5e
Yiping Kang <ypkang@umich.edu>
2022-09-18 09:59:08 -0400

    Tag up 1.3.4.9 for all three jaseci packages

--- jaseci_ai_kit/setup.py
+++ jaseci_ai_kit/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="jaseci_ai_kit",
-    version="1.3.4.8",
+    version="1.3.4.9",
     packages=find_packages(include=["jaseci_ai_kit", "jaseci_ai_kit.*"]),
     install_requires=[
         "jaseci",
