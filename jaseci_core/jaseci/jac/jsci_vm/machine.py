e8120f0ad692f3e3d588f4d0c5cdf2a57f420d46
Jason Mars <jason@mars.ninja>
2022-10-10 21:31:54 -0400

    Machine started

--- jaseci_core/jaseci/jac/jsci_vm/machine.py
+++ jaseci_core/jaseci/jac/jsci_vm/machine.py
@@ -1,3 +1,6 @@
+from jaseci.jac.jsci_vm.op_codes import JsOp, JsAttr
+
+
 class Stack(object):
     def __init__(self):
         self.stack = []
@@ -18,10 +21,45 @@ class JaseciMachine(Stack):
     def __init__(self):
         Stack.__init__(self)
         self.ip = 0
+        self.bytecode = None
+        self.op = self.build_op_call()
 
-    def __call__(self, cmd):
-        self.codes = cmd
-        while self.instruction_pointer < len(self.codes["instruction"]):
+    def build_op_call(self):
+        op_map = {}
+        for op in JsOp:
+            op_map[op.value] = getattr(self, f"op_{op.name}")
+        return op_map
+
+    def run(self, bytecode):
+        self.bytecode = bytecode
+        while self.ip < len(self.bytecode):
             (opcode, oparg) = self.codes["instruction"][self.instruction_pointer]
             # branching statements for multiple opcode
             self.instruction_pointer += 1
+
+    def op_PUSH_SCOPE(self):
+        pass
+
+    def op_POP_SCOPE(self):
+        pass
+
+    def op_PUSH(self):
+        pass
+
+    def op_ADD(self):
+        pass
+
+    def op_SUB(self):
+        pass
+
+    def op_LOAD_CONST(self):
+        pass
+
+    def op_LOAD_NAME(self):
+        pass
+
+    def op_REPORT(self):
+        pass
+
+    def op_ACTION_CALL(self):
+        pass
