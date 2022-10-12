e7525fd178fe24efe276cf2b3d122b12512f3906
Jason Mars <jason@mars.ninja>
2022-10-12 19:53:22 -0400

    BytecodeMachine fused into interpreter stack

--- jaseci_core/jaseci/jac/jsci_vm/machine.py
+++ jaseci_core/jaseci/jac/jsci_vm/machine.py
@@ -4,33 +4,33 @@ from jaseci.jac.machine.machine_state import MachineState
 
 class Stack(object):
     def __init__(self):
-        self.stack = []
+        self._stk = []
 
     def is_empty(self) -> bool:
-        return len(self.stack) == 0
+        return len(self._stk) == 0
 
     def pop(self):
         if self.is_empty():
-            raise Exception("stack is empty")
-        return self.stack.pop()
+            raise Exception("JaseciMachine stack is empty")
+        return self._stk.pop()
 
     def push(self, value):
-        self.stack.append(value)
+        self._stk.append(value)
 
     def peak_stack(self, count=0):
         if not count:
-            print(list(reversed(self.stack)))
+            print(list(reversed(self._stk)))
         else:
-            print(list(reversed(self.stack))[0:count])
+            print(list(reversed(self._stk))[0:count])
 
 
 class BytecodeMachine(MachineState, Stack):
-    def __init__(self, *args):
+    def __init__(self, **kwargs):
         Stack.__init__(self)
-        MachineState.__init__(self, *args)
-        self.ip = 0
-        self.bytecode = None
-        self.op = self.build_op_call()
+        MachineState.__init__(self, **kwargs)
+        self._ip = 0
+        self._bytecode = None
+        self._op = self.build_op_call()
 
     def build_op_call(self):
         op_map = {}
@@ -39,10 +39,10 @@ class BytecodeMachine(MachineState, Stack):
         return op_map
 
     def run(self, bytecode):
-        self.bytecode = bytearray(bytecode)
-        while self.ip < len(self.bytecode):
-            self.op[self.bytecode[self.ip]]()
-            self.ip += 1
+        self._bytecode = bytearray(bytecode)
+        while self._ip < len(self._bytecode):
+            self._op[self._bytecode[self._ip]]()
+            self._ip += 1
 
     def op_PUSH_SCOPE(self):  # noqa
         pass
@@ -58,14 +58,14 @@ class BytecodeMachine(MachineState, Stack):
         self.push(self.pop() - rhs)
 
     def op_LOAD_CONST(self):  # noqa
-        typ = JsAttr(self.bytecode[self.ip + 1])
-        byte_length = self.bytecode[self.ip + 2]
+        typ = JsAttr(self._bytecode[self._ip + 1])
+        byte_length = self._bytecode[self._ip + 2]
         if typ == JsAttr.INT:
             val = int.from_bytes(
-                self.bytecode[self.ip + 3 : self.ip + 3 + byte_length], "little"
+                self._bytecode[self._ip + 3 : self._ip + 3 + byte_length], "little"
             )
         self.push(val)
-        self.ip += 2 + byte_length
+        self._ip += 2 + byte_length
 
     def op_LOAD_NAME(self):  # noqa
         pass
