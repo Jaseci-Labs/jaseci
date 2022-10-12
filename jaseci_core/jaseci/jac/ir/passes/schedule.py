13158b1da46e022be32012505cb87fb6d9d124d8
roland daynauth <daynauth@umich.edu>
2022-10-12 15:04:30 -0400

    added type hint

--- jaseci_core/jaseci/jac/ir/passes/schedule.py
+++ jaseci_core/jaseci/jac/ir/passes/schedule.py
@@ -1,9 +1,10 @@
 from jaseci.jac.ir.passes.prune_pass import PrunePass
 from jaseci.jac.ir.passes.printer_pass import PrinterPass
 from jaseci.jac.ir.passes.stats_pass import StatsPass
+from jaseci.jac.ir.ast_builder import JacAstBuilder
 
 
-def multi_pass_optimizer(jac_ast, debug_out=False, print_walk=False):
+def multi_pass_optimizer(jac_ast: JacAstBuilder, debug_out: bool = True, print_walk: bool = True):
     if debug_out:
         if print_walk:
             PrinterPass(jac_ast).run()
