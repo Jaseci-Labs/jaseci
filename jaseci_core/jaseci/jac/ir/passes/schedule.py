901b0594f0276a0a02b7949a972616ebcd5dc2b3
roland daynauth <daynauth@umich.edu>
2022-10-12 16:25:00 -0400

    turned off debug

--- jaseci_core/jaseci/jac/ir/passes/schedule.py
+++ jaseci_core/jaseci/jac/ir/passes/schedule.py
@@ -4,7 +4,7 @@ from jaseci.jac.ir.passes.stats_pass import StatsPass
 from jaseci.jac.ir.ast_builder import JacAstBuilder
 
 
-def multi_pass_optimizer(jac_ast: JacAstBuilder, debug_out: bool = True, print_walk: bool = True):
+def multi_pass_optimizer(jac_ast: JacAstBuilder, debug_out: bool = False, print_walk: bool = False):
     if debug_out:
         if print_walk:
             PrinterPass(jac_ast).run()
