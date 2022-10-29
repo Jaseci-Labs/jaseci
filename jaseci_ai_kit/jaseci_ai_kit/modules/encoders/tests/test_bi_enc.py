87ed12801c46026cf91685f00da80267c09edca6
AshishMahendra <ashishmahendra@live.com>
2022-10-29 21:10:53 +0530

    updated

--- jaseci_ai_kit/jaseci_ai_kit/modules/encoders/tests/test_bi_enc.py
+++ jaseci_ai_kit/jaseci_ai_kit/modules/encoders/tests/test_bi_enc.py
@@ -1,27 +1,19 @@
 from jaseci.utils.test_core import CoreTest, jac_testcase
-import unittest
-import argparse
+from jaseci.actions.live_actions import load_module_actions, unload_module
 
 
 class BiEncTest(CoreTest):
     fixture_src = __file__
 
-    def setUp(self) -> None:
-        super().setUp()
-        ret = self.call(
-            self.smast,
-            ["actions_load_module", {"mod": "jaseci_ai_kit.modules.encoders.bi_enc"}],
-        )
-        self.assertEqual(ret["success"], True)
-        mod_list = self.call(self.smast, ["actions_module_list", {}])
-        self.assertIn("jaseci_ai_kit.modules.encoders.bi_enc", mod_list)
-        act_list = self.call(self.smast, ["actions_list", {}])
-        act_list = [v for v in act_list if "bi_enc" in v]
-        self.assertEqual(len(act_list), 14)
+    @classmethod
+    def setUpClass(cls):
+        super(BiEncTest, cls).setUpClass()
+        load_module_actions("jaseci_ai_kit.bi_enc")
 
     @jac_testcase("bi_enc.jac", "test_bi_enc_cos_sim")
     def test_cos_sim_function(self, ret):
-        self.assertEqual(round(ret["report"][0], 2), 0.91)
+        print(ret)
+        self.assertEqual(round(ret["report"][0], 2), 0.98)
 
     @jac_testcase("bi_enc.jac", "test_bi_enc_infer")
     def test_biencoder_infer(self, ret):
@@ -47,7 +39,6 @@ class BiEncTest(CoreTest):
 
     @jac_testcase("bi_enc.jac", "test_bi_enc_train")
     def test_biencoder_train(self, ret):
-        print(ret["report"][0])
         self.assertEqual(ret["report"][0], "Model Training is complete.")
         self.assertEqual(ret["success"], True)
 
@@ -59,52 +50,7 @@ class BiEncTest(CoreTest):
     def test_biencoder_load_model(self, ret):
         self.assertEqual(ret["success"], True)
 
-    def tearDown(self) -> None:
-        super().tearDown()
-        ret = self.call(self.smast, ["actions_module_list", {}])
-        before = len(ret)
-
-        ret = self.call(
-            self.smast,
-            [
-                "actions_unload_module",
-                {"name": "jaseci_ai_kit.modules.encoders.bi_enc"},
-            ],
-        )
-        ret = self.call(self.smast, ["actions_module_list", {}])
-        self.assertEqual(len(ret), before - 1)
-
-
-def test_suite(args):
-    suite = unittest.TestSuite()
-    suite.addTest(BiEncTest("test_cos_sim_function"))
-    suite.addTest(BiEncTest("test_biencoder_infer"))
-    suite.addTest(BiEncTest("test_biencoder_context_emb"))
-    suite.addTest(BiEncTest("test_biencoder_candidate_emb"))
-    suite.addTest(BiEncTest("test_biencoder_train_config"))
-    suite.addTest(BiEncTest("test_biencoder_model_config"))
-    suite.addTest(BiEncTest("test_biencoder_train"))
-    suite.addTest(BiEncTest("test_biencoder_save_model"))
-    suite.addTest(BiEncTest("test_biencoder_load_model"))
-
-    return suite
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--local",
-        help="Use action load local to load the module",
-        default=False,
-        action="store_true",
-    )
-    parser.add_argument(
-        "--remote",
-        help="Use action load remote to load the module",
-        default=False,
-        action="store_true",
-    )
-    args = parser.parse_args()
-
-    runner = unittest.TextTestRunner(failfast=True, warnings="ignore")
-    result = runner.run(test_suite(args))
+    @classmethod
+    def tearDownClass(cls):
+        super(BiEncTest, cls).tearDownClass()
+        unload_module("jaseci_ai_kit.bi_enc")
