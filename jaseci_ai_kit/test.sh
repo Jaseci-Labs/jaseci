24f04103c7984df65782900c19956fc45c6bff71
chandralegend <irugalbandarachandra@gmail.com>
2022-11-05 06:29:00 +0000

    removed .bak files and comments

--- jaseci_ai_kit/test.sh
+++ jaseci_ai_kit/test.sh
@@ -1,7 +1,3 @@
-# python3 -m unittest discover jaseci_ai_kit/ -p "test_$1*.py"
-# # pytest --exitfirst -v
-# # python3 utils/test_runner.py --filter "test_$1"
-
 for file in $(find jaseci_ai_kit/ -name "test_$1*.py"); do
     echo "running $file"
     pytest --exitfirst -v $file
