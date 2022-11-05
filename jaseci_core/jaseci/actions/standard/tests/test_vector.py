ff7fb0d660dcfaf4703ee2209f8f2d866d8bc266
chandralegend <irugalbandarachandra@gmail.com>
2022-11-05 19:08:12 +0000

    simplified the vector tests

--- jaseci_core/jaseci/actions/standard/tests/test_vector.py
+++ jaseci_core/jaseci/actions/standard/tests/test_vector.py
@@ -1,4 +1,4 @@
-from jaseci.utils.test_core import CoreTest
+from jaseci.utils.test_core import CoreTest, jac_testcase
 
 
 class VectorTest(CoreTest):
@@ -6,48 +6,26 @@ class VectorTest(CoreTest):
 
     fixture_src = __file__
 
-    def test_get_centroid(self):
-        ret = self.call(
-            self.mast,
-            ["sentinel_register", {"code": self.load_jac("vector.jac")}],
-        )
-        ret = self.call(self.mast, ["walker_run", {"name": "get_centroid_test"}])
+    @jac_testcase("vector.jac", "get_centroid_test")
+    def test_get_centroid(self, ret):
         ret = [ret["report"][0][0], round(ret["report"][0][1], 3)]
         self.assertEqual(ret, [[1.0, 2.0, 3.5], 0.998])
 
-    def test_softmax(self):
-        ret = self.call(
-            self.mast,
-            ["sentinel_register", {"code": self.load_jac("vector.jac")}],
-        )
-        ret = self.call(self.mast, ["walker_run", {"name": "softmax_test"}])
+    @jac_testcase("vector.jac", "softmax_test")
+    def test_softmax(self, ret):
         ret = [round(num, 2) for num in ret["report"][0]]
         self.assertEqual(ret, [0.09, 0.24, 0.67])
 
-    def test_dot_product(self):
-        ret = self.call(
-            self.mast,
-            ["sentinel_register", {"code": self.load_jac("vector.jac")}],
-        )
-        ret = self.call(self.mast, ["walker_run", {"name": "dot_product_test"}])
+    @jac_testcase("vector.jac", "dot_product_test")
+    def test_dot_product(self, ret):
         self.assertEqual(ret["report"][0], 17)
 
-    def test_cosine_similarity_single(self):
-        ret = self.call(
-            self.mast,
-            ["sentinel_register", {"code": self.load_jac("vector.jac")}],
-        )
-        ret = self.call(self.mast, ["walker_run", {"name": "cosine_similarity_test"}])
+    @jac_testcase("vector.jac", "cosine_similarity_test")
+    def test_cosine_similarity_single(self, ret):
         ret = round(ret["report"][0], 3)
         self.assertEqual(ret, 0.991)
 
-    def test_cosine_similarity_batch(self):
-        ret = self.call(
-            self.mast,
-            ["sentinel_register", {"code": self.load_jac("vector.jac")}],
-        )
-        ret = self.call(
-            self.mast, ["walker_run", {"name": "cosine_similarity_batch_test"}]
-        )
+    @jac_testcase("vector.jac", "cosine_similarity_batch_test")
+    def test_cosine_similarity_batch(self, ret):
         ret = [round(num, 2) for num in ret["report"][0]]
         self.assertEqual(ret, [0.99, 1.0])
