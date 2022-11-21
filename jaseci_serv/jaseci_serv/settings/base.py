4973e0a115f5b357b8be8a0e7f9aac65e0636baa
Shyam Pathak <pathakshyam101@gmail.com>
2022-11-21 12:15:45 +0530

    removed duplicate entries of static files

--- jaseci_serv/jaseci_serv/settings/base.py
+++ jaseci_serv/jaseci_serv/settings/base.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
-    "django.contrib.staticfiles",
     "rest_framework",
     "knox",
     "django_rest_passwordreset",
