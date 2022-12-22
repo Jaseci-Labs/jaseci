c95a81085175368ae0d0c7c9f24e13c99f863726
Tharuka Kasthuriarachchi <tharuka.ckasthuri@gmail.com>
2022-12-22 11:50:16 +0530

    fixes

--- jaseci_ai_kit/setup.py
+++ jaseci_ai_kit/setup.py
@@ -38,6 +38,7 @@ setup(
         "unidecode==1.3.6",
         "soundfile<=0.11.0",
         "speechbrain==0.5.13",
+        "pydub==0.25.1",
     ],
     package_data={
         "": ["*.json", "*.cfg", "VERSION", "*.yaml"],
