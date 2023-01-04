b8f99b83a1fc54538dc68ffc60b233c5ab7c7c29
AshishMahendra <ashishmahendra@live.com>
2023-01-04 19:50:59 +0530

    fix linting

--- jaseci_ai_kit/jaseci_ai_kit/modules/bi_ner/model/metric.py
+++ jaseci_ai_kit/jaseci_ai_kit/modules/bi_ner/model/metric.py
@@ -1,19 +1,16 @@
 from abc import abstractmethod, ABCMeta
 
-import torch
 from torch import Tensor
-from torch.nn import Parameter, CosineSimilarity, Module
+from torch.nn import CosineSimilarity, Module
 
 
 class Metric(Module, metaclass=ABCMeta):
-
     @abstractmethod
     def forward(self, x: Tensor, y: Tensor) -> Tensor:
         pass
 
 
 class CosSimilarity(Module):
-
     def __init__(self, scale: float):
         super().__init__()
         self._scale = scale
