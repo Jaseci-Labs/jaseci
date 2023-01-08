f6cdd339bbe689a83f0f8147af4e7848a50c064d
AshishMahendra <ashishmahendra@live.com>
2023-01-08 21:46:08 +0530

    created ZS classifier module

---
+++ jaseci_ai_kit/jaseci_ai_kit/modules/zs_classifier/tests/test_zs_classifier.py
@@ -0,0 +1,27 @@
+from jaseci.utils.test_core import CoreTest, jac_testcase
+from jaseci.actions.live_actions import load_module_actions, unload_module
+import pytest
+
+
+class ZS_Classifier_Test(CoreTest):
+    fixture_src = __file__
+    """
+    Test Class for ZS_Classifier Module to test the functionality of api's
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        super(ZS_Classifier_Test, cls).setUpClass()
+        ret = load_module_actions("jaseci_ai_kit.zs_classifier")
+        assert ret is True
+
+    @pytest.mark.order(1)
+    @jac_testcase("zs_classifier.jac", "test_zs_classify")
+    def test_zs_classify(self, ret):
+        self.assertEqual(ret["success"], True)
+
+    @classmethod
+    def tearDownClass(cls):
+        super(ZS_Classifier_Test, cls).tearDownClass()
+        ret = unload_module("jaseci_ai_kit.modules.zs_classifier.zs_classifier")
+        assert ret is True
---
+++ jaseci_ai_kit/jaseci_ai_kit/modules/zs_classifier/zs_classifier.py
@@ -0,0 +1,54 @@
+from typing import List, Union
+from fastapi import HTTPException
+from jaseci.actions.live_actions import jaseci_action
+import torch
+from flair.models import TARSClassifier
+from flair.data import Sentence
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+def init_model(reload=False):
+    """create a classifier for ZS classification"""
+
+    global classifier
+    model_name = "tars-base"
+    classifier = TARSClassifier.load(model_name)
+    print(f"loaded mode : [{model_name}]")
+
+
+# defining the api for entity detection
+@jaseci_action(act_group=["zs_classifier"], allow_remote=True)
+def classify(text: Union[str, List[str]], classes: List[str]):
+    """
+    API for detectiing provided entity in text
+    """
+    response_data_format = []
+    try:
+        if isinstance(text, list):
+            for sent_text in text:
+                resp_data = {}
+                sent = Sentence(sent_text)
+                # predicting class for the text
+                classifier.predict_zero_shot(sent, classes)
+                pred_output = sent.to_dict()
+                resp_data[pred_output["text"]] = pred_output["all labels"]
+                response_data_format.append(resp_data)
+        else:
+            sent = Sentence(text)
+            classifier.predict_zero_shot(sent, classes)
+            pred_output = sent.to_dict()
+            resp_data = {}
+            resp_data[pred_output["text"]] = pred_output["all labels"]
+            response_data_format.append(resp_data)
+        return response_data_format
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(f"Exception :{e}"))
+
+
+if __name__ == "__main__":
+    from jaseci.actions.remote_actions import launch_server
+
+    # initialize the classifier
+    init_model()
+    launch_server(port=8000)
