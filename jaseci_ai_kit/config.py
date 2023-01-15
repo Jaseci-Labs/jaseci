600e4c769c80bfff8ed3ef23622a0a33b9fc7cb3
chandralegend <irugalbandarachandra@gmail.com>
2023-01-15 19:32:05 +0000

    cleanup

--- jaseci_ai_kit/config.py
+++ jaseci_ai_kit/config.py
@@ -1,16 +1,14 @@
-from .modules.test_module.action_config import TEST_MODULE_ACTION_CONFIG
-from .modules.cl_summer.action_config import CL_SUMMER_ACTION_CONFIG
-from .modules.encoders.action_config import BI_ENC_ACTION_CONFIG
-from .modules.ent_ext.action_config import ENT_EXT_ACTION_CONFIG
-from .modules.fast_enc.action_config import FAST_ENC_ACTION_CONFIG
-from .modules.pdf_ext.action_config import PDF_EXT_ACTION_CONFIG
-from .modules.text_seg.action_config import TEXT_SEG_ACTION_CONFIG
-from .modules.tfm_ner.action_config import TFM_NER_ACTION_CONFIG
-from .modules.use_enc.action_config import USE_ENC_ACTION_CONFIG
-from .modules.use_qa.action_config import USE_QA_ACTION_CONFIG
+from .jac_nlp.jac_nlp.cl_summer.action_config import CL_SUMMER_ACTION_CONFIG
+from .jac_nlp.jac_nlp.bi_enc.action_config import BI_ENC_ACTION_CONFIG
+from .jac_nlp.jac_nlp.ent_ext.action_config import ENT_EXT_ACTION_CONFIG
+from .jac_nlp.jac_nlp.fast_enc.action_config import FAST_ENC_ACTION_CONFIG
+from .jac_misc.jac_misc.pdf_ext.action_config import PDF_EXT_ACTION_CONFIG
+from .jac_nlp.jac_nlp.text_seg.action_config import TEXT_SEG_ACTION_CONFIG
+from .jac_nlp.jac_nlp.tfm_ner.action_config import TFM_NER_ACTION_CONFIG
+from .jac_nlp.jac_nlp.use_enc.action_config import USE_ENC_ACTION_CONFIG
+from .jac_nlp.jac_nlp.use_qa.action_config import USE_QA_ACTION_CONFIG
 
 ACTION_CONFIGS = {
-    "test_module": TEST_MODULE_ACTION_CONFIG,
     "cl_summer": CL_SUMMER_ACTION_CONFIG,
     "bi_enc": BI_ENC_ACTION_CONFIG,
     "ent_ext": ENT_EXT_ACTION_CONFIG,
