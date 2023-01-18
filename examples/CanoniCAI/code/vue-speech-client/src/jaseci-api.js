cfcffb8f4c0a13f136066ce3fdce6e1c890ad070
Matt Thurling <mthurling@icloud.com>
2023-01-18 17:49:42 +0000

    Speech Client Draft

---
+++ examples/CanoniCAI/code/vue-speech-client/src/jaseci-api.js
@@ -0,0 +1,30 @@
+// NOTE: Change this URL to your Jaseci server URL:
+const apiUrl = 'http://localhost:8000'
+// NOTE: Change the token to your authenticated token:
+const apiToken = '9427c0ef2ba6a09c5953c9b092f594d5f8b9569eac2174451aba9fed14fb3cd6'
+
+export default {
+  methods: {
+    async jacTalk(request) {
+      let data = {
+        ctx: { "request": request },
+        name: "talk"
+      }
+      let result = await fetch(apiUrl + '/js/walker_run', {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'token ' + apiToken
+      },
+      body: JSON.stringify(data),
+    })
+    result = await result.json();
+
+    const answer = result.report[0];
+      
+    return answer
+    },
+  }
+}
+
