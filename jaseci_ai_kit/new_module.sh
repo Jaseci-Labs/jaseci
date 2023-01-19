76a9cedbfe6f70abeb16e0f38d0e58a7ae88e16d
chandralegend <irugalbandarachandra@gmail.com>
2023-01-19 19:18:34 +0000

    init jac_vision

--- jaseci_ai_kit/new_module.sh
+++ jaseci_ai_kit/new_module.sh
@@ -13,19 +13,19 @@ if [ -z "$MODULE_NAME" ]; then
     exit 1
 fi
 
-if [ -d "$MODULE_TYPE/$MODULE_NAME" ]; then
+if [ -d "$MODULE_TYPE/$MODULE_TYPE/$MODULE_NAME" ]; then
     echo "Module $MODULE_NAME already exists"
     exit 1
 fi
 
-mkdir $MODULE_TYPE/$MODULE_NAME
-echo "from .$MODULE_NAME import * # noqa" >> $MODULE_TYPE/$MODULE_NAME/__init__.py
-echo >> $MODULE_TYPE/$MODULE_NAME/$MODULE_NAME.py
-echo >> $MODULE_TYPE/$MODULE_NAME/.gitignore
-echo >> $MODULE_TYPE/$MODULE_NAME/README.md
-echo >> $MODULE_TYPE/$MODULE_NAME/requirements.txt
+mkdir $MODULE_TYPE/$MODULE_TYPE/$MODULE_NAME
+echo "from .$MODULE_NAME import * # noqa" >> $MODULE_TYPE/$MODULE_TYPE/$MODULE_NAME/__init__.py
+echo >> $MODULE_TYPE/$MODULE_TYPE/$MODULE_NAME/$MODULE_NAME.py
+echo >> $MODULE_TYPE/$MODULE_TYPE/$MODULE_NAME/.gitignore
+echo >> $MODULE_TYPE/$MODULE_TYPE/$MODULE_NAME/README.md
+echo >> $MODULE_TYPE/$MODULE_TYPE/$MODULE_NAME/requirements.txt
 
-mkdir $MODULE_TYPE/$MODULE_NAME/tests
-echo >> $MODULE_TYPE/$MODULE_NAME/tests/test_$MODULE_NAME.py
-mkdir $MODULE_TYPE/$MODULE_NAME/tests/fixtures
-echo >> $MODULE_TYPE/$MODULE_NAME/tests/fixtures/$MODULE_NAME.jac
+mkdir $MODULE_TYPE/$MODULE_TYPE/$MODULE_NAME/tests
+echo >> $MODULE_TYPE/$MODULE_TYPE/$MODULE_NAME/tests/test_$MODULE_NAME.py
+mkdir $MODULE_TYPE/$MODULE_TYPE/$MODULE_NAME/tests/fixtures
+echo >> $MODULE_TYPE/$MODULE_TYPE/$MODULE_NAME/tests/fixtures/$MODULE_NAME.jac
