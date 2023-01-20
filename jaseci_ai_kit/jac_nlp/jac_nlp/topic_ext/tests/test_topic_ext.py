c2fb8e115f1e171bc5e089a1439f4c1509591c28
Tharuka Kasthuriarachchi <tharuka.ckasthuri@gmail.com>
2023-01-20 19:06:27 +0530

    added headline generation feature in topic modeling

--- jaseci_ai_kit/jac_nlp/jac_nlp/topic_ext/tests/test_topic_ext.py
+++ jaseci_ai_kit/jac_nlp/jac_nlp/topic_ext/tests/test_topic_ext.py
@@ -18,6 +18,11 @@ class TextTopicExtModule(CoreTest):
     def test_topic_ext(self, ret):
         self.assertEqual(ret["success"], True)
 
+    @pytest.mark.order(1)
+    @jac_testcase("topic_ext.jac", "test_headline_gen")
+    def test_headline_gen(self, ret):
+        self.assertEqual(ret["success"], True)
+
     @classmethod
     def tearDownClass(cls):
         super(TextTopicExtModule, cls).tearDownClass()
