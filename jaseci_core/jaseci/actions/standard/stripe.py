e3c5bb59246fee5e95a08853000f1ac755c3d908
Alexie (Boyong) Madolid <maex@tuta.io>
2023-01-23 16:25:34 +0800

    [STRIPE-ACTION]: Refactor and Cleanup

--- jaseci_core/jaseci/actions/standard/stripe.py
+++ jaseci_core/jaseci/actions/standard/stripe.py
@@ -1,507 +1,314 @@
 """Built in actions for Jaseci"""
 import stripe
-from fastapi import HTTPException
 from datetime import datetime
 from jaseci.actions.live_actions import jaseci_action
 
 
-def set_api_key(meta):
-    api_key = meta["h"].get_glob("STRIPE_API_KEY")
-
-    if not api_key:
-        raise Exception(
-            {
-                "message": "Stripe is not yet configured. Please set a valid stripe key.",
-                "success": False,
-            }
-        )
-
-    stripe.api_key = api_key
-
-
 @jaseci_action()
-def create_product(
-    name: str, description: str, api_key: str, mock_api: bool = False, meta: dict = {}
-):
+def create_product(api_key: str, name: str, description: str, metadata: dict = {}):
     """create product"""
-    # set_api_key(meta)
-    api_key = api_key or meta["h"].get_glob("STRIPE_API_KEY")
-
-    if mock_api:
-        return {"success": True}
-
-    try:
-        return stripe.Product.create(
-            api_key=api_key, name=name, description=description
-        )
-    except Exception as e:
-        raise Exception(str(e))
+    return stripe.Product.create(
+        api_key=api_key, name=name, description=description, metadata=metadata
+    )
 
 
 @jaseci_action()
 def create_product_price(
+    api_key: str,
     productId: str,
     amount: int,
     currency: str,
     interval: str,
-    api_key: str,
-    mock_api: bool = False,
-    meta: dict = {},
+    metadata: dict = {},
 ):
     """modify product price"""
-    # set_api_key(meta)
-    api_key = api_key or meta["h"].get_glob("STRIPE_API_KEY")
 
-    if mock_api:
-        return {"success": True}
-
-    try:
-        return stripe.Price.create(
-            product=productId,
-            unit_amount=amount,
-            currency=currency,
-            recurring={"interval": interval},
-        )
-    except Exception as e:
-        raise Exception(str(e))
+    return stripe.Price.create(
+        api_key=api_key,
+        product=productId,
+        unit_amount=amount,
+        currency=currency,
+        recurring={"interval": interval},
+        metadata=metadata,
+    )
 
 
 @jaseci_action()
-def product_list(detailed: bool, mock_api: bool = False, meta: dict = {}):
+def product_list(api_key: str, detailed: bool):
     """retrieve all producs"""
-    set_api_key(meta)
 
-    if mock_api:
-        return {"success": True}
-
-    if not detailed:
-        try:
-            return stripe.Product.list(active=True)
-        except Exception as e:
-            raise Exception(str(e))
-    else:  # retrieve product price
-        try:
-            return stripe.Product.list()
-        except Exception as e:
-            raise Exception(str(e))
+    return stripe.Product.list(api_key=api_key, **{"active": True} if detailed else {})
 
 
 @jaseci_action()
 def create_customer(
+    api_key: str,
     email: str,
     name: str,
-    metadata: dict or None,
-    address: dict or None,
-    payment_method_id: str or None,
-    api_key: str,
-    mock_api: bool = False,
-    meta: dict = {},
+    address: dict,
+    payment_method_id: str,
+    metadata: dict = {},
 ):
     """create customer"""
-    # set_api_key(meta)
-    api_key = api_key or meta["h"].get_glob("STRIPE_API_KEY")
 
-    if mock_api:
-        return {"success": True}
-
-    try:
-        return stripe.Customer.create(
-            api_key=api_key,
-            email=email,
-            name=name,
-            metadata=metadata,
-            address=address,
-            payment_method=payment_method_id,
-            invoice_settings={"default_payment_method": payment_method_id},
-        )
-    except Exception as e:
-        raise Exception(str(e))
+    return stripe.Customer.create(
+        api_key=api_key,
+        email=email,
+        name=name,
+        address=address,
+        payment_method=payment_method_id,
+        invoice_settings={"default_payment_method": payment_method_id},
+        metadata=metadata,
+    )
 
 
 @jaseci_action()
-def get_customer(customer_id: str, mock_api: bool = False, meta: dict = {}):
+def get_customer(api_key: str, customer_id: str):
     """retrieve customer information"""
-    set_api_key(meta)
 
-    if mock_api:
-        return {"success": True}
-
-    try:
-        return stripe.Customer.retrieve(customer_id)
-    except Exception as e:
-        raise Exception(str(e))
+    return stripe.Customer.retrieve(api_key=api_key, id=customer_id)
 
 
 @jaseci_action()
-def attach_payment_method(
-    payment_method_id: str, customer_id: str, mock_api: bool = False, meta: dict = {}
-):
+def attach_payment_method(api_key: str, payment_method_id: str, customer_id: str):
     """attach payment method to customer"""
-    set_api_key(meta)
 
-    if mock_api:
-        return {"success": True}
+    paymentMethods = get_payment_methods(api_key, customer_id)
 
-    try:
-        paymentMethods = get_payment_methods(customer_id, meta)
+    paymentMethod = stripe.PaymentMethod.attach(
+        api_key=api_key, payment_method=payment_method_id, customer=customer_id
+    )
 
-        paymentMethod = stripe.PaymentMethod.attach(
-            payment_method_id, customer=customer_id
-        )
+    is_default = True
+    if paymentMethods.get("data"):
+        update_default_payment_method(api_key, customer_id, payment_method_id)
+        is_default = False
 
-        if len(paymentMethods.data) == 0:
-            update_default_payment_method(customer_id, payment_method_id, meta)
+    paymentMethod.is_default = is_default
 
-        paymentMethod.is_default = len(paymentMethods.data) == 0
-
-        return paymentMethod
-
-    except Exception as e:
-        raise Exception(str(e))
+    return paymentMethod
 
 
 @jaseci_action()
-def delete_payment_method(
-    payment_method_id: str, mock_api: bool = False, meta: dict = {}
-):
+def delete_payment_method(api_key: str, payment_method_id: str):
     """detach payment method from customer"""
-    set_api_key(meta)
 
-    if mock_api:
-        return {"success": True}
-
-    try:
-        return stripe.PaymentMethod.detach(payment_method_id)
-    except Exception as e:
-        raise Exception(str(e))
+    return stripe.PaymentMethod.detach(
+        api_key=api_key, payment_method=payment_method_id
+    )
 
 
 @jaseci_action()
-def get_payment_methods(customer_id: str, mock_api: bool = False, meta: dict = {}):
+def get_payment_methods(api_key: str, customer_id: str):
     """get customer list of payment methods"""
-    set_api_key(meta)
 
-    if mock_api:
-        return {"success": True}
-
-    try:
-        return stripe.PaymentMethod.list(
-            customer=customer_id,
-            type="card",
-        )
-    except Exception as e:
-        raise Exception(str(e))
+    return stripe.PaymentMethod.list(
+        api_key=api_key,
+        customer=customer_id,
+        type="card",
+    )
 
 
 @jaseci_action()
 def update_default_payment_method(
-    customer_id: str, payment_method_id: str, mock_api: bool = False, meta: dict = {}
+    api_key: str, customer_id: str, payment_method_id: str
 ):
     """update default payment method of customer"""
-    set_api_key(meta)
 
-    if mock_api:
-        return {"success": True}
-
-    try:
-        return stripe.Customer.modify(
-            customer_id,
-            invoice_settings={"default_payment_method": payment_method_id},
-        )
-    except Exception as e:
-        raise Exception(str(e))
+    return stripe.Customer.modify(
+        api_key=api_key,
+        customer=customer_id,
+        invoice_settings={"default_payment_method": payment_method_id},
+    )
 
 
 @jaseci_action()
-def create_invoice(customer_id: str, mock_api: bool = False, meta: dict = {}):
+def create_invoice(api_key: str, customer_id: str, metadata: dict = {}):
     """create customer invoice"""
-    set_api_key(meta)
 
-    if mock_api:
-        return {"success": True}
-
-    try:
-        return stripe.Invoice.create(customer=customer_id)
-    except Exception as e:
-        raise Exception(str(e))
+    return stripe.Invoice.create(
+        api_key=api_key, customer=customer_id, metadata=metadata
+    )
 
 
 @jaseci_action()
 def get_invoice_list(
+    api_key: str,
     customer_id: str,
     subscription_id: str,
     starting_after: str = "",
     limit: int = 10,
-    mock_api: bool = False,
-    meta: dict = {},
 ):
     """retrieve customer list of invoices"""
-    set_api_key(meta)
 
-    if mock_api:
-        return {"success": True}
-
-    try:
-        if starting_after != "":
-            invoices = stripe.Invoice.list(
-                customer=customer_id,
-                limit=limit,
-                starting_after=starting_after,
-                subscription=subscription_id,
-            )
-        else:
-            invoices = stripe.Invoice.list(
-                customer=customer_id, limit=limit, subscription=subscription_id
-            )
-        return invoices
-    except Exception as e:
-        raise Exception(str(e))
+    return stripe.Invoice.list(
+        api_key=api_key,
+        customer=customer_id,
+        limit=limit,
+        subscription=subscription_id,
+        **{"starting_after": starting_after} if starting_after else {}
+    )
 
 
 @jaseci_action()
 def get_payment_intents(
-    customer_id: str,
-    starting_after: str = "",
-    limit: int = 10,
-    mock_api: bool = False,
-    meta: dict = {},
+    api_key: str, customer_id: str, starting_after: str = "", limit: int = 10
 ):
     """get customer payment intents"""
-    set_api_key(meta)
 
-    if mock_api:
-        return {"success": True}
-
-    try:
-        if starting_after != "":
-            payment_intents = stripe.PaymentIntent.list(
-                customer=customer_id,
-                limit=limit,
-                starting_after=starting_after,
-            )
-        else:
-            payment_intents = stripe.PaymentIntent.list(
-                customer=customer_id, limit=limit
-            )
-        return payment_intents
-    except Exception as e:
-        raise Exception(str(e))
+    return stripe.PaymentIntent.list(
+        api_key=api_key,
+        customer=customer_id,
+        limit=limit,
+        **{"starting_after": starting_after} if starting_after else {}
+    )
 
 
 @jaseci_action()
 def create_payment_intents(
+    api_key: str,
     customer_id: str,
     amount: int,
     currency: str,
     payment_method_types: str = "card",
-    mock_api: bool = False,
-    meta: dict = {},
+    metadata: dict = {},
 ):
     """Create customer payment"""
-    set_api_key(meta)
 
-    if mock_api:
-        return {"success": True}
-
-    try:
-        return stripe.PaymentIntent.create(
-            customer=customer_id,
-            amount=amount,
-            currency=currency,
-            payment_method_types=[payment_method_types],
-        )
-    except Exception as e:
-        raise Exception(str(e))
+    return stripe.PaymentIntent.create(
+        api_key=api_key,
+        customer=customer_id,
+        amount=amount,
+        currency=currency,
+        payment_method_types=[payment_method_types],
+        metadata=metadata,
+    )
 
 
 @jaseci_action()
-def get_customer_subscription(
-    customer_id: str, mock_api: bool = False, meta: dict = {}
-):
+def get_customer_subscription(api_key: str, customer_id: str):
     """retrieve customer subcription list"""
-    set_api_key(meta)
 
-    if mock_api:
-        return {"success": True}
+    subscription = stripe.Subscription.list(api_key=api_key, customer=customer_id)
 
-    try:
-        subscription = stripe.Subscription.list(customer=customer_id)
+    if not subscription.data:
+        return {"status": "inactive", "message": "Customer has no subscription"}
 
-        if len(subscription.data) == 0:
-            return {"status": "inactive", "message": "Customer has no subscription"}
-
-        return subscription
-    except Exception as e:
-        raise Exception(str(e))
+    return subscription
 
 
 @jaseci_action()
 def create_payment_method(
-    card_type: str, card: dict, mock_api: bool = False, meta: dict = {}
+    api_key: str, card_type: str, card: dict, metadata: dict = {}
 ):
     """create payment method"""
-    set_api_key(meta)
 
-    if mock_api:
-        return {"success": True}
-
-    try:
-        return stripe.PaymentMethod.create(type=card_type, card=card)
-
-    except Exception as e:
-        raise Exception(str(e))
+    return stripe.PaymentMethod.create(
+        api_key=api_key, type=card_type, card=card, metadata=metadata
+    )
 
 
 @jaseci_action()
 def create_trial_subscription(
+    api_key: str,
     payment_method_id: str,
     customer_id: str,
     items: list,
     trial_period_days: int = 14,
     expand: list = [],
     metadata: dict = {},
-    mock_api: bool = False,
-    meta: dict = {},
 ):
     """create customer trial subscription"""
-    set_api_key(meta)
 
-    if mock_api:
-        return {"success": True}
+    # attach payment method to customer
+    attach_payment_method(api_key, payment_method_id, customer_id)
 
-    try:
-        # attach payment method to customer
-        attach_payment_method(payment_method_id, customer_id, False, meta)
+    # set card to default payment method
+    update_default_payment_method(api_key, customer_id, payment_method_id)
 
-        # set card to default payment method
-        update_default_payment_method(customer_id, payment_method_id, False, meta)
-
-        return stripe.Subscription.create(
-            customer=customer_id,
-            items=items,
-            trial_period_days=trial_period_days,
-            expand=expand,
-            metadata=metadata,
-        )
-    except Exception as e:
-        raise Exception(str(e))
+    return stripe.Subscription.create(
+        api_key=api_key,
+        customer=customer_id,
+        items=items,
+        trial_period_days=trial_period_days,
+        expand=expand,
+        metadata=metadata,
+    )
 
 
 @jaseci_action()
 def create_subscription(
+    api_key: str,
     payment_method_id: str,
     items: list,
     customer_id: str,
-    mock_api: bool = False,
-    meta: dict = {},
+    metadata: dict = {},
 ):
     """create customer subscription"""
-    set_api_key(meta)
 
-    if mock_api:
-        return {"success": True}
+    # attach payment method to customer
+    attach_payment_method(api_key, payment_method_id, customer_id)
 
-    try:
-        # attach payment method to customer
-        attach_payment_method(payment_method_id, customer_id, False, meta)
+    # set card to default payment method
+    update_default_payment_method(api_key, customer_id, payment_method_id)
 
-        # set card to default payment method
-        update_default_payment_method(customer_id, payment_method_id, False, meta)
-
-        return stripe.Subscription.create(
-            customer=customer_id,
-            items=items,
-        )
-    except Exception as e:
-        raise Exception(str(e))
+    return stripe.Subscription.create(
+        api_key=api_key, customer=customer_id, items=items, metadata=metadata
+    )
 
 
 @jaseci_action()
-def cancel_subscription(
-    subscription_id: str, metadata: dict = {}, mock_api: bool = False, meta: dict = {}
-):
+def cancel_subscription(api_key: str, subscription_id: str, metadata: dict = {}):
     """cancel customer subscription"""
-    set_api_key(meta)
 
-    if mock_api:
-        return {"success": True}
-
-    try:
-        return stripe.Subscription.delete(subscription_id)
-    except Exception as e:
-        raise Exception(str(e))
+    return stripe.Subscription.delete(api_key=api_key, sid=subscription_id)
 
 
 @jaseci_action()
-def get_subscription(subscription_id: str, mock_api: bool = False, meta: dict = {}):
+def get_subscription(api_key: str, subscription_id: str):
     """retrieve customer subcription details"""
-    set_api_key(meta)
 
-    if mock_api:
-        return {"success": True}
-
-    try:
-        return stripe.Subscription.retrieve(subscription_id)
-    except Exception as e:
-        raise Exception(str(e))
+    return stripe.Subscription.retrieve(api_key=api_key, id=subscription_id)
 
 
 @jaseci_action()
 def update_subscription(
+    api_key: str,
     subscription_id: str,
     subscription_item_id: str,
     price_id: str,
-    mock_api: bool = False,
-    meta: dict = {},
+    metadata: dict = {},
 ):
     """update subcription details"""
-    set_api_key(meta)
 
-    if mock_api:
-        return {"success": True}
-
-    try:
-        return stripe.Subscription.modify(
-            subscription_id,
-            cancel_at_period_end=False,
-            items=[
-                {
-                    "id": subscription_item_id,
-                    "price": price_id,
-                },
-            ],
-        )
-    except Exception as e:
-        raise Exception(str(e))
+    return stripe.Subscription.modify(
+        api_key=api_key,
+        sid=subscription_id,
+        cancel_at_period_end=False,
+        items=[
+            {
+                "id": subscription_item_id,
+                "price": price_id,
+            },
+        ],
+        metadata=metadata,
+    )
 
 
 @jaseci_action()
-def get_invoice(invoice_id: str, mock_api: bool = False, meta: dict = {}):
+def get_invoice(api_key: str, invoice_id: str):
     """get invoice information"""
-    set_api_key(meta)
 
-    if mock_api:
-        return {"success": True}
-
-    try:
-        return stripe.Invoice.retrieve(invoice_id)
-    except Exception as e:
-        raise Exception(str(e))
+    return stripe.Invoice.retrieve(api_key=api_key, id=invoice_id)
 
 
 @jaseci_action()
-def create_usage_report(
-    subscription_item_id: str, quantity: int, mock_api: bool = False, meta: dict = {}
-):
+def create_usage_report(api_key: str, subscription_item_id: str, quantity: int):
     """Create usage record"""
-    set_api_key(meta)
 
-    if mock_api:
-        return {"success": True}
-
-    try:
-        return stripe.SubscriptionItem.create_usage_record(
-            subscription_item_id, quantity=quantity, timestamp=datetime.now()
-        )
-    except Exception as e:
-        raise Exception(str(e))
+    return stripe.SubscriptionItem.create_usage_record(
+        api_key=api_key,
+        id=subscription_item_id,
+        quantity=quantity,
+        timestamp=datetime.now(),
+    )
