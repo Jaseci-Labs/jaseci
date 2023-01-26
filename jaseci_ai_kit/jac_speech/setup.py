f3b18a344fc2f035f230604832539a1543b7f49a
Chandra Irugalbandara <52673571+chandralegend@users.noreply.github.com>
2023-01-26 22:35:46 +0530

    Platform Specific Requirements (#850)
    * Added test implementation for jac_speech
    
    * removed weights

--- jaseci_ai_kit/jac_speech/setup.py
+++ jaseci_ai_kit/jac_speech/setup.py
@@ -1,20 +1,58 @@
 from setuptools import setup, find_packages
-from os.path import join
+from setuptools.command.install import install
+import atexit
+import sys
+import platform
+import os
+
 
 MODULES = ["stt", "tts", "vc_tts"]
+INSTALLABLE_MODULES = []
+
+
+def _post_install():
+    UNSUPPORTED_MODULES = [
+        module for module in MODULES if module not in INSTALLABLE_MODULES
+    ]
+    if UNSUPPORTED_MODULES:
+        print(
+            "\033[93m"
+            + f"{UNSUPPORTED_MODULES} modules are not supported on your system. "
+            + "\033[0m"
+        )
+
+
+class new_install(install):
+    def __init__(self, *args, **kwargs):
+        super(new_install, self).__init__(*args, **kwargs)
+        atexit.register(_post_install)
 
 
 def get_ver():
-    with open(join("./jac_speech", "VERSION")) as version_file:
+    with open(os.path.join("./jac_speech", "VERSION")) as version_file:
         return version_file.read().strip()
+
+
+def get_os():
+    if sys.platform == "darwin" and platform.machine() == "arm64":
+        return ".darwin"
+    return ""
 
 
 def get_extras_requires():
     extras_requires = {"all": []}
+    platform = get_os()
     for module in MODULES:
-        with open(join("./jac_speech", module, "requirements.txt")) as req_file:
+        if not os.path.exists(
+            os.path.join("jac_speech", module, f"requirements{platform}.txt")
+        ):
+            continue
+        with open(
+            os.path.join("jac_speech", module, f"requirements{platform}.txt")
+        ) as req_file:
             extras_requires[module] = req_file.read().splitlines()
             extras_requires["all"].extend(extras_requires[module])
+        INSTALLABLE_MODULES.append(module)
     return extras_requires
 
 
@@ -30,4 +68,5 @@ setup(
     author="Jason Mars",
     author_email="jason@jaseci.org",
     url="https://github.com/Jaseci-Labs/jaseci",
+    cmdclass={"install": new_install},
 )
