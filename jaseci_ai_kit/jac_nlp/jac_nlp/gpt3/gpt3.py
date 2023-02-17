136931ffbc207f3522b9c75ad35b56171e6efd5e
chandralegend <irugalbandarachandra@gmail.com>
2023-02-17 13:02:05 +0000

    added set_api_key action

--- jaseci_ai_kit/jac_nlp/jac_nlp/gpt3/gpt3.py
+++ jaseci_ai_kit/jac_nlp/jac_nlp/gpt3/gpt3.py
@@ -4,44 +4,40 @@ from jaseci.actions.live_actions import jaseci_action
 import traceback
 from fastapi import HTTPException
 
-TASKS = {
-    "q&a": {
-        "model": "text-davinci-003",
-        "temperature": 0,
-        "max_tokens": 100,
-        "top_p": 1,
-        "frequency_penalty": 0.0,
-        "presence_penalty": 0.0,
-        "stop": ["\n"],
-    },
-}
-
 
 def setup(api_key: str):
     if api_key:
         openai.api_key = api_key
     else:
-        raise HTTPException(status_code=400, detail="OPENAI_API_KEY not set")
+        raise HTTPException(
+            status_code=400,
+            detail="Please provide an API key through the OPENAI_API_KEY environment variable or the set_api_key action.",
+        )
 
 
 setup(api_key=os.environ.get("OPENAI_API_KEY", None))
 
 
-@jaseci_action(act_group=["gpt2"], allow_remote=True)
-def generate(text: str, task: str = None, args: dict = {}):
-    if task and task in TASKS:
-        default_args = TASKS[task]
-        default_args.update(args)
-    elif task:
-        raise HTTPException(status_code=400, detail=f"task {task} not found")
-    elif args:
-        default_args = args
-    else:
-        raise HTTPException(status_code=400, detail=f"task or args must be provided")
-
+@jaseci_action(act_group=["gpt3"], allow_remote=True)
+def set_api_key(api_key: str):
     try:
-        response = openai.Completion.create(prompt=text, **default_args)
+        setup(api_key)
+    except Exception as e:
+        traceback.print_exc()
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@jaseci_action(act_group=["gpt3"], allow_remote=True)
+def generate(text: str, args: dict = {}):
+    try:
+        response = openai.Completion.create(prompt=text, **args)
         return response
     except Exception as e:
         traceback.print_exc()
         raise HTTPException(status_code=500, detail=str(e))
+
+
+if __name__ == "__main__":
+    from jaseci.actions.remote_actions import launch_server
+
+    launch_server(port=8000)
