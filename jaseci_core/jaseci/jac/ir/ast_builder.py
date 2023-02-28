abac70cea3b456669844b80cb098cad7c5627bdf
Gimel Dick <gimeldick@gmail.com>
2023-02-28 08:38:56 -0400

    allow specifying prediction mode of parser from jac ast builder

--- jaseci_core/jaseci/jac/ir/ast_builder.py
+++ jaseci_core/jaseci/jac/ir/ast_builder.py
@@ -21,10 +21,12 @@ class JacAstBuilder:
         mod_dir="./",
         jac_text=None,
         start_rule="start",
+        prediction_mode=None,
     ):
         self.root = Ast(mod_name)
         self._parse_errors = []
         self._start_rule = start_rule
+        self._prediction_mode = prediction_mode
         self._mod_dir = mod_dir
         self.dependencies = []
         if jac_text:
@@ -38,6 +40,9 @@ class JacAstBuilder:
         stream = CommonTokenStream(lexer)
         errors = JacTreeError(self)
         parser = jacParser(stream)
+        # set a prediction mode based on performance and accuracy needs
+        if self._prediction_mode:
+            parser._interp.predictionMode = self._prediction_mode
         parser.removeErrorListeners()
         parser.addErrorListener(errors)
         tree = getattr(parser, self._start_rule)()
