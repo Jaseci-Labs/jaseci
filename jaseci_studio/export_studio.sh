60bfc9104d7d1ac91d693cc8e5b032d521b3b252
Gimel Dick <46765702+Gim3l@users.noreply.github.com>
2023-03-01 16:29:21 -0400

    add install command to jaseci_serv setup file (#968)
    * add install command to jaseci_serv setup file
    
    * include studio templates in jsserv package
    
    * fix issues with bundling static files

--- jaseci_studio/export_studio.sh
+++ jaseci_studio/export_studio.sh
@@ -1,9 +1,10 @@
+yarn install
 yarn build
 rm -rf sandbox
 rm -rf ../jaseci_serv/templates/studio
 rm -rf ../jaseci_serv/static/studio
-mkdir sandbox sandbox/templates sandbox/static
-mkdir ../jaseci_serv/templates/studio ../jaseci_serv/static/studio
+mkdir -p sandbox/templates sandbox/static
+mkdir -p ../jaseci_serv/templates/studio ../jaseci_serv/static/studio
 # move all files ending with .html from dist folder to sandbox folder
 mv dist/*.html ../jaseci_serv/templates/studio
 mv dist/* ../jaseci_serv/static/studio
