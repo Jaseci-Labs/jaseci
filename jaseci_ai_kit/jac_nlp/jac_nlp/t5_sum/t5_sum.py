f83aed5ca18cd0da4a51a643b38a075e2e71ee48
chandralegend <irugalbandarachandra@gmail.com>
2023-03-03 13:21:49 +0000

    updated docs

--- jaseci_ai_kit/jac_nlp/jac_nlp/t5_sum/t5_sum.py
+++ jaseci_ai_kit/jac_nlp/jac_nlp/t5_sum/t5_sum.py
@@ -18,6 +18,7 @@ def setup(model_name: str = "t5-small", tokenizer_name: str = "t5-small"):
 
 setup()
 
+
 # generates summary based on text
 def t5_generate_sum(text, min_length, max_length):
     preprocess_text = text.strip().replace("\n", "")
