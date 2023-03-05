c96d25c1026300f74d21132ff13d1c7845ee889a
Yiping Kang <ypkang@umich.edu>
2023-03-05 14:23:37 -0500

    Fix linting

--- jaseci_ai_kit/jac_speech/jac_speech/t5_phraser/paraphraser.py
+++ jaseci_ai_kit/jac_speech/jac_speech/t5_phraser/paraphraser.py
@@ -6,12 +6,16 @@ import warnings
 warnings.filterwarnings("ignore")
 parrot = Parrot(model_tag="prithivida/parrot_paraphraser_on_T5", use_gpu=False)
 
+
 @jaseci_action(act_group=["t5_phraser"], allow_remote=True)
-def paraphrase_text(phrases:List):
-    para_phrases={}
+def paraphrase_text(phrases: List):
+    para_phrases = {}
     for phrase in phrases:
-        para_phrases[phrase] = parrot.augment(input_phrase=phrase,max_return_phrases=10,do_diverse=True)
+        para_phrases[phrase] = parrot.augment(
+            input_phrase=phrase, max_return_phrases=10, do_diverse=True
+        )
     return para_phrases
+
 
 if __name__ == "__main__":
     from jaseci.actions.remote_actions import launch_server
