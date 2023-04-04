4e0540c367cf8a286a2f506e5eec6aca6bf3e274
AshishMahendra <am028275@gmail.com>
2023-04-04 03:38:51 +0000

    repository update

--- jaseci_ai_kit/jac_nlp/jac_nlp/action_configs/bi_enc_action_config.py
+++ jaseci_ai_kit/jac_nlp/jac_nlp/action_configs/bi_enc_action_config.py
@@ -56,7 +56,7 @@ BI_ENC_ACTION_CONFIG = {
                         "containers": [
                             {
                                 "name": "bi-enc",
-                                "image": "jaseci/jac-experiment:1.4.0.12",
+                                "image": "jaseci/jaseci-experiment:1.4.0.12",
                                 "command": ["bash", "-c", "source script/prod_up"],
                                 "ports": [{"containerPort": 80, "protocol": "TCP"}],
                                 "resources": {
