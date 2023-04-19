853590f0deb7f81465d60f167a28f9ccf3f72c66
Yiping Kang <ypkang@umich.edu>
2023-04-19 14:54:12 -0400

    Default darkmode for docs

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -94,6 +94,11 @@ const config = {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
       },
+      colorMode: {
+        defaultMode: 'dark',
+        disableSwitch: false,
+        respectPrefersColorScheme: false,
+      }
     }),
 };
 
