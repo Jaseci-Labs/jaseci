3121f60f808ddf60c2223bf0a2179798fb6eef45
Yiping Kang <ypkang@umich.edu>
2023-04-19 14:38:22 -0400

    Changes to get docusuarus to build

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -15,11 +15,7 @@ function HomepageHeader() {
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="docs/getting-started/getting-to-know-jaseci">
-            Getting Started -  5 mins ⏱️
-          </Link>
+          Getting Started -  5 mins ⏱️
         </div>
       </div>
     </header>
