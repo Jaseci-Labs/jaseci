5cb8a4d951cf9d13c3f7468511ea418dd7604ad0
AshishMahendra <am028275@gmail.com>
2023-04-25 06:16:34 +0000

    pybind issue fix

--- jaseci_ai_kit/jac_nlp/setup.py
+++ jaseci_ai_kit/jac_nlp/setup.py
@@ -41,7 +41,12 @@ setup(
     name="jac_nlp",
     version=get_ver(),
     packages=find_packages(include=["jac_nlp", "jac_nlp.*"]),
-    install_requires=["jaseci", "pytest>=7.0.1,<7.1", "pytest-order>=1.0.1,<1.1"],
+    install_requires=[
+        "jaseci",
+        "pytest>=7.0.1,<7.1",
+        "pytest-order>=1.0.1,<1.1",
+        "pybind11>=2.2",
+    ],
     extras_require=get_extras_requires(),
     package_data={
         "": ["*.json", "*.cfg", "VERSION", "*.yaml", "requirements.txt", "*.jac"],
