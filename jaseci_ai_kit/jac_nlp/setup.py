52c89e6ce29e6e988e759753cd02cda658fa8466
AshishMahendra <am028275@gmail.com>
2023-04-25 09:03:33 +0000

    another version

--- jaseci_ai_kit/jac_nlp/setup.py
+++ jaseci_ai_kit/jac_nlp/setup.py
@@ -3,11 +3,7 @@ from os.path import join
 import subprocess
 import sys
 
-try:
-    import pybind11
-except ImportError:
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "pybind11"])
-    import pybind11
+subprocess.check_call([sys.executable, "-m", "pip", "install", "pybind11"])
 
 
 MODULES = [
@@ -30,28 +26,6 @@ MODULES = [
     "sentiment",
     "paraphraser",
 ]
-
-
-class get_pybind_include(object):
-    """Helper class to determine the pybind11 include path
-
-    The purpose of this class is to postpone importing pybind11
-    until it is actually installed, so that the ``get_include()``
-    method can be invoked."""
-
-    def __init__(self, user=False):
-        try:
-            import pybind11
-        except ImportError:
-            if subprocess.call([sys.executable, "-m", "pip", "install", "pybind11"]):
-                raise RuntimeError("pybind11 install failed.")
-
-        self.user = user
-
-    def __str__(self):
-        import pybind11
-
-        return pybind11.get_include(self.user)
 
 
 def get_ver():
