ada3c952fb22f800b3df56c0874d6462e783831c
Chandra Irugalbandara <52673571+chandralegend@users.noreply.github.com>
2023-04-26 21:26:23 +0530

    Open AI Integration (#1090)
    * init openai module
    
    * added openai integration
    
    * added docstrings
    
    * added docs and updated requirements
    
    * linted
    
    * Delete test_openai.py
    
    * Update requirements.txt
    
    ---------
    
    Co-authored-by: Yiping Kang <yipingkang93@gmail.com>

--- jaseci_ai_kit/jac_misc/setup.py
+++ jaseci_ai_kit/jac_misc/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 from os.path import join
 
-MODULES = ["pdf_ext", "translator", "cluster", "ph"]
+MODULES = ["pdf_ext", "translator", "cluster", "ph", "openai"]
 
 
 def get_ver():
