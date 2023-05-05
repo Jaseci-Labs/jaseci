f7e4d4190b21edafa03debbd6843011708e87322
chandralegend <irugalbandarachandra@gmail.com>
2023-05-05 13:20:31 +0000

    added the ability to use your customhiggingface inference point if need

--- jaseci_ai_kit/jac_misc/jac_misc/huggingface/huggingface.py
+++ jaseci_ai_kit/jac_misc/jac_misc/huggingface/huggingface.py
@@ -18,21 +18,28 @@ def setup(api_key: str = os.environ.get("HUGGINGFACE_API_KEY", None)):
 
 
 @jaseci_action(act_group=["hf"], allow_remote=True)
-def query(task: str, model: str = "default", **kwargs):
+def query(
+    task: str = None,
+    model: str = "default",
+    api_url: str = None,
+    api_type: str = None,
+    **kwargs,
+):
     if not os.environ.get("HUGGINGFACE_API_KEY", None):
         logger.error(
             "Huggingface API key not found. Please set the environment variable HUGGINGFACE_API_KEY or pass it as an argument to the setup function using actions call hf.setup"
         )
         return
-    if task not in API_ENDPOINTS or model not in API_ENDPOINTS[task]:
+    if task and (task not in API_ENDPOINTS or model not in API_ENDPOINTS[task]):
         logger.error(
             f"Task {task} or model {model} not found. Please check the documentation for a list of available tasks and models."
         )
         return
 
     HEADERS = {"Authorization": f"Bearer {os.environ['HUGGINGFACE_API_KEY']}"}
-    API_URL = API_ENDPOINTS[task][model]["API_URL"]
-    API_TYPE = API_ENDPOINTS[task][model]["API_TYPE"]
+
+    API_URL = API_ENDPOINTS[task][model]["API_URL"] if not api_url else api_url
+    API_TYPE = API_ENDPOINTS[task][model]["API_TYPE"] if not api_type else api_type
 
     if API_TYPE == "file":
         file = kwargs.pop("file", None)
