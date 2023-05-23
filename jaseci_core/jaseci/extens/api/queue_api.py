478576300a9ea78e58aa3629801cf0fbfcc08d07
Alexie (Boyong) Madolid <maex@tuta.io>
2023-05-23 20:10:27 +0800

    [TASK]: Scheduled Queues APIs

--- jaseci_core/jaseci/extens/api/queue_api.py
+++ jaseci_core/jaseci/extens/api/queue_api.py
@@ -40,3 +40,69 @@ class QueueApi:
             return "Task id is required!"
         else:
             return task.get_by_task_id(task_id, True, timeout)
+
+    @Interface.private_api()
+    def add_scheduled_walker(self, name: str, schedule: dict, body: dict = {}):
+        """
+        temp
+        """
+        task = JsOrc.svc("task", TaskService)
+        if not task.is_running():
+            return "Task hook is not yet initialized!"
+
+        from jaseci.prim.super_master import SuperMaster
+
+        if not isinstance(self, SuperMaster) or not body.get("mst"):
+            body["mst"] = self.jid
+
+        return (
+            "Schedued Walker created successfully!"
+            if task.add_scheduled_queue(task.scheduled_walker, name, schedule, body)
+            else "Django is required to support scheduled walker!"
+        )
+
+    @Interface.admin_api()
+    def add_scheduled_sequence(self, name: str, schedule: dict, body: dict):
+        """
+        temp
+        """
+        task = JsOrc.svc("task", TaskService)
+        if not task.is_running():
+            return "Task hook is not yet initialized!"
+
+        return (
+            "Schedued Sequence created successfully!"
+            if task.add_scheduled_queue(task.scheduled_sequence, name, schedule, body)
+            else "Django is required to support scheduled walker!"
+        )
+
+    @Interface.private_api()
+    def get_scheduled_queues(
+        self, limit: int = 10, offset: int = 0, asc: bool = False, search: str = None
+    ):
+        """
+        temp
+        """
+        task = JsOrc.svc("task", TaskService)
+        if not task.is_running():
+            return "Task hook is not yet initialized!"
+
+        return task.get_scheduled_queues(limit, offset, asc, search, self)
+
+    @Interface.private_api()
+    def delete_scheduled_queue(self, scheduled_queue_id: int):
+        """
+        temp
+        """
+        task = JsOrc.svc("task", TaskService)
+        if not task.is_running():
+            return "Task hook is not yet initialized!"
+
+        result = task.delete_scheduled_queue(scheduled_queue_id, self)
+        if result == None:
+            return "Django is required to support scheduled walker!"
+        return (
+            "Successfully deleted"
+            if result
+            else f"Scheduled Queue with id({scheduled_queue_id}) is not existing!"
+        )
