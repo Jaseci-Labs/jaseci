48051d8f6612ffad47a7b52cd5b667f891f855be
marsninja <jason@mars.ninja>
2023-05-28 08:20:40 -0400

    updates to setup.py

--- setup.py
+++ setup.py
@@ -9,22 +9,26 @@ setup(
     version=VERSION,
     packages=find_packages(include=["jaseci", "jaseci.*"]),
     install_requires=[
-        "click>=8.1.0,<8.2.0",
-        "click-shell>=2.1,<2.2",
         "flake8",
+        "flake8_import_order",
+        "flake8_docstrings",
+        "flake8_comprehensions",
+        "flake8_bugbear",
+        "flake8_annotations",
         "pep8-naming",
         "pytest",
         "pytest-xdist",
         "pytest-cov",
+        "sly",
     ],
     package_data={
         "": ["*.ini"],
     },
     entry_points={
-        "console_scripts": [
-            "jsctl = jaseci.cli_tools.jsctl:jsctl",
-            "jac = jaseci.cli_tools.jsctl:jac",
-        ]
+        # "console_scripts": [
+        #     "jsctl = jaseci.cli_tools.jsctl:jsctl",
+        #     "jac = jaseci.cli_tools.jsctl:jac",
+        # ]
     },
     author="Jason Mars",
     author_email="jason@jaseci.org",
