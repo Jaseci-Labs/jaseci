a6127e79e8d47c515de2cb312014bb513fe97767
marsninja <jason@mars.ninja>
2023-06-01 12:49:43 -0400

    Added more code and tweaks to grammar for transpilation

--- jaseci/core/edge.py
+++ jaseci/core/edge.py
@@ -1,1 +1,10 @@
 """Edge class for Jaseci."""
+from jaseci.core.object import Object
+
+
+class Edge(Object):
+    """Edge class for Jaseci."""
+
+    def __init__(self: "Edge", *args: list, **kwargs: dict) -> None:
+        """Initialize edge."""
+        super().__init__(*args, **kwargs)
--- jaseci/core/object.py
+++ jaseci/core/object.py
@@ -1,1 +1,11 @@
 """Object class for Jaseci."""
+from jaseci.core.element import Element
+
+
+class Object(Element):
+    """Object class for Jaseci."""
+
+    def __init__(self: "Object", *args: list, **kwargs: dict) -> None:
+        """Initialize object."""
+        self.context = {}
+        super().__init__(*args, **kwargs)
