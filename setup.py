e0b862c01c32d91e9ad5c7d3df32e0aaca4c4f34
marsninja <jason@mars.ninja>
2023-06-06 12:28:49 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,18 @@
-"""Jaseci setup file."""
+"""Jaclang setup file."""
 
 from setuptools import find_packages, setup
 
 VERSION = "2.0.0"
 
 setup(
-    name="jaseci",
+    name="jaclang",
     version=VERSION,
-    packages=find_packages(include=["jaseci", "jaseci.*"]),
+    packages=find_packages(include=["jaclang", "jaclang.*"]),
     install_requires=[],
     package_data={
         "": ["*.ini"],
     },
-    entry_points={
-        # "console_scripts": [
-        #     "jsctl = jaseci.cli_tools.jsctl:jsctl",
-        #     "jac = jaseci.cli_tools.jsctl:jac",
-        # ]
-    },
+    entry_points={},
     author="Jason Mars",
     author_email="jason@jaseci.org",
     url="https://github.com/Jaseci-Labs/jaseci2",
