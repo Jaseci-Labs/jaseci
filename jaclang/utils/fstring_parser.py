7573f91805a27037878bd958e185b72979d46660
marsninja <jason@mars.ninja>
2023-06-07 17:42:11 -0400

    fstrings basic

---
+++ jaclang/utils/fstring_parser.py
@@ -0,0 +1,69 @@
+"""Python Like F-String Parser."""
+from jaclang.utils.sly.lex import Lexer
+from jaclang.utils.sly.yacc import Parser, YaccProduction
+
+_ = None  # For flake8 linting and sly compatibility
+
+
+class FStringLexer(Lexer):
+    """Python Like F-String Lexer."""
+
+    tokens = {
+        "STRING_START",
+        "STRING_END",
+        "EXPR_START",
+        "EXPR_END",
+        "PIECE",
+    }
+    ignore = " \t"
+
+    # Tokens
+    STRING_START = r"f\""
+    STRING_END = r"\""
+    EXPR_START = r"\{"
+    EXPR_END = r"\}"
+    PIECE = r"[^\{\}\"]+"
+
+
+class FStringParser(Parser):
+    """Python Like F-String Parser."""
+
+    tokens = FStringLexer.tokens
+
+    def __init__(self: "FStringParser") -> None:
+        """Initialize the parser."""
+        self.names = {}
+
+    @_("STRING_START parts STRING_END")
+    def fstring(self: "FStringParser", p: YaccProduction) -> YaccProduction:
+        """Start rule for fstring."""
+        return p
+
+    @_(
+        "parts expr",
+        "parts PIECE",
+        "PIECE",
+    )
+    def parts(self: "FStringParser", p: YaccProduction) -> YaccProduction:
+        """Parts of the string both in string and in expression."""
+        return p
+
+    @_("EXPR_START parts EXPR_END")
+    def expr(self: "FStringParser", p: YaccProduction) -> YaccProduction:
+        """Expressions rule."""
+        return p
+
+
+if __name__ == "__main__":
+    """Run the parser for live testing."""
+    lexer = FStringLexer()
+    parser = FStringParser()
+    while True:
+        try:
+            text = input("fstring > ")
+        except EOFError:
+            break
+        if text:
+            tokens = lexer.tokenize(text)
+            result = parser.parse(tokens)
+            print(result)
