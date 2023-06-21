138d7e632f0f378e61318ca801ca6d9f3fc92ddc
AshishMahendra <am028275@gmail.com>
2023-06-21 03:47:02 +0000

    time_out update

--- jaseci_core/jaseci/jsorc/live_actions.py
+++ jaseci_core/jaseci/jsorc/live_actions.py
@@ -24,7 +24,7 @@ import time
 # MAX_WORKERS = 100
 # actions_sem = Semaphore(MAX_WORKERS + 1)
 
-ACTION_SUBPROC_TIMEOUT = 4  # 3 seconds
+ACTION_SUBPROC_TIMEOUT = 2  # 3 seconds
 
 live_actions_lock = RXW1Lock()
 live_actions = {}  # {"act.func": func_obj, ...}
