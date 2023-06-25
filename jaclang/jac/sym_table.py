c4d625f2fdbed7596c8dc8bababe20905d7718d0
marsninja <jason@mars.ninja>
2023-06-25 12:19:49 -0400

    started symtable

---
+++ jaclang/jac/sym_table.py
@@ -0,0 +1,21 @@
+"""Jac Symbol Table."""
+from typing import Optional
+
+
+class Symbol:
+    """Symbol."""
+
+    def __init__(self) -> None:
+        """Initialize."""
+        self.name = None
+        self.access = None
+        self.typ = None
+
+
+class SymbolTable:
+    """Symbol Table."""
+
+    def __init__(self, parent: Optional["SymbolTable"] = None) -> None:
+        """Initialize."""
+        self.parent = parent
+        self.table: dict[str, Symbol] = {}
