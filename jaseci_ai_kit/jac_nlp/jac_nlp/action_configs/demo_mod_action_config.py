09d145906b8b9d1d14c914f1b9eec7ca7dc928f7
AshishMahendra <am028275@gmail.com>
2023-06-28 14:30:58 +0000

    debug

--- jaseci_ai_kit/jac_nlp/jac_nlp/action_configs/demo_mod_action_config.py
+++ jaseci_ai_kit/jac_nlp/jac_nlp/action_configs/demo_mod_action_config.py
@@ -26,7 +26,7 @@ DEMO_MOD_ACTION_CONFIG = {
                 "creationTimestamp": None,
             },
             "data": {
-                "prod_up": "git clone -b action_policy https://github.com/Jaseci-Labs/jaseci.git; cd jaseci; cd jaseci_core; source install.sh; cd ../jaseci_ai_kit/jac_nlp; pip install -e .[demo_mod]; uvicorn jac_nlp.demo_mod:serv_actions --host 0.0.0.0 --port 80;"
+                "prod_up": "git clone -b action_policy https://github.com/Jaseci-Labs/jaseci.git; uvicorn jac_nlp.demo_mod:serv_actions --host 0.0.0.0 --port 80;"
                 # "prod_up": "uvicorn jac_nlp.demo_mod:serv_actions --host 0.0.0.0 --port 80"
             },
         },
