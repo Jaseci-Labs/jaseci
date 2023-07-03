b918dc1cefb259e5ba1c80e383be6cca1edd722a
Tharuka Kasthuriarachchi <tharuka.ckasthuri@gmail.com>
2023-07-03 17:28:48 +0530

    updated test cases

--- jaseci_core/jaseci/extens/act_lib/tests/test_maths.py
+++ jaseci_core/jaseci/extens/act_lib/tests/test_maths.py
@@ -40,3 +40,50 @@ class MathsTests(CoreTest):
     def test_fmod(self, ret):
         ret = ret["report"][0]
         self.assertEqual(ret, 1.0)
+
+    @jac_testcase("maths.jac", "frexp_test")
+    def test_frexp(self, ret):
+        ret = ret["report"][0]
+        self.assertEqual(ret, [0.78125, 7])
+
+    @jac_testcase("maths.jac", "fsum_test")
+    def test_fsum(self, ret):
+        ret = ret["report"][0]
+        self.assertEqual(ret, 18.0)
+
+    # gcd todo
+
+    # isclose todo
+
+    # isfinite todo
+
+    # isinf todo
+
+    # isnan toto
+
+    @jac_testcase("maths.jac", "isqrt_test")
+    def test_isqrt(self, ret):
+        ret = ret["report"][0]
+        self.assertEqual(ret, 3)
+
+    # lcm todo
+
+    @jac_testcase("maths.jac", "ldexp_test")
+    def test_isqrt(self, ret):
+        ret = ret["report"][0]
+        self.assertEqual(ret, 36.0)
+
+    @jac_testcase("maths.jac", "modf_test")
+    def test_modf(self, ret):
+        ret = ret["report"][0]
+        self.assertEqual(ret, [0.0, 9.0])
+
+    @jac_testcase("maths.jac", "nextafter_test")
+    def test_nextafter(self, ret):
+        ret = ret["report"][0]
+        self.assertEqual(ret, 8.999999999999998)
+
+    @jac_testcase("maths.jac", "perm_test")
+    def test_perm(self, ret):
+        ret = ret["report"][0]
+        self.assertEqual(ret, 25852016738884976640000)
