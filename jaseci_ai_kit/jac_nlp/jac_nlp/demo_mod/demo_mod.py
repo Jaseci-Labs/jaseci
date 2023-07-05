06b28579e363b44467fcc072aa8b4d21f6a0370f
AshishMahendra <am028275@gmail.com>
2023-07-05 13:27:22 +0000

    demo mod updated

--- jaseci_ai_kit/jac_nlp/jac_nlp/demo_mod/demo_mod.py
+++ jaseci_ai_kit/jac_nlp/jac_nlp/demo_mod/demo_mod.py
@@ -1,9 +1,14 @@
 from jaseci.jsorc.live_actions import jaseci_action
+import time
+import random
 
 
 @jaseci_action(act_group=["demo_mod"], allow_remote=True)
 def demo_mod_action(x: int = 1, y: int = 1):
-    #    print(f"x={x}, y={y}")
+    # Generate a random sleep time between 1 and 3 seconds
+    sleep_time = random.uniform(1, 3)
+    # Sleep for the generated time
+    time.sleep(sleep_time)
     return x + y
 
 
