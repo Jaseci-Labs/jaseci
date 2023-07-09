b30e9cd812ebb56a60bd694b8fb2c31db252a026
marsninja <jason@mars.ninja>
2023-07-09 17:18:15 -0400

    Pass import order matters

--- jaclang/jac/passes/__init__.py
+++ jaclang/jac/passes/__init__.py
@@ -1,12 +1,12 @@
 """Collection of passes for Jac IR."""
 
-from .ast_build_pass import AstBuildPass
-from .blue_pygen_pass import BluePygenPass
-from .decl_def_match_pass import DeclDefMatchPass
-from .import_pass import ImportPass
 from .ir_pass import Pass
+from .ast_build_pass import AstBuildPass  # noqa: I100
 from .sub_node_tab_pass import SubNodeTabPass
+from .import_pass import ImportPass  # noqa: I100
+from .decl_def_match_pass import DeclDefMatchPass  # noqa: I100
 from .type_analyze_pass import TypeAnalyzePass
+from .blue_pygen_pass import BluePygenPass  # noqa: I100
 
 
 __all__ = [
