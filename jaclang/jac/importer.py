fb3efd54242db9ade79f9f37ca98e18c1dd85b53
marsninja <jason@mars.ninja>
2023-07-11 14:58:35 -0400

    gen by default for now

--- jaclang/jac/importer.py
+++ jaclang/jac/importer.py
@@ -1,55 +1,52 @@
-"""Special importer for Jac files."""
+"""Special Imports for Jac Code."""
 import inspect
 import sys
 import traceback
 import types
-from os import path
-from typing import Optional
+from os import makedirs, path
+from typing import Callable, Optional
 
 from jaclang.jac.transpiler import transpile_jac_blue, transpile_jac_purple
 from jaclang.jac.utils import add_line_numbers
 
 
-def jac_blue_import(
-    target: str, base_path: Optional[str] = None, save_file: bool = False
+def import_jac_module(
+    transpiler_func: Callable,
+    target: str,
+    base_path: Optional[str] = None,
+    save_file: bool = False,
 ) -> Optional[types.ModuleType]:
-    """Import a module from a path."""
-    # Convert python import paths to directory paths
+    """Core Import Process."""
     target = path.join(*(target.split("."))) + ".jac"
 
-    # Get module name and package path
     dir_path, file_name = path.split(target)
     module_name = path.splitext(file_name)[0]
     package_path = dir_path.replace(path.sep, ".")
 
-    # If module already loaded in sys return it
     if package_path and f"{package_path}.{module_name}" in sys.modules:
         return sys.modules[f"{package_path}.{module_name}"]
     elif not package_path and module_name in sys.modules:
         return sys.modules[module_name]
 
-    # Get the directory of the calling module
     if base_path:
         caller_dir = path.dirname(base_path) if not path.isdir(base_path) else base_path
     else:
-        frame = inspect.stack()[1]
+        frame = inspect.stack()[2]
         caller_dir = path.dirname(path.abspath(frame[0].f_code.co_filename))
     full_target = path.normpath(path.join(caller_dir, target))
 
-    # Transpile the Jac file
-    code_string = transpile_jac_blue(file_path=full_target, base_dir=caller_dir)
-    if save_file:
-        with open(path.join(dir_path, module_name + ".py"), "w") as f:
-            f.write(code_string)
+    code_string = transpiler_func(file_path=full_target, base_dir=caller_dir)
 
-    # Create a module object
+    # if save_file:
+    dev_dir = path.join(caller_dir, "_dev")
+    makedirs(dev_dir, exist_ok=True)
+    with open(path.join(dev_dir, module_name + ".py"), "w") as f:
+        f.write(code_string)
+
     module = types.ModuleType(module_name)
-
-    # Set __file__ attribute
     module.__file__ = full_target
     module.__name__ = module_name
 
-    # Execute the code in the context of the module's namespace
     try:
         exec(code_string, module.__dict__)
     except Exception as e:
@@ -60,89 +57,30 @@ def jac_blue_import(
         )
         return None
 
-    # Register the module in sys.modules
     if package_path:
-        # Register the package and all subpackages
         parts = package_path.split(".")
         for i in range(len(parts)):
             package_name = ".".join(parts[: i + 1])
             if package_name not in sys.modules:
                 sys.modules[package_name] = types.ModuleType(package_name)
 
-        # Set the module as an attribute of the package
         setattr(sys.modules[package_path], module_name, module)
         sys.modules[f"{package_path}.{module_name}"] = module
     else:
         sys.modules[module_name] = module
 
-    # Add the module to the calling context's global variables
     return module
+
+
+def jac_blue_import(
+    target: str, base_path: Optional[str] = None, save_file: bool = False
+) -> Optional[types.ModuleType]:
+    """Jac Blue Imports."""
+    return import_jac_module(transpile_jac_blue, target, base_path, save_file)
 
 
 def jac_purple_import(
     target: str, base_path: Optional[str] = None, save_file: bool = False
 ) -> Optional[types.ModuleType]:
-    """Import a module from a path."""
-    # Convert python import paths to directory paths
-    target = path.join(*(target.split("."))) + ".jac"
-
-    # Get module name and package path
-    dir_path, file_name = path.split(target)
-    module_name = path.splitext(file_name)[0]
-    package_path = dir_path.replace(path.sep, ".")
-
-    # If module already loaded in sys return it
-    if package_path and f"{package_path}.{module_name}" in sys.modules:
-        return sys.modules[f"{package_path}.{module_name}"]
-    elif not package_path and module_name in sys.modules:
-        return sys.modules[module_name]
-
-    # Get the directory of the calling module
-    if base_path:
-        caller_dir = path.dirname(base_path) if not path.isdir(base_path) else base_path
-    else:
-        frame = inspect.stack()[1]
-        caller_dir = path.dirname(path.abspath(frame[0].f_code.co_filename))
-    full_target = path.normpath(path.join(caller_dir, target))
-
-    # Transpile the Jac file
-    code_string = transpile_jac_purple(file_path=full_target, base_dir=caller_dir)
-    if save_file:
-        with open(path.join(dir_path, module_name + ".py"), "w") as f:
-            f.write(code_string)
-
-    # Create a module object
-    module = types.ModuleType(module_name)
-
-    # Set __file__ attribute
-    module.__file__ = full_target
-    module.__name__ = module_name
-
-    # Execute the code in the context of the module's namespace
-    try:
-        exec(code_string, module.__dict__)
-    except Exception as e:
-        traceback.print_exc()
-        print(
-            f"Error in module {module_name}\nJac file: {full_target}\n"
-            f"Error: {str(e)}\nIR:\n{add_line_numbers(code_string)}"
-        )
-        return None
-
-    # Register the module in sys.modules
-    if package_path:
-        # Register the package and all subpackages
-        parts = package_path.split(".")
-        for i in range(len(parts)):
-            package_name = ".".join(parts[: i + 1])
-            if package_name not in sys.modules:
-                sys.modules[package_name] = types.ModuleType(package_name)
-
-        # Set the module as an attribute of the package
-        setattr(sys.modules[package_path], module_name, module)
-        sys.modules[f"{package_path}.{module_name}"] = module
-    else:
-        sys.modules[module_name] = module
-
-    # Add the module to the calling context's global variables
-    return module
+    """Jac Purple Imports."""
+    return import_jac_module(transpile_jac_purple, target, base_path, save_file)
