fac73aa04d6f751e3c260669d18b1a31fdbef6ba
AshishMahendra <am028275@gmail.com>
2023-07-21 15:03:07 +0000

    skip another try

--- jaseci_ai_kit/jac_nlp/jac_nlp/ent_ext/tests/test_ent_ext.py
+++ jaseci_ai_kit/jac_nlp/jac_nlp/ent_ext/tests/test_ent_ext.py
@@ -10,39 +10,45 @@ class EntExtTest(CoreTest):
     """
     Test Class for EntExt Module to test the functionality of api's
     """
-    pytest.skip(allow_module_level=True)
 
+    @pytest.mark.skip(reason="memory issue")
     @classmethod
     def setUpClass(cls):
         super(EntExtTest, cls).setUpClass()
         ret = load_module_actions("jac_nlp.ent_ext")
         assert ret is True
 
+    @pytest.mark.skip(reason="memory issue")
     @pytest.mark.order(1)
     @jac_testcase("ent_ext.jac", "test_ent_ext_set_config")
     def test_ent_ext_set_config(self, ret):
         self.assertEqual(ret["success"], True)
 
+    @pytest.mark.skip(reason="memory issue")
     @pytest.mark.order(2)
     @jac_testcase("ent_ext.jac", "test_train_ner")
     def test_train_ner(self, ret):
         self.assertEqual(ret["success"], True)
 
+    @pytest.mark.skip(reason="memory issue")
     @pytest.mark.order(3)
     @jac_testcase("ent_ext.jac", "test_extract_entity")
     def test_extract_entity(self, ret):
         self.assertEqual(ret["success"], True)
 
+    @pytest.mark.skip(reason="memory issue")
     @pytest.mark.order(4)
     @jac_testcase("ent_ext.jac", "test_ent_ext_save_model")
     def test_ent_ext_save_model(self, ret):
         self.assertEqual(ret["success"], True)
 
+    @pytest.mark.skip(reason="memory issue")
     @pytest.mark.order(5)
     @jac_testcase("ent_ext.jac", "test_ent_ext_load_model")
     def test_ent_ext_load_model(self, ret):
         self.assertEqual(ret["success"], True)
 
+    @pytest.mark.skip(reason="memory issue")
     @classmethod
     def tearDownClass(cls):
         super(EntExtTest, cls).tearDownClass()
