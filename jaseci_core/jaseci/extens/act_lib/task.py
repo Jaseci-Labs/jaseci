349332f9d45f6e07d0ec186681bcbfc06077b248
Alexie (Boyong) Madolid <maex@tuta.io>
2023-08-03 21:55:20 +0800

    [TASKS]: Add timeout on task.get_result action

--- jaseci_core/jaseci/extens/act_lib/task.py
+++ jaseci_core/jaseci/extens/act_lib/task.py
@@ -5,9 +5,9 @@ from jaseci.extens.svc.task_svc import TaskService
 
 
 @jaseci_action()
-def get_result(task_id, wait, meta):
+def get_result(task_id, wait=False, timeout=30):
     """
     Get task result by task_id
     """
 
-    return JsOrc.svc("task").poke(TaskService).get_by_task_id(task_id, wait)
+    return JsOrc.svc("task").poke(TaskService).get_by_task_id(task_id, wait, timeout)
