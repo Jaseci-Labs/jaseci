7adb3e32d82793af85983ecfcad732422294db82
marsninja <jason@mars.ninja>
2023-09-11 11:03:09 -0400

    Added test for Jac red

--- jaclang/jac/passes/purple/tests/test_purple_pygen_pass.py
+++ jaclang/jac/passes/purple/tests/test_purple_pygen_pass.py
@@ -30,6 +30,23 @@ class PurplePygenPassTests(TestCaseMicroSuite):
             "\nValue: 5\nValue: 6\nValue: 7\nFinal Value: 8\nDone walking.\n",
         )
 
+    def test_guess_game(self) -> None:
+        """Parse micro jac file."""
+        from jaclang import jac_purple_import
+
+        captured_output = io.StringIO()
+        sys.stdout = captured_output
+        jac_purple_import(
+            "guess_game.guess_game_test",
+            self.fixture_abs_path("../../../../../../examples/"),
+        )
+        sys.stdout = sys.__stdout__
+        stdout_value = captured_output.getvalue()
+        self.assertEqual(
+            stdout_value,
+            "Too high!\nToo low!\nToo high!\nCongratulations! You guessed correctly.\n",
+        )
+
     def micro_suite_test(self, filename: str) -> None:
         """Parse micro jac file."""
         code_gen = transpile_jac_purple(filename, "")
