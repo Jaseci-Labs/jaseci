af3e25a32f969e633be7f1e6231756f46983e4e2
marsninja <jason@mars.ninja>
2023-09-24 09:40:07 -0400

    Cleaner importer

--- jaclang/jac/importer.py
+++ jaclang/jac/importer.py
@@ -20,9 +20,7 @@ def import_jac_module(
     override_name: Optional[str] = None,
 ) -> Optional[types.ModuleType]:
     """Core Import Process."""
-    target = path.join(*(target.split("."))) + ".jac"
-
-    dir_path, file_name = path.split(target)
+    dir_path, file_name = path.split(path.join(*(target.split("."))) + ".jac")
     module_name = path.splitext(file_name)[0]
     package_path = dir_path.replace(path.sep, ".")
 
@@ -36,44 +34,41 @@ def import_jac_module(
     else:
         frame = inspect.stack()[2]
         caller_dir = path.dirname(path.abspath(frame[0].f_code.co_filename))
-    full_target = path.normpath(path.join(caller_dir, target))
+    caller_dir = path.join(caller_dir, dir_path)
 
-    dev_dir = path.join(caller_dir, Con.JAC_GEN_DIR)
-    makedirs(dev_dir, exist_ok=True)
-    py_file_path = path.join(dev_dir, module_name + ".py")
+    gen_dir = path.join(caller_dir, Con.JAC_GEN_DIR)
+    full_target = path.normpath(path.join(caller_dir, file_name))
+
+    py_file_path = path.join(gen_dir, module_name + ".py")
+    pyc_file_path = path.join(gen_dir, module_name + ".pyc")
     if (
         cachable
         and path.exists(py_file_path)
         and path.getmtime(py_file_path) > path.getmtime(full_target)
+        and path.exists(pyc_file_path)
+        and path.getmtime(pyc_file_path) > path.getmtime(full_target)
     ):
         with open(py_file_path, "r") as f:
             code_string = f.read()
+        with open(pyc_file_path, "rb") as f:
+            codeobj = marshal.load(f)
     else:
-        code_string = transpiler_func(file_path=full_target, base_dir=caller_dir)
-        with open(py_file_path, "w") as f:
-            f.write(code_string)
+        transpiler_func(file_path=full_target, base_dir=caller_dir)
+        with open(py_file_path, "r") as f:
+            code_string = f.read()
+        try:
+            codeobj = compile(code_string, f"_jac_py_gen ({full_target})", "exec")
+        except Exception as e:
+            tb = traceback.extract_tb(e.__traceback__)
+            err = handle_jac_error(code_string, e, tb)
+            raise type(e)(str(e) + "\n" + err)
+        with open(pyc_file_path, "wb") as f:
+            marshal.dump(codeobj, f)
 
     module = types.ModuleType(module_name)
     module.__file__ = full_target
     module.__name__ = override_name if override_name else module_name
     module.__dict__["_jac_pycodestring_"] = code_string
-
-    if (
-        cachable
-        and path.exists(py_file_path + "c")
-        and path.getmtime(py_file_path + "c") > path.getmtime(full_target)
-    ):
-        with open(py_file_path + "c", "rb") as f:
-            codeobj = marshal.load(f)
-    else:
-        try:
-            codeobj = compile(code_string, f"_jac_py_gen ({module.__file__})", "exec")
-        except Exception as e:
-            tb = traceback.extract_tb(e.__traceback__)
-            err = handle_jac_error(code_string, e, tb)
-            raise type(e)(str(e) + "\n" + err)
-        with open(py_file_path + "c", "wb") as f:
-            marshal.dump(codeobj, f)
 
     try:
         exec(codeobj, module.__dict__)
--- jaclang/jac/passes/blue/pyout_pass.py
+++ jaclang/jac/passes/blue/pyout_pass.py
@@ -1,6 +1,7 @@
 """Connect Decls and Defs in AST."""
 import os
 
+
 import jaclang.jac.absyntree as ast
 from jaclang.jac.constant import Constants as Con
 from jaclang.jac.passes import Pass
@@ -30,10 +31,26 @@ class PyOutPass(Pass):
             or not node.meta["py_code"]
         ):
             return
-        dir_name, file_name = os.path.split(node.mod_path)
+        base_path, file_name = os.path.split(node.mod_path)
+        gen_path = os.path.join(base_path, Con.JAC_GEN_DIR)
+        os.makedirs(gen_path, exist_ok=True)
+        with open(os.path.join(gen_path, "__init__.py"), "w") as f:
+            pass
+        mods = [node] + self.get_all_sub_nodes(node, ast.Module)
+        for mod in mods:
+            self.gen_python(mod, base_path=base_path, gen_path=gen_path)
+        self.terminate()
+
+    def gen_python(self, node: ast.Module, base_path: str, gen_path: str) -> None:
+        """Generate Python."""
+        mod_dir, file_name = os.path.split(node.mod_path)
+        mod_dir = mod_dir.replace(base_path, "").lstrip(os.sep)
         base_name, _ = os.path.splitext(file_name)
         file_name = f"{base_name}.py"
-        os.makedirs(os.path.join(dir_name, Con.JAC_GEN_DIR), exist_ok=True)
-        out_path = os.path.join(dir_name, Con.JAC_GEN_DIR, file_name)
+        out_dir = os.path.join(gen_path, mod_dir)
+        out_path = os.path.join(out_dir, file_name)
+        os.makedirs(out_dir, exist_ok=True)
+        with open(os.path.join(out_dir, "__init__.py"), "w") as f:
+            pass
         with open(out_path, "w") as f:
             f.write(node.meta["py_code"])
