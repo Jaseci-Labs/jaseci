9b507aae21b51307ca5f65139034b9d1e028ef5f
marsninja <jason@mars.ninja>
2023-10-01 12:45:27 -0400

    Some tweaks a bit more polishign needed but mergable

--- jaclang/jac/passes/blue/dot_exporter_pass.py
+++ jaclang/jac/passes/blue/dot_exporter_pass.py
@@ -1,14 +1,12 @@
 """Jac Blue pass for drawing AST."""
 import inspect
-from typing import Dict, List, Type, TypeVar
 
 import jaclang.jac.absyntree as ast
 from jaclang.jac.absyntree import Ability, AbilityDef, ArchDef, Architype
 from jaclang.jac.passes import Pass
 
 
-T = TypeVar("T", bound=Pass)
-DOT_GRAPH_CLASS_COLOR_MAP: Dict[Type[T], str] = {
+DOT_GRAPH_CLASS_COLOR_MAP: dict[type, str] = {
     Architype: "red",
     Ability: "green",
     ArchDef: "blue",
@@ -21,8 +19,8 @@ class DotGraphPass(Pass):
 
     def before_pass(self) -> None:
         """Initialize pass."""
-        self.__dot_lines : List[str] = []
-        self.__id_map : Dict[int, int] = {}
+        self.__dot_lines: list[str] = []
+        self.__id_map: dict[int, int] = {}
         self.__lase_id_used = 0
         return super().before_pass()
 
@@ -37,48 +35,54 @@ class DotGraphPass(Pass):
         fillcolor = ""
         style = ""
         if node.__class__ in DOT_GRAPH_CLASS_COLOR_MAP:
-            shape = "shape=\"box\""
-            style = "style=\"filled\""
-            fillcolor = f"fillcolor=\"{DOT_GRAPH_CLASS_COLOR_MAP[node.__class__]}\""
+            shape = 'shape="box"'
+            style = 'style="filled"'
+            fillcolor = f'fillcolor="{DOT_GRAPH_CLASS_COLOR_MAP[node.__class__]}"'
 
         info = self.__gen_node_info(node)
         if len(info) == 0:
-            label = f"\"{node.__class__.__name__}\""
+            label = f'"{node.__class__.__name__}"'
         else:
             label = f"<{node.__class__.__name__}"
             for i in info:
                 label += f"<BR/> {i[0]}: {i[1]}"
             label += ">"
 
-        return f"[label={label} {shape} {style} {fillcolor}]"
+        label = f"{label} {shape} {style} {fillcolor}".strip()
+        return f"[label={label}]"
 
-    def __gen_node_info(self, node: ast.AstNode) -> None:
+    def __gen_node_info(self, node: ast.AstNode) -> list[tuple[str, str]]:
         init_source = inspect.getsource(node.__class__.__init__)
-        info_to_be_dumped: List[str] = []
+        info_to_be_dumped: list[tuple[str, str]] = []
         for line in init_source.split("\n"):
             if "def" in line:
                 continue
             elif "->" in line:
                 break
             elif "# DotExporter::AddInfo" in line:
-                info_to_be_dumped.append((
-                    line.strip().split(":")[0].strip(),
-                    line.strip().split(":")[1].split("#")[0].strip()
-                ))
+                info_to_be_dumped.append(
+                    (
+                        line.strip().split(":")[0].strip(),
+                        line.strip().split(":")[1].split("#")[0].strip(),
+                    )
+                )
         return info_to_be_dumped
 
     def enter_node(self, node: ast.AstNode) -> None:
         """Enter node."""
-        self.__dot_lines.append(f"{self.__gen_node_id(node)} {self.__gen_node_parameters(node)};")
+        self.__dot_lines.append(
+            f"{self.__gen_node_id(node)} {self.__gen_node_parameters(node)};"
+        )
         for kid_node in node.kid:
             if kid_node:
-                self.__dot_lines.append(f"{self.__gen_node_id(node)}  -> {self.__gen_node_id(kid_node)};")
+                self.__dot_lines.append(
+                    f"{self.__gen_node_id(node)}  -> {self.__gen_node_id(kid_node)};"
+                )
 
     def after_pass(self) -> None:
         """Finalize pass by generating the dot file."""
-        f = open("out.dot", "w")
-        f.write("digraph graph1 {")
-        f.write("\n".join(self.__dot_lines))
-        f.write("}")
-        f.close()
+        with open("out.dot", "w") as f:
+            f.write("digraph graph1 {")
+            f.write("\n".join(self.__dot_lines))
+            f.write("}")
         return super().after_pass()
