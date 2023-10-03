d0236f6c9beae0b3bc6b4e79f7a1ee61f02d6e8e
marsninja <jason@mars.ninja>
2023-10-03 18:07:13 -0400

    Getting schaffold up

--- jaclang/jac/larkparse.py
+++ jaclang/jac/larkparse.py
@@ -1,27 +1,25 @@
+"""Lark parser for Jac Lang."""
 import os
+from typing import Optional
 
 from jaclang.vendor.lark import Lark, Transformer, v_args
+import jaclang.jac.absyntree as ast
+from jaclang.jac.transform import ABCParserMeta, Transform
 
 with open(os.path.join(os.path.dirname(__file__), "jac.lark"), "r") as f:
-    c_like_grammar = f.read()
+    jac_grammar = f.read()
 
-parser = Lark(c_like_grammar)
 
-test_code = """
-int main() {
-    int a;
-    a = 5;
-    if (a == 5) {
-        return 1;
-    } else {
-        return 0;
-    }
-}
+class JacParser(Transform):
+    def __init__(
+        self,
+        mod_path: str,
+        input_ir: str,
+        base_path: str = "",
+        prior: Optional[Transform] = None,
+    ):
+        self.parser = Lark(jac_grammar)
+        self.ir = self.transform(input_ir)
 
-int add(int x, int y) {
-    return x + y;
-}
-"""
-
-tree = parser.parse(test_code)
-print(tree.pretty())
+    def transform(self, ir: str) -> ast.AstNode:
+        return self.parser.parse(ir)
---
+++ jaclang/jac/tests/test_larkparser.py
@@ -0,0 +1,20 @@
+"""Tests for Jac parser."""
+from jaclang.jac.larkparse import JacParser
+from jaclang.utils.test import TestCaseMicroSuite
+
+
+class TestLarkParser(TestCaseMicroSuite):
+    """Test Jac self.prse."""
+
+    def setUp(self) -> None:
+        """Set up test."""
+        return super().setUp()
+
+    def micro_suite_test(self, filename: str) -> None:
+        """Parse micro jac file."""
+        prse = JacParser(mod_path=filename, input_ir=self.file_to_str(filename))
+        print(prse.ir)
+        self.assertFalse(prse.errors_had)
+
+
+TestLarkParser.self_attach_micro_tests()
