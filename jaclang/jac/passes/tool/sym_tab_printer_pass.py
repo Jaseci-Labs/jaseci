e1158f6cfacc6211a5b09bcbbe9aba5b1076e08b
marsninja <jason@mars.ninja>
2023-10-20 11:37:29 -0400

    Symtable printer refacotred

--- jaclang/jac/passes/tool/sym_tab_printer_pass.py
+++ jaclang/jac/passes/tool/sym_tab_printer_pass.py
@@ -7,75 +7,71 @@ import jaclang.jac.absyntree as ast
 from jaclang.jac.passes import Pass
 
 
+class _SymbolTree:
+    def __init__(
+        self,
+        node_name: str,
+        parent: Optional["_SymbolTree"] = None,
+        children: Optional[List["_SymbolTree"]] = None,
+    ) -> None:
+        self.parent = parent
+        self.kid = children if children is not None else []
+        self.name = node_name
+
+    @property
+    def parent(self) -> Optional["_SymbolTree"]:
+        return self.__parent
+
+    @parent.setter
+    def parent(self, parent_node: Optional["_SymbolTree"]) -> None:
+        if parent_node:
+            self.__parent = parent_node
+            parent_node.kid.append(self)
+
+
+def _build_symbol_tree_common(
+    node: ast.SymbolTable, parent_node: Optional[_SymbolTree] = None
+) -> _SymbolTree:
+    root = _SymbolTree(
+        node_name=f"SymTable::{node.owner.__class__.__name__}({node.name})",
+        parent=parent_node,
+    )
+    symbols = _SymbolTree(node_name="Symbols", parent=root)
+    children = _SymbolTree(node_name="Children", parent=root)
+
+    for sym in node.tab.values():
+        symbol_node = _SymbolTree(node_name=f"Symbol({sym.name})", parent=symbols)
+        _SymbolTree(
+            node_name=f"declared in {sym.decl.__class__.__name__}", parent=symbol_node
+        )
+
+        defn = _SymbolTree(node_name="defn", parent=symbol_node)
+        [_SymbolTree(node_name=n.__class__.__name__, parent=defn) for n in sym.defn]
+
+        uses = _SymbolTree(node_name="uses", parent=symbol_node)
+        [_SymbolTree(node_name=n.__class__.__name__, parent=uses) for n in sym.uses]
+
+    for k in node.kid:
+        _build_symbol_tree_common(k, children)
+    return root
+
+
 class SymbolTablePrinterPass(Pass):
-    """Jac symbol table convertion to ascii tree."""
-
-    class __SymbolTree:
-        def __init__(
-            self,
-            node_name: str,
-            parent: Optional[SymbolTablePrinterPass.__SymbolTree] = None,
-            children: Optional[List[SymbolTablePrinterPass.__SymbolTree]] = None,
-        ) -> None:
-            self.parent = parent
-            self.kid = children if children is not None else []
-            self.name = node_name
-
-        @property
-        def parent(self) -> Optional[SymbolTablePrinterPass.__SymbolTree]:
-            return self.__parent
-
-        @parent.setter
-        def parent(
-            self, parent_node: Optional[SymbolTablePrinterPass.__SymbolTree]
-        ) -> None:
-            if parent_node:
-                self.__parent = parent_node
-                parent_node.kid.append(self)
+    """Jac symbol table conversion to ASCII tree."""
 
     SAVE_OUTPUT: Optional[str] = None
 
     def before_pass(self) -> None:
         """Initialize pass."""
         if isinstance(self.ir.sym_tab, ast.SymbolTable):
-            self.__root = self.__build_symbol_tree(self.ir.sym_tab)
-            self.__print_tree(self.__root)
+            root = _build_symbol_tree_common(self.ir.sym_tab)
+            self._print_tree(root)
         self.terminate()
         return super().before_pass()
 
-    def __build_symbol_tree(
-        self, node: ast.SymbolTable, parent_node: Optional[__SymbolTree] = None
-    ) -> __SymbolTree:
-        root = self.__SymbolTree(
-            node_name=f"SymTable::{node.owner.__class__.__name__}({node.name})",
-            parent=parent_node,
-        )
-        symbols = self.__SymbolTree(node_name="Symbols", parent=root)
-        children = self.__SymbolTree(node_name="Children", parent=root)
-
-        for sym in node.tab:
-            sym = node.tab[sym]
-            symbol_node = self.__SymbolTree(
-                node_name=f"Symbol({sym.name})", parent=symbols
-            )
-            self.__SymbolTree(
-                node_name=f"declared in {sym.decl.__class__.__name__}",
-                parent=symbol_node,
-            )
-            defn = self.__SymbolTree(node_name="defn", parent=symbol_node)
-            for n in sym.defn:
-                self.__SymbolTree(node_name=n.__class__.__name__, parent=defn)
-            uses = self.__SymbolTree(node_name="uses", parent=symbol_node)
-            for n in sym.uses:
-                self.__SymbolTree(node_name=n.__class__.__name__, parent=uses)
-
-        for k in node.kid:
-            self.__build_symbol_tree(k, children)
-        return root
-
-    def __print_tree(
+    def _print_tree(
         self,
-        root: __SymbolTree,
+        root: _SymbolTree,
         marker: str = "+-- ",
         level_markers: Optional[List[bool]] = None,
     ) -> None:
@@ -104,34 +100,11 @@ class SymbolTablePrinterPass(Pass):
             # The last child will not need connection markers on the current level
             # (see example above)
             is_last = i == len(root.kid) - 1
-            self.__print_tree(child, marker, [*level_markers, not is_last])
+            self._print_tree(child, marker, [*level_markers, not is_last])
 
 
 class SymbolTableDotGraphPass(Pass):
-    """Jac AST convertion to DOT graph."""
-
-    class __SymbolTree:
-        def __init__(
-            self,
-            node_name: str,
-            parent: Optional[SymbolTableDotGraphPass.__SymbolTree] = None,
-            children: Optional[List[SymbolTableDotGraphPass.__SymbolTree]] = None,
-        ) -> None:
-            self.parent = parent
-            self.kid = children if children is not None else []
-            self.name = node_name
-
-        @property
-        def parent(self) -> Optional[SymbolTableDotGraphPass.__SymbolTree]:
-            return self.__parent
-
-        @parent.setter
-        def parent(
-            self, parent_node: Optional[SymbolTableDotGraphPass.__SymbolTree]
-        ) -> None:
-            if parent_node:
-                self.__parent = parent_node
-                parent_node.kid.append(self)
+    """Jac AST conversion to DOT graph."""
 
     OUTPUT_FILE_PATH: Optional[str] = "out.dot"
 
@@ -141,47 +114,17 @@ class SymbolTableDotGraphPass(Pass):
         self.__id_map: dict[int, int] = {}
         self.__lase_id_used = 0
         if isinstance(self.ir.sym_tab, ast.SymbolTable):
-            self.__gen_dot_graph(self.__build_symbol_tree(self.ir.sym_tab))
+            self.__gen_dot_graph(_build_symbol_tree_common(self.ir.sym_tab))
         self.terminate()
         return super().before_pass()
 
-    def __build_symbol_tree(
-        self, node: ast.SymbolTable, parent_node: Optional[__SymbolTree] = None
-    ) -> __SymbolTree:
-        root = self.__SymbolTree(
-            node_name=f"SymTable::{node.owner.__class__.__name__}({node.name})",
-            parent=parent_node,
-        )
-        symbols = self.__SymbolTree(node_name="Symbols", parent=root)
-        children = self.__SymbolTree(node_name="Children", parent=root)
-
-        for sym in node.tab:
-            sym = node.tab[sym]
-            symbol_node = self.__SymbolTree(
-                node_name=f"Symbol({sym.name})", parent=symbols
-            )
-            self.__SymbolTree(
-                node_name=f"declared in {sym.decl.__class__.__name__}",
-                parent=symbol_node,
-            )
-            defn = self.__SymbolTree(node_name="defn", parent=symbol_node)
-            for n in sym.defn:
-                self.__SymbolTree(node_name=n.__class__.__name__, parent=defn)
-            uses = self.__SymbolTree(node_name="uses", parent=symbol_node)
-            for n in sym.uses:
-                self.__SymbolTree(node_name=n.__class__.__name__, parent=uses)
-
-        for k in node.kid:
-            self.__build_symbol_tree(k, children)
-        return root
-
-    def __gen_node_id(self, node: __SymbolTree) -> int:
+    def __gen_node_id(self, node: _SymbolTree) -> int:
         if id(node) not in self.__id_map:
             self.__id_map[id(node)] = self.__lase_id_used
             self.__lase_id_used += 1
         return self.__id_map[id(node)]
 
-    def __gen_node_parameters(self, node: __SymbolTree) -> str:
+    def __gen_node_parameters(self, node: _SymbolTree) -> str:
         shape = ""
         fillcolor = ""
         style = ""
@@ -189,7 +132,7 @@ class SymbolTableDotGraphPass(Pass):
         label = f"{label} {shape} {style} {fillcolor}".strip()
         return f"[label={label}]"
 
-    def __gen_dot_graph(self, node: __SymbolTree) -> None:
+    def __gen_dot_graph(self, node: _SymbolTree) -> None:
         self.__dot_lines.append(
             f"{self.__gen_node_id(node)} {self.__gen_node_parameters(node)};"
         )
