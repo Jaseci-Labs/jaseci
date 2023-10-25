20ca0abcc99914f37b5ce7d0c3e714a94689899b
marsninja <jason@mars.ninja>
2023-10-25 13:34:13 -0400

    hotfix

--- jaclang/jac/passes/blue/sub_node_tab_pass.py
+++ jaclang/jac/passes/blue/sub_node_tab_pass.py
@@ -29,7 +29,7 @@ class SubNodeTabPass(Pass):
 
     def exit_module(self, node: ast.Module) -> None:
         """Update cur module."""
-        self.cur_module = node
+        self.cur_module = node.parent.mod_link if node.parent else None
 
     def exit_node(self, node: ast.AstNode) -> None:
         """Table builder."""
