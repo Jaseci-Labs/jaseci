208be0cfb6caf594b146d1f3be604f92b116b5fd
marsninja <jason@mars.ninja>
2023-11-01 07:37:55 -0400

    need different test

--- jaclang/jac/passes/blue/tests/test_jac_format_pass.py
+++ jaclang/jac/passes/blue/tests/test_jac_format_pass.py
@@ -1,8 +1,4 @@
 """Test ast build pass module."""
-import io
-import sys
-
-from jaclang.cli import cmds
 from jaclang.jac.passes.blue import JacFormatPass
 from jaclang.jac.transpiler import jac_file_to_pass
 from jaclang.utils.test import AstSyncTestMixin, TestCaseMicroSuite
@@ -38,83 +34,6 @@ class JacFormatPassTests(TestCaseMicroSuite, AstSyncTestMixin):
             self.fixture_abs_path(filename), target=JacFormatPass
         )
         self.assertGreater(len(code_gen.ir.gen.jac), 10)
-
-    # def test_circle_jac(self) -> None:
-    #     """Basic test for pass."""
-    #     captured_output = io.StringIO()
-    #     sys.stdout = captured_output
-
-    #     # Execute the function
-    #     cmds.run(
-    #         self.fixture_abs_path("../../../../../examples/manual_code/circle.jac")
-    #     )  # type: ignore
-
-    #     sys.stdout = sys.__stdout__
-    #     stdout_value = captured_output.getvalue()
-
-    #     # Assertions or verifications
-    #     self.assertIn(
-    #         "Area of a circle with radius 5 using function: 78",
-    #         stdout_value,
-    #     )
-    #     self.assertIn(
-    #         "Area of a Circle with radius 5 using class: 78",
-    #         stdout_value,
-    #     )
-
-    # def test_circle_jac_test(self) -> None:
-    #     """Basic test for pass."""
-    #     captured_output = io.StringIO()
-    #     sys.stderr = captured_output
-
-    #     # Execute the function
-    #     cmds.test(
-    #         self.fixture_abs_path("../../../../examples/manual_code/circle.jac")
-    #     )  # type: ignore
-
-    #     sys.stderr = sys.__stderr__
-    #     stderr_value = captured_output.getvalue()
-    #     # Assertions or verifications
-    #     self.assertIn("Ran 3 tests", stderr_value)
-
-    # def test_clean_circle_jac(self) -> None:
-    #     """Basic test for pass."""
-    #     captured_output = io.StringIO()
-    #     sys.stdout = captured_output
-
-    #     # Execute the function
-    #     cmds.run(
-    #         self.fixture_abs_path("../../../../examples/manual_code/circle.jac")
-    #     )  # type: ignore
-    #     stdout_value = captured_output.getvalue()
-    #     sys.stdout = sys.__stdout__
-    #     print(stdout_value)
-    #     # Assertions or verifications
-    #     self.assertIn(
-    #         "Area of a circle with radius 5 using function: 78",
-    #         stdout_value,
-    #     )
-    #     self.assertIn(
-    #         "Area of a Circle with radius 5 using class: 78",
-    #         stdout_value,
-    #     )
-
-    # def test_clean_circle_jac_test(self) -> None:
-    #     """Basic test for pass."""
-    #     captured_output = io.StringIO()
-    #     sys.stderr = captured_output
-
-    #     # Execute the function
-    #     cmds.test(
-    #         self.fixture_abs_path(
-    #             "../../../../examples/manual_code/circle_clean_tests.jac"
-    #         )
-    #     )  # type: ignore
-
-    #     sys.stderr = sys.__stderr__
-    #     stderr_value = captured_output.getvalue()
-    #     # Assertions or verifications
-    #     self.assertIn("Ran 3 tests", stderr_value)
 
 
 JacFormatPassTests.self_attach_micro_tests()
