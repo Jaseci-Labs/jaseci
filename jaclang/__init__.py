1e8e0e5be3e11c1c5b38e02dbea201f6cd816704
marsninja <jason@mars.ninja>
2023-11-12 16:03:42 -0500

    nto sure why there was a circular but whatever

--- jaclang/__init__.py
+++ jaclang/__init__.py
@@ -4,10 +4,10 @@ import sys
 
 sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), "vendor"))
 
-import jaclang.core  # noqa: F401, E402
+
 from jaclang.jac.importer import jac_import  # noqa: E402
 from jaclang.utils.helpers import handle_jac_error  # noqa: E402
-
+import jaclang.core  # noqa: F401, E402, I100
 
 __all__ = [
     "jac_import",
