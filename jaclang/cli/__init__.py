60aedcb3231a82b3336df5e255d95d4d151be26e
marsninja <jason@mars.ninja>
2023-11-23 06:36:41 +0800

    feat(corelib): Core lib plugin worksgit add .

--- jaclang/cli/__init__.py
+++ jaclang/cli/__init__.py
@@ -1,11 +1,7 @@
 """CLI for jaclang."""
 from jaclang import jac_import as jac_import
-from jaclang.jac.plugin.feature import JacFeature
 
 cli = jac_import("cli")
 cmds = jac_import("cmds")
 
 cli.cmd_registry = cmds.cmd_reg  # type: ignore
-
-
-JacFeature.pm.load_setuptools_entrypoints("jac")
