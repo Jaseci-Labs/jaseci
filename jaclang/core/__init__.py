56b788457c4efdea558b075cf341bf63135b8356
marsninja <jason@mars.ninja>
2023-12-07 04:45:33 -0500

    feat(dslib): walker/node start walk is good, need to update event registration

--- jaclang/core/__init__.py
+++ jaclang/core/__init__.py
@@ -1,10 +1,7 @@
 """Core primitives for Jaseci."""
 from jaclang import jac_import
 
-try:
-    from jaclang.core.__jac_gen__ import corelib
-except Exception:
-    corelib = jac_import("corelib")
+corelib = jac_import("corelib")
 if not corelib:
     raise ImportError("Could not import primitives, internal jaseci error")
 
