6fd3b707583b2dda782163b188371afe646153ad
marsninja <jason@mars.ninja>
2024-01-06 07:25:54 -0500

    fix(language): precedence fixed due to right recursion instead of left recursion in grammar

--- jaclang/tests/test_language.py
+++ jaclang/tests/test_language.py
@@ -1,14 +1,11 @@
-"""Test Jac cli module."""
+"""Test Jac language generally."""
 import io
-import os
 import sys
-from contextlib import redirect_stdout
-from typing import Callable, Optional
 
-import jaclang
+
 from jaclang import jac_import
 from jaclang.cli import cli
-from jaclang.compiler.transpiler import jac_file_to_pass
+from jaclang.compiler.transpiler import jac_str_to_pass
 from jaclang.core import construct
 from jaclang.utils.test import TestCase
 
@@ -111,66 +108,12 @@ class JacLanguageTests(TestCase):
         stdout_value = captured_output.getvalue()
         self.assertEqual(stdout_value.count("check"), 2)
 
-
-class JacReferenceTests(TestCase):
-    """Test Reference examples."""
-
-    test_ref_jac_files_fully_tested: Optional[Callable[[TestCase], None]] = None
-    methods: list[str] = []
-
-    @classmethod
-    def self_attach_ref_tests(cls) -> None:
-        """Attach micro tests."""
-        for filename in [
-            os.path.normpath(os.path.join(root, name))
-            for root, _, files in os.walk(
-                os.path.join(
-                    os.path.dirname(os.path.dirname(jaclang.__file__)),
-                    "examples/reference",
-                )
-            )
-            for name in files
-            if name.endswith(".jac") and not name.startswith("err")
-        ]:
-            method_name = (
-                f"test_ref_{filename.replace('.jac', '').replace(os.sep, '_')}"
-            )
-            cls.methods.append(method_name)
-            setattr(cls, method_name, lambda self, f=filename: self.micro_suite_test(f))
-
-        def test_ref_jac_files_fully_tested(self: TestCase) -> None:  # noqa: ANN001
-            """Test that all micro jac files are fully tested."""
-            for filename in cls.methods:
-                if os.path.isfile(filename):
-                    method_name = (
-                        f"test_ref_{filename.replace('.jac', '').replace(os.sep, '_')}"
-                    )
-                    self.assertIn(method_name, dir(self))
-
-        cls.test_ref_jac_files_fully_tested = test_ref_jac_files_fully_tested
-
-    def micro_suite_test(self, filename: str) -> None:
-        """Test file."""
-
-        def execute_and_capture_output(code: str) -> str:
-            f = io.StringIO()
-            with redirect_stdout(f):
-                exec(code)
-            return f.getvalue()
-
-        try:
-            code_content = jac_file_to_pass(filename).ir.gen.py
-            output_jac = execute_and_capture_output(code_content)
-
-            filename = filename.replace(".jac", ".py")
-            with open(filename, "r") as file:
-                code_content = file.read()
-            output_py = execute_and_capture_output(code_content)
-
-            self.assertGreater(len(output_py), 0)
-            self.assertEqual(output_py, output_jac)
-        except Exception as e:
-            self.skipTest(f"Test failed on {filename}: {e}")
-
-
-JacReferenceTests.self_attach_ref_tests()
+    def test_arith_precedence(self) -> None:
+        """Basic precedence test."""
+        prog = jac_str_to_pass("with entry {print(4-5-4);}", "test.jac")
+        captured_output = io.StringIO()
+        sys.stdout = captured_output
+        exec(compile(prog.ir.gen.py_ast, "test.py", "exec"))
+        sys.stdout = sys.__stdout__
+        stdout_value = captured_output.getvalue()
+        self.assertEqual(stdout_value, "-5\n")
---
+++ jaclang/tests/test_reference.py
@@ -0,0 +1,73 @@
+"""Test Jac reference examples."""
+import io
+import os
+from contextlib import redirect_stdout
+from typing import Callable, Optional
+
+import jaclang
+from jaclang.compiler.transpiler import jac_file_to_pass
+from jaclang.utils.test import TestCase
+
+
+class JacReferenceTests(TestCase):
+    """Test Reference examples."""
+
+    test_ref_jac_files_fully_tested: Optional[Callable[[TestCase], None]] = None
+    methods: list[str] = []
+
+    @classmethod
+    def self_attach_ref_tests(cls) -> None:
+        """Attach micro tests."""
+        for filename in [
+            os.path.normpath(os.path.join(root, name))
+            for root, _, files in os.walk(
+                os.path.join(
+                    os.path.dirname(os.path.dirname(jaclang.__file__)),
+                    "examples/reference",
+                )
+            )
+            for name in files
+            if name.endswith(".jac") and not name.startswith("err")
+        ]:
+            method_name = (
+                f"test_ref_{filename.replace('.jac', '').replace(os.sep, '_')}"
+            )
+            cls.methods.append(method_name)
+            setattr(cls, method_name, lambda self, f=filename: self.micro_suite_test(f))
+
+        def test_ref_jac_files_fully_tested(self: TestCase) -> None:  # noqa: ANN001
+            """Test that all micro jac files are fully tested."""
+            for filename in cls.methods:
+                if os.path.isfile(filename):
+                    method_name = (
+                        f"test_ref_{filename.replace('.jac', '').replace(os.sep, '_')}"
+                    )
+                    self.assertIn(method_name, dir(self))
+
+        cls.test_ref_jac_files_fully_tested = test_ref_jac_files_fully_tested
+
+    def micro_suite_test(self, filename: str) -> None:
+        """Test file."""
+
+        def execute_and_capture_output(code: str) -> str:
+            f = io.StringIO()
+            with redirect_stdout(f):
+                exec(code)
+            return f.getvalue()
+
+        try:
+            code_content = jac_file_to_pass(filename).ir.gen.py
+            output_jac = execute_and_capture_output(code_content)
+
+            filename = filename.replace(".jac", ".py")
+            with open(filename, "r") as file:
+                code_content = file.read()
+            output_py = execute_and_capture_output(code_content)
+
+            self.assertGreater(len(output_py), 0)
+            self.assertEqual(output_py, output_jac)
+        except Exception as e:
+            self.skipTest(f"Test failed on {filename}: {e}")
+
+
+JacReferenceTests.self_attach_ref_tests()
