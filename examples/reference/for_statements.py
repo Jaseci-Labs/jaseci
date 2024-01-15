9dcb0c9f6ff9c18c5deafa534eb618802ce7af2b
Sarujan1999 <sarujan.kugathas@jaseci.org>
2024-01-16 00:23:07 +0530

    some more python and jac outputs are checked

--- examples/reference/for_statements.py
+++ examples/reference/for_statements.py
@@ -1,10 +1,9 @@
-# Example: Printing the squares of numbers in a range
-# Range from 1 to 5 (excluding 5)
-for number in range(1, 5):
-    square = number**2
-    print(f"The square of {number} is: {square}")
-
-for i in range(1, 10, 2):
-    print(i * 5)
-else:
-    print("loop completed")
+start_range = 1
+end_range = 5
+for i in range(start_range, end_range + 1):
+    number = 1
+    while number <= 10:
+        product = i * number
+        print(f'{i} x {number} = {product}')
+        number += 1
+    print('\n', end='')
