0c61eb3d5087cda65a5ce687abfafe2739a4d2de
marsninja <jason@mars.ninja>
2024-01-31 14:42:28 -0500

    refactor: tweaked

--- jaclang/tests/test_reference.py
+++ jaclang/tests/test_reference.py
@@ -49,20 +49,25 @@ class JacReferenceTests(TestCase):
     def micro_suite_test(self, filename: str) -> None:
         """Test file."""
 
-        def execute_and_capture_output(code: str) -> str:
+        def execute_and_capture_output(code: str | bytes, filename: str = "") -> str:
             f = io.StringIO()
             with redirect_stdout(f):
-                exec(code, {})
+                exec(code, {"__file__": filename})
             return f.getvalue()
 
         try:
-            code_content = jac_file_to_pass(filename).ir.gen.py
-            output_jac = execute_and_capture_output(code_content)
+            jacast = jac_file_to_pass(filename).ir
+            code_content = compile(
+                source=jacast.gen.py_ast,
+                filename=jacast.loc.mod_path,
+                mode="exec",
+            )
+            output_jac = execute_and_capture_output(code_content, filename=filename)
 
             filename = filename.replace(".jac", ".py")
             with open(filename, "r") as file:
                 code_content = file.read()
-            output_py = execute_and_capture_output(code_content)
+            output_py = execute_and_capture_output(code_content, filename=filename)
 
             # print(f"\nJAC Output:\n{output_jac}")
             # print(f"\nPython Output:\n{output_py}")
