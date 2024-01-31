5f6ab7a3ee11415688befd9313e66e2559ce6036
marsninja <jason@mars.ninja>
2024-01-31 15:05:44 -0500

    refactor: updated jac ref examples

--- examples/reference/data_spatial_walker_statements.py
+++ examples/reference/data_spatial_walker_statements.py
@@ -1,36 +1,14 @@
-from jaclang.plugin.feature import JacFeature as jac
+from __future__ import annotations
+from jaclang.plugin.feature import JacFeature as _Jac
 
 
-@jac.make_architype("walker", on_entry=[jac.DSFunc("create", jac.RootType)], on_exit=[])
-class Creator:
-    def create(self, Creator_here: jac.RootType) -> None:
-        end = Creator_here
-        i = 0
-        while i <= 4:
-            jac.connect(
-                end,
-                (end := leaf(val=i + 1)),
-                jac.build_edge(jac.EdgeDir.OUT, None, None),
-            )
-            i += 1
-        if jac.visit_node(
-            self, jac.edge_ref(Creator_here, jac.EdgeDir.OUT, None, None)
-        ):
-            pass
+@_Jac.make_walker(on_entry=[_Jac.DSFunc("self_destruct", None)], on_exit=[])
+class Visitor:
+    def self_destruct(self, _jac_here_) -> None:
+        print("get's here")
+        _Jac.disengage(self)
+        return
+        print("but not here")
 
 
-@jac.make_architype("node", on_entry=[jac.DSFunc("print", Creator)], on_exit=[])
-class leaf:
-    val: int
-
-    def print(self, leaf_here: Creator) -> None:
-        print(f"This is {leaf_here} walk along {self}")
-        if self.val == 4:
-            print("walking stopped!!!")
-            jac.disengage(leaf_here)
-            return
-        if jac.visit_node(leaf_here, jac.edge_ref(self, jac.EdgeDir.OUT, None, None)):
-            pass
-
-
-jac.spawn_call(jac.get_root(), Creator())
+_Jac.spawn_call(_Jac.get_root(), Visitor())
