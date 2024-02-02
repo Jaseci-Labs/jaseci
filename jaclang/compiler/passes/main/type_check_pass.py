362efda9980659701cc806584e934c9652c7696e
Mohamed Gamal <m.g.t.m.prog@gmail.com>
2024-02-02 15:23:22 +0200

    remove all dep that uses jaclang

--- jaclang/compiler/passes/main/type_check_pass.py
+++ jaclang/compiler/passes/main/type_check_pass.py
@@ -85,7 +85,7 @@ class JacTypeCheckPass(Pass):
                 root_source=False,
                 ast_override=tree,
             )
-            st.dependencies = list(filter(lambda x: x != "jaclang", st.dependencies))
+            st.dependencies = list(filter(lambda x: "jaclang" not in x, st.dependencies)) 
             mypy_graph[module.name] = st
             new_modules.append(st)
 
