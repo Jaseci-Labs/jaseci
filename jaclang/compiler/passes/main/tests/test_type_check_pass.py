6b7137baa81dc9ae2cea257ad817ed0a57574508
Mohamed Gamal <m.g.t.m.prog@gmail.com>
2024-02-04 00:33:55 +0200

    Adding a new test in type_check for type checking over imported modules

--- jaclang/compiler/passes/main/tests/test_type_check_pass.py
+++ jaclang/compiler/passes/main/tests/test_type_check_pass.py
@@ -31,3 +31,18 @@ class MypyTypeCheckPassTests(TestCase):
             '(got "str", expected "int")',
         ]:
             self.assertIn(i, errs + files)
+
+    def test_imported_module_typecheck(self) -> None:
+        """Basic test for pass."""
+        type_checked = jac_file_to_pass(
+            file_path=self.fixture_abs_path("game1.jac"),
+            schedule=py_code_gen_typed,
+        )
+
+        errs = "\n".join([i.msg for i in type_checked.warnings_had])
+        files = "\n".join([i.loc.mod_path for i in type_checked.warnings_had])
+
+        for i in [
+            'Argument 2 to "is_pressed" of "Button" has incompatible type "tuple[bool, bool, bool]"; expected "str"',
+        ]:
+            self.assertIn(i, errs + files)
