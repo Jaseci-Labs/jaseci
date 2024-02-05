0a5fcd6257ae381849fd726b7cad8a44f9a5432c
marsninja <jason@mars.ninja>
2024-02-05 00:17:02 -0500

    fix: pass order

--- jaclang/compiler/passes/main/__init__.py
+++ jaclang/compiler/passes/main/__init__.py
@@ -1,15 +1,15 @@
 """Collection of passes for Jac IR."""
 
-from .def_impl_match_pass import DeclDefMatchPass
-from .def_use_pass import DefUsePass
-from .import_pass import ImportPass
-from .pyast_gen_pass import PyastGenPass
-from .pyast_load_pass import PyastBuildPass  # type: ignore
-from .pyout_pass import PyOutPass
-from .schedules import py_code_gen
 from .sub_node_tab_pass import SubNodeTabPass
-from .sym_tab_build_pass import SymTabBuildPass
-from .type_check_pass import JacTypeCheckPass
+from .import_pass import ImportPass  # noqa: I100
+from .sym_tab_build_pass import SymTabBuildPass  # noqa: I100
+from .def_impl_match_pass import DeclDefMatchPass  # noqa: I100
+from .def_use_pass import DefUsePass  # noqa: I100
+from .pyout_pass import PyOutPass  # noqa: I100
+from .pyast_load_pass import PyastBuildPass  # type: ignore # noqa: I100
+from .pyast_gen_pass import PyastGenPass  # noqa: I100
+from .schedules import py_code_gen  # noqa: I100
+from .type_check_pass import JacTypeCheckPass  # noqa: I100
 
 
 pass_schedule = py_code_gen
