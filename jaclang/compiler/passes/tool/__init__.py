6ca194340d5e48d0bbae707f2ad17db92382bda1
sivasuthan <sivasuthan.sukumar@jaseci.org>
2024-02-10 03:13:49 +0530

    refactored sym_tab,ast printers

--- jaclang/compiler/passes/tool/__init__.py
+++ jaclang/compiler/passes/tool/__init__.py
@@ -1,18 +1,9 @@
 """Collection of passes for Jac IR."""
 
-from .ast_printer_pass import AstDotGraphPass, AstPrinterPass  # noqa: I100
-from .sym_tab_printer_pass import (  # noqa: I100
-    SymbolTableDotGraphPass,
-    SymbolTablePrinterPass,
-)
 from .fuse_comments_pass import FuseCommentsPass  # noqa: I100
 from .jac_formatter_pass import JacFormatPass  # noqa: I100
 
 __all__ = [
-    "AstDotGraphPass",
-    "AstPrinterPass",
-    "SymbolTablePrinterPass",
-    "SymbolTableDotGraphPass",
     "FuseCommentsPass",
     "JacFormatPass",
 ]
