ae94303b6186fbb2cc9ddc702a32952a37e01999
kugesan1105 <kugesan.sivasothynathan@jaseci.org>
2024-02-11 19:25:08 +0530

    test added for disconnect

--- jaclang/tests/test_language.py
+++ jaclang/tests/test_language.py
@@ -240,7 +240,8 @@ class JacLanguageTests(TestCase):
         jac_import("disconn", base_path=self.fixture_abs_path("./"))
         sys.stdout = sys.__stdout__
         stdout_value = captured_output.getvalue()
-        self.assertIn("jaclang.core", stdout_value.split("\n")[0])
+        self.assertIn("c(cc=0)", stdout_value)
+        self.assertIn("c(cc=2)", stdout_value)
 
     def test_simple_archs(self) -> None:
         """Test conn assign on edges."""
