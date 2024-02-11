8bce46908f1a3b3b8898366152e27723c326dd7e
marsninja <jason@mars.ninja>
2024-02-10 21:14:30 -0500

    chore: tweaks

--- jaclang/compiler/passes/main/tests/test_pyast_build_pass.py
+++ jaclang/compiler/passes/main/tests/test_pyast_build_pass.py
@@ -18,7 +18,9 @@ class PyastBuildPassTests(TestCase):
     def test_synced_to_latest_py_ast(self) -> None:
         """Basic test for pass."""
         visit_methods = [
-            method for method in dir(py_ast._Unparser) if method.startswith("visit_")  # type: ignore
+            method
+            for method in dir(getattr(py_ast, "_Unparser"))  # noqa: B009
+            if method.startswith("visit_")
         ]
         node_names = [
             pascal_to_snake(method.replace("visit_", "")) for method in visit_methods
@@ -28,6 +30,6 @@ class PyastBuildPassTests(TestCase):
             if name.startswith("proc_") and inspect.isfunction(value):
                 pass_func_names.append(name.replace("proc_", ""))
         for name in pass_func_names:
-            self.assertIn(name, node_names)  # type: ignore
+            self.assertIn(name, node_names)
         for name in node_names:
-            self.assertIn(name, pass_func_names)  # type: ignore
+            self.assertIn(name, pass_func_names)
