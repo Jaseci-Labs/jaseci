aa2e370bfcb16624aaf0cc27bdd5b6591e7947b5
jayanaka-98 <jayanaka.dantanarayana@jaseci.org>
2024-02-22 12:04:19 +0530

    upload

---
+++ examples/rpg_game/jac_impl/jac_impl_5/test.py
@@ -0,0 +1,8 @@
+# With pipeline, just specify the task and the model id from the Hub.
+from transformers import pipeline
+pipe = pipeline("text-generation", model="distilgpt2")
+
+# If you want more control, you will need to define the tokenizer and model.
+from transformers import AutoTokenizer, AutoModelForCausalLM
+tokenizer = AutoTokenizer.from_pretrained("distilgpt2")
+model = AutoModelForCausalLM.from_pretrained("distilgpt2")
