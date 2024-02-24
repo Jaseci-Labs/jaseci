74682f0d46501f3b63e0dc3492229d62a902af4f
marsninja <jason@mars.ninja>
2024-02-24 16:30:37 -0500

    refactor: tweaks on type fusing pass

--- jaclang/compiler/passes/main/fuse_typeinfo_pass.py
+++ jaclang/compiler/passes/main/fuse_typeinfo_pass.py
@@ -12,7 +12,7 @@ from mypy.types import Instance as mypyInstance
 # import mypy.nodes as mnodes
 
 
-class FuseTypeInfo(Pass):
+class FuseTypeInfoPass(Pass):
     """Python and bytecode file printing pass."""
 
     def __debug_print(self, *argv):
@@ -25,34 +25,45 @@ class FuseTypeInfo(Pass):
                 mypy_type = mypy_node.type
                 if isinstance(mypy_type, mypyInstance):
                     node.sym_info = ast.SymbolInfo(mypy_type)
-                    self.__debug_print(f"FuseTypeInfo:: \"{node.__class__.__name__}::{node.value}\" type is \"{mypy_type}\"")
+                    self.__debug_print(
+                        f'"{node.__class__.__name__}::{node.value}"'
+                        ' type is "{mypy_type}"'
+                    )
                 else:
-                    self.__debug_print(f"FuseTypeInfo:: {type(mypy_type)} isn't supported yet")
+                    self.__debug_print(f"{type(mypy_type)} isn't supported yet")
             elif len(node.gen.mypy_ast) > 1:
-                self.__debug_print(f"FuseTypeInfo:: jac Name node \"{node.__class__.__name__}::{node.value}\" has multiple mypy nodes associated to it")
+                self.__debug_print(
+                    f'jac Name node "{node.__class__.__name__}::{node.value}"'
+                    " has multiple mypy nodes associated to it"
+                )
             else:
-                self.__debug_print(f"FuseTypeInfo:: jac Name node \"{node.__class__.__name__}::{node.value}\" doesn't have mypy node associated to it")
+                self.__debug_print(
+                    f'jac Name node "{node.__class__.__name__}::{node.value}"'
+                    " doesn't have mypy node associated to it"
+                )
         except:
-            self.__debug_print(f"FuseTypeInfo:: Internal error with \"{node.__class__.__name__}::{node}\"")
+            self.__debug_print(
+                f'Internal error with "{node.__class__.__name__}::{node}"'
+            )
 
     def enter_name(self, node: ast.Name) -> None:
         self.__handle_node(node)
-    
+
     def enter_module_path(self, node: ast.ModulePath):
         self.__handle_node(node)
-    
+
     def enter_module_item(self, node: ast.ModuleItem) -> None:
         self.__handle_node(node)
-    
+
     def enter_architype(self, node: ast.Architype) -> None:
         self.__handle_node(node)
-    
+
     def enter_arch_def(self, node: ast.ArchDef) -> None:
         self.__handle_node(node)
-    
+
     def enter_enum(self, node: ast.Enum) -> None:
         self.__handle_node(node)
-    
+
     def enter_enum_def(self, node: ast.EnumDef) -> None:
         self.__handle_node(node)
 
@@ -67,10 +78,7 @@ class FuseTypeInfo(Pass):
 
     def enter_has_var(self, node: ast.HasVar) -> None:
         self.__handle_node(node)
-    
-    def enter_multi_string(self, node: ast.MultiString) -> None:
-        self.__handle_node(node)
-    
+
     def enter_multi_string(self, node: ast.MultiString) -> None:
         self.__handle_node(node)
 
