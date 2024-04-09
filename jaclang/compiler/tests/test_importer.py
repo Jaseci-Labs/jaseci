845caeab5246c2cb310507627478a72611986b51
sivasuthan <sivasuthan.sukumar@jaseci.org>
2024-04-09 17:58:43 +0530

    test_updated

--- jaclang/compiler/tests/test_importer.py
+++ jaclang/compiler/tests/test_importer.py
@@ -1,8 +1,10 @@
 """Tests for Jac Loader."""
 
+import io
 import sys
 
 from jaclang import jac_import
+from jaclang.cli import cli
 from jaclang.utils.test import TestCase
 
 
@@ -23,3 +25,12 @@ class TestLoader(TestCase):
         jac_import("fixtures.hello_world", base_path=__file__)
         self.assertIn("module 'hello_world'", str(sys.modules))
         self.assertIn("/tests/fixtures/hello_world.jac", str(sys.modules))
+
+    def test_jac_py_import(self) -> None:
+        """Basic test for pass."""
+        captured_output = io.StringIO()
+        sys.stdout = captured_output
+        cli.run(self.fixture_abs_path("../../../tests/fixtures/jp_importer.jac"))
+        sys.stdout = sys.__stdout__
+        stdout_value = captured_output.getvalue()
+        self.assertIn("Hello World!", stdout_value)
