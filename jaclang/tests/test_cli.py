53df7373e50877743dcfbca76a05d315dbb4521a
marsninja <jason@mars.ninja>
2024-04-21 08:43:51 -0400

    fix: cleanup

--- jaclang/tests/test_cli.py
+++ jaclang/tests/test_cli.py
@@ -224,5 +224,7 @@ class JacCliTests(TestCase):
         )
         sys.stdout = sys.__stdout__
         stdout_value = captured_output.getvalue()
+        if os.path.exists("connect_expressions.dot"):
+            os.remove("connect_expressions.dot")
         self.assertIn("11\n13\n15\n>>> Graph content saved to", stdout_value)
         self.assertIn("connect_expressions.dot\n", stdout_value)
