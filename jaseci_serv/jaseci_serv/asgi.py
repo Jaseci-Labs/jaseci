871bf532473b9afca8af321bf8a942d6a7b5dfbb
Alexie (Boyong) Madolid <maex@tuta.io>
2024-04-22 22:36:15 +0800

    Revert "[FEATURE-REQUEST]: Websocket via channels[daphne]"
    This reverts commit 7470016ab15f3320aba1e8463ac60bc8cbab2457.

--- jaseci_serv/jaseci_serv/asgi.py
+++ jaseci_serv/jaseci_serv/asgi.py
@@ -8,16 +8,9 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 """
 
 import os
-from .socket.routing import websocket_urlpatterns
+
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jaseci_serv.settings")
 
-application = ProtocolTypeRouter(
-    {
-        "http": get_asgi_application(),
-        "websocket": AuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
-    }
-)
+application = get_asgi_application()
