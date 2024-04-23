b32f8b352e3eaa424d321b9f9c090ce6137f3559
marsninja <jason@mars.ninja>
2024-04-23 11:59:38 -0400

    test: tweak

--- jaclang/tests/test_language.py
+++ jaclang/tests/test_language.py
@@ -196,7 +196,7 @@ class JacLanguageTests(TestCase):
         """Parse micro jac file."""
         captured_output = io.StringIO()
         sys.stdout = captured_output
-        jac_import("hashcheck", base_path=self.fixture_abs_path("./"))
+        jac_import("hashcheck_dup", base_path=self.fixture_abs_path("./"))
         sys.stdout = sys.__stdout__
         stdout_value = captured_output.getvalue()
         self.assertEqual(stdout_value.count("check"), 2)
