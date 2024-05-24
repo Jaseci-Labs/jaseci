2b269ba0bf06868805a88cb4bd388f6e261dd60f
kugesan1105 <kugesan.sivasothynathan@jaseci.org>
2024-05-25 03:39:46 +0530

    refactor: Clean up import_pass

--- jaclang/compiler/passes/main/import_pass.py
+++ jaclang/compiler/passes/main/import_pass.py
@@ -21,9 +21,7 @@ from jaclang.utils.helpers import import_target_to_relative_path
 
 class JacImportPass(Pass):
     """Jac statically imports Jac modules."""
-
-    x = 0
-
+    x=0
     def before_pass(self) -> None:
         """Run once before pass."""
         self.import_table: dict[str, ast.Module] = {}
@@ -32,13 +30,13 @@ class JacImportPass(Pass):
         """Run Importer."""
         self.cur_node = node
         self.import_table[node.loc.mod_path] = node
-        self.__annex_impl(node)
+        self.annex_impl(node)
         self.terminate()  # Turns off auto traversal for deliberate traversal
         self.run_again = True
-        new_modpaths = self.get_all_sub_nodes(node, ast.ModulePath)
-        breakk = None
+        new_modpaths=self.get_all_sub_nodes(node, ast.ModulePath)
+        breakk=None
         while self.run_again:
-            self.x += 1
+            self.x+=1
             self.run_again = False
             # print('\n\nwhiel loop starts over !! over !!!')  #uncomment me
             # all_imports = self.get_all_sub_nodes(node, ast.ModulePath)
@@ -46,32 +44,28 @@ class JacImportPass(Pass):
             # all_imports = list(set(all_imports))
             # print(f"beofre for loop --)))) all_imports: {[i.path_str for i in all_imports]}")
             # for i in all_imports:
-            new_modpaths_dummy = []
+            new_modpaths_dummy=[]
             for i in new_modpaths:
                 # print(1)
                 # print(self.x, '.',all_imports.index(i))
                 # print(f"Importing  {i.path_str}")
-                modds = self.process_import(node, i)
+                modds=self.process_import(node, i)
                 if modds:
                     for md in modds:
                         # if md.path_str == 'types':
-                        #     print('mayiruuu i am daa')
+                        #     print('break at types i am daa')
                         #     breakk=8989
                         #     break
-                        if md.path_str in ["re", "math", "_decimal", "binascii"]:
-                            continue
+                        if md.path_str in ['re','math','_decimal','binascii']:
+                                continue
                         try:
 
                             spec = importlib.util.find_spec(md.path_str)
                         except:
                             continue
-                        if (
-                            spec
-                            and spec.origin
-                            and spec.origin not in {None, "built-in", "frozen"}
-                        ):
+                        if spec and spec.origin and spec.origin not in {None, "built-in", "frozen"}:
                             if spec.origin in self.import_table:
-                                md.sub_module = self.import_table[spec.origin]
+                                md.sub_module= self.import_table[spec.origin]
                                 continue
                         # print(self.import_table)
                         # print(f"Importing  {md.path_str}")
@@ -81,10 +75,10 @@ class JacImportPass(Pass):
                     new_modpaths_dummy.extend(modds)
                 # self.process_import(node, i)
                 self.enter_module_path(i)
-            new_modpaths = new_modpaths_dummy
+            new_modpaths=new_modpaths_dummy
             # if breakk:
             #     self.run_again = False
-            # break
+                # break
             # SubNodeTabPass(prior=self, input_ir=node)
         self.annex_impl(node)
         node.mod_deps = self.import_table
@@ -93,24 +87,24 @@ class JacImportPass(Pass):
         """Process an import."""
         lang = i.parent_of_type(ast.Import).hint.tag.value
         if lang == "jac" and not i.sub_module:
-            mod = self.import_jac_module(
+            mod = self.import_module(
                 node=i,
                 mod_path=node.loc.mod_path,
             )
             if mod:
                 self.run_again = True
-                self.__annex_impl(mod)
+                self.annex_impl(mod)
                 i.sub_module = mod
                 i.add_kids_right([mod], pos_update=False)
 
-    def __annex_impl(self, node: ast.Module) -> None:
+    def annex_impl(self, node: ast.Module) -> None:
         """Annex impl and test modules."""
         if not node.loc.mod_path:
             self.error("Module has no path")
         if node.loc.mod_path.endswith(".jac") and path.exists(
             f"{node.loc.mod_path[:-4]}.impl.jac"
         ):
-            mod = self.import_jac_mod_from_file(f"{node.loc.mod_path[:-4]}.impl.jac")
+            mod = self.import_mod_from_file(f"{node.loc.mod_path[:-4]}.impl.jac")
             if mod:
                 node.impl_mod = mod
                 node.add_kids_left([mod], pos_update=False)
@@ -118,7 +112,7 @@ class JacImportPass(Pass):
         if node.loc.mod_path.endswith(".jac") and path.exists(
             f"{node.loc.mod_path[:-4]}.test.jac"
         ):
-            mod = self.import_jac_mod_from_file(f"{node.loc.mod_path[:-4]}.test.jac")
+            mod = self.import_mod_from_file(f"{node.loc.mod_path[:-4]}.test.jac")
             if mod:
                 node.test_mod = mod
                 node.add_kids_right([mod], pos_update=False)
@@ -135,17 +129,18 @@ class JacImportPass(Pass):
             node.sub_module.name = node.alias.value
         # Items matched during def/decl pass
 
-    def import_jac_module(
-        self, node: ast.ModulePath, mod_path: str
-    ) -> ast.Module | None:
+    # Utility functions
+    # -----------------
+
+    def import_module(self, node: ast.ModulePath, mod_path: str) -> ast.Module | None:
         """Import a module."""
         self.cur_node = node  # impacts error reporting
         target = import_target_to_relative_path(
             node.level, node.path_str, path.dirname(node.loc.mod_path)
         )
-        return self.import_jac_mod_from_file(target)
+        return self.import_mod_from_file(target)
 
-    def import_jac_mod_from_file(self, target: str) -> ast.Module | None:
+    def import_mod_from_file(self, target: str) -> ast.Module | None:
         """Import a module from a file."""
         from jaclang.compiler.compile import jac_file_to_pass
         from jaclang.compiler.passes.main import SubNodeTabPass
@@ -172,26 +167,11 @@ class JacImportPass(Pass):
             self.error(f"Module {target} is not a valid Jac module.")
             return None
 
-
-class PyImportPass(JacImportPass):
-    """Jac statically imports Python modules."""
-
-    def process_import(self, node: ast.Module, i: ast.ModulePath) -> None:
-        """Process an import."""
-        lang = i.parent_of_type(ast.Import).hint.tag.value
-        if lang == "py" and not i.sub_module and settings.py_raise:
-            mod = self.import_py_module(node=i, mod_path=node.loc.mod_path)
-            if mod:
-                self.run_again = True
-                i.sub_module = mod
-                i.add_kids_right([mod], pos_update=False)
-
     def import_py_module(
         self, node: ast.ModulePath, mod_path: str
     ) -> Optional[ast.Module]:
         """Import a module."""
         from jaclang.compiler.passes.main import PyastBuildPass
-
         base_dir = path.dirname(mod_path)
         sys.path.append(base_dir)
         # print('mod_path:---> ',node.alias)
@@ -245,7 +225,7 @@ class PyImportPass(JacImportPass):
         # print(7)
         lang = i.parent_of_type(ast.Import).hint.tag.value
         if lang == "py" and not i.sub_module and settings.py_raise:
-            # if lang == "py" and not i.sub_module :
+        # if lang == "py" and not i.sub_module :
             # print(f"Importing python module {i.path_str}")
             # print(2)
             # base_dir = path.dirname(node.loc.mod_path)
@@ -264,7 +244,7 @@ class PyImportPass(JacImportPass):
             mod = self.import_py_module(node=i, mod_path=node.loc.mod_path)
             # print(3)
             if mod:
-
+                
                 # if mod
                 # print('ppppppppp==>',mod.name)
                 # if mod.name =='typing':
@@ -275,47 +255,5 @@ class PyImportPass(JacImportPass):
                 i.sub_module = mod
                 i.add_kids_right([mod], pos_update=False)
                 # return self.get_all_sub_nodes(mod, ast.ModulePath, brute_force=True)
-                xxx = self.get_all_sub_nodes(mod, ast.ModulePath, 89)
-                return [
-                    yyy
-                    for yyy in xxx
-                    if yyy.path_str
-                    not in [
-                        "grp",
-                        "_contextvars",
-                        "_bisect",
-                        "_bz2",
-                        "_lzma",
-                        "termios",
-                        "_socket",
-                        "array",
-                        "_random",
-                        "fcntl",
-                        "_posixsubprocess",
-                        "_hashlib",
-                        "_sha3",
-                        "_md5",
-                        "pyexpat",
-                        "_datetime",
-                        "_sha1",
-                        "_sha2",
-                        "_blake2",
-                        "_statistics",
-                        "_ssl",
-                        "_struct",
-                        "select",
-                        "encodings",
-                        "encodings",
-                        "zlib",
-                        "_opcode",
-                        "readline",
-                        "_pickle",
-                        "unicodedata",
-                        "importlib",
-                        "_heapq",
-                        "re",
-                        "math",
-                        "_decimal",
-                        "binascii",
-                    ]
-                ]
+                xxx=self.get_all_sub_nodes(mod, ast.ModulePath,89)
+                return [yyy for yyy in xxx if yyy.path_str not in ['grp','_contextvars','_bisect','_bz2','_lzma','termios','_socket','array','_random','fcntl','_posixsubprocess','_hashlib','_sha3','_md5','pyexpat','_datetime','_sha1','_sha2','_blake2','_statistics','_ssl','_struct','select','encodings','encodings','zlib','_opcode','readline','_pickle','unicodedata','importlib','_heapq','re','math','_decimal','binascii']]
