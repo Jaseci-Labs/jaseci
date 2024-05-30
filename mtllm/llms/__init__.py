06e00ce0d467b197fb92c7130c6fc2060842695e
chandralegend <irugalbandarachandra@gmail.com>
2024-05-30 06:10:54 +0530

    init

---
+++ mtllm/llms/__init__.py
@@ -0,0 +1,18 @@
+"""LLM implementations for MTLLM."""
+
+from .anthropic import Anthropic
+from .groq import Groq
+from .huggingface import Huggingface
+from .ollama import Ollama
+from .openai import OpenAI
+from .togetherai import TogetherAI
+
+
+__all__ = [
+    "Anthropic",
+    "Ollama",
+    "Huggingface",
+    "Groq",
+    "OpenAI",
+    "TogetherAI",
+]
