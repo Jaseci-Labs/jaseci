7e3de15c61e0e4af1149946dbbc59420e00ad977
kugesan1105 <kugesan.sivasothynathan@jaseci.org>
2024-06-02 22:07:43 +0530

    tweaked touch for now

--- jaclang/compiler/passes/ir_pass.py
+++ jaclang/compiler/passes/ir_pass.py
@@ -17,7 +17,6 @@ class Pass(Transform[T]):
         self.term_signal = False
         self.prune_signal = False
         self.ir: ast.AstNode = input_ir
-        self.touch: list[ast.AstNode] = []
         Transform.__init__(self, input_ir, prior)
 
     def before_pass(self) -> None:
@@ -105,7 +104,6 @@ class Pass(Transform[T]):
         # Only performs passes on proper ASTs
         if not isinstance(ir, ast.AstNode):
             return ir
-        self.touch = []
         self.before_pass()
         if not isinstance(ir, ast.AstNode):
             raise ValueError("Current node is not an AstNode.")
@@ -121,8 +119,7 @@ class Pass(Transform[T]):
         self.enter_node(node)
         if not self.prune_signal:
             for i in node.kid:
-                if i and (i not in self.touch or isinstance(i, ast.AbilityDef)):
-                    self.touch.append(i)
+                if i:
                     self.traverse(i)
         else:
             self.prune_signal = False
