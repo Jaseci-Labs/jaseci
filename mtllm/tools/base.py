0ed5f7e85813f96bf16e9098c84c305c0f41582d
chandralegend <irugalbandarachandra@gmail.com>
2024-06-04 17:36:52 +0530

    chore: Update release workflow to use pip install for githubrelease and run python script for release

--- mtllm/tools/base.py
+++ mtllm/tools/base.py
@@ -1,22 +1,35 @@
+"""Tools for the MTLLM framework."""
+
+from typing import Callable
+
+import wikipedia as wikipedia_utils
+
+
 class Tool:
+    """Base class for tools."""
+
     description: str = "Description of the tool"
     inputs: list[tuple[str, str, str]] = [("input1", "str", "Input 1 description")]
     output: tuple[str, str] = ("str", "Output description")
 
-    def forward(self, input1: str) -> str:
+    def forward(self, *args, **kwargs) -> str:  # noqa
+        """Forward function of the tool."""
         raise NotImplementedError
-    
-    def get_function(self):
+
+    def get_function(self) -> Callable:
+        """Return the forward function of the tool."""
         return self.forward
 
 
-import wikipedia   
+class wikipedia(Tool):  # noqa
+    """Wikipedia tool."""
 
-class wikipedia(Tool):
     description: str = "Get the summary of a Wikipedia article"
-    inputs: list[tuple[str, str, str]] = [("title", "str", "Title of the Wikipedia article")]
+    inputs: list[tuple[str, str, str]] = [
+        ("title", "str", "Title of the Wikipedia article")
+    ]
     output: tuple[str, str] = ("str", "Summary of the Wikipedia article")
 
     def forward(self, title: str) -> str:
-        return wikipedia.summary(title)
-    
+        """Get the summary of a Wikipedia article."""
+        return wikipedia_utils.summary(title)
---
+++ scripts/gh_release.py
@@ -0,0 +1,19 @@
+"""GH Release script for MTLLM."""
+
+from github_release import gh_release_create
+
+import tomllib
+
+with open("pyproject.toml", "rb") as f:
+    data = tomllib.load(f)
+
+version = data["tool"]["poetry"]["version"]
+
+gh_release_create(
+    "chandralegend/mttlm ",
+    version,
+    publish=True,
+    name=f"MTLLM {version}",
+    asset_pattern="dist/*",
+    body="Release notes for MTLLM",
+)
