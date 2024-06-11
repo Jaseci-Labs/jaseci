453c15969aa03fb874ab0238e168398ac8e5b7ba
marsninja <jason@mars.ninja>
2024-06-10 21:59:36 -0400

    feat: feature baselines

--- jaclang/langserve/server.py
+++ jaclang/langserve/server.py
@@ -11,6 +11,13 @@ import lsprotocol.types as lspt
 server = JacLangServer()
 
 
+@server.feature(lspt.TEXT_DOCUMENT_DID_OPEN)
+async def did_open(ls: JacLangServer, params: lspt.DidOpenTextDocumentParams) -> None:
+    """Check syntax on change."""
+    ls.quick_check(params.text_document.uri)
+    ls.push_diagnostics(params.text_document.uri)
+
+
 @server.feature(lspt.TEXT_DOCUMENT_DID_CHANGE)
 @debounce(0.1)
 async def did_change(
@@ -19,6 +26,28 @@ async def did_change(
     """Check syntax on change."""
     ls.quick_check(params.text_document.uri)
     ls.push_diagnostics(params.text_document.uri)
+
+
+@server.feature(lspt.TEXT_DOCUMENT_DID_SAVE)
+async def did_save(ls: JacLangServer, params: lspt.DidSaveTextDocumentParams) -> None:
+    """Check syntax on save."""
+    ls.quick_check(params.text_document.uri)
+    ls.push_diagnostics(params.text_document.uri)
+
+
+@server.feature(
+    lspt.WORKSPACE_DID_CREATE_FILES,
+    lspt.FileOperationRegistrationOptions(
+        filters=[
+            lspt.FileOperationFilter(pattern=lspt.FileOperationPattern("**/*.jac"))
+        ]
+    ),
+)
+async def did_create_files(ls: JacLangServer, params: lspt.CreateFilesParams) -> None:
+    """Check syntax on file creation."""
+    for file in params.files:
+        ls.quick_check(file.uri)
+        ls.push_diagnostics(file.uri)
 
 
 @server.feature(lspt.TEXT_DOCUMENT_FORMATTING)
