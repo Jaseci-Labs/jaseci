88c1e7027ed9b06be6e8ca567abc36df23145305
marsninja <jason@mars.ninja>
2024-06-21 11:06:41 -0400

    improv: now no doubles in vscode

--- jaclang/compiler/passes/transform.py
+++ jaclang/compiler/passes/transform.py
@@ -40,7 +40,7 @@ class Transform(ABC, Generic[T]):
         prior: Optional[Transform] = None,
     ) -> None:
         """Initialize pass."""
-        self.logger = logging.getLogger(self.__class__.__module__)
+        self.logger = logging.getLogger(self.__class__.__name__)
         self.errors_had: list[Alert] = [] if not prior else prior.errors_had
         self.warnings_had: list[Alert] = [] if not prior else prior.warnings_had
         self.cur_node: AstNode = input_ir  # tracks current node during traversal
