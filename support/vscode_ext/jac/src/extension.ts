e17f27f8fbfaef6829d9dc1b05f9565a9b5a6196
marsninja <jason@mars.ninja>
2024-06-22 13:34:30 -0400

    ver: bump vscode ext

--- support/vscode_ext/jac/src/extension.ts
+++ support/vscode_ext/jac/src/extension.ts
@@ -11,14 +11,29 @@ let client: LanguageClient;
 function getCondaEnvironment(): string | undefined {
     const condaPath = process.env.CONDA_PREFIX;
     if (condaPath) {
-        return path.join(condaPath, 'bin', 'jac');
+        const jacPath = path.join(condaPath, 'bin', 'jac');
+        if (fs.existsSync(jacPath)) {
+            return jacPath;
+        }
+    }
+    return undefined;
+}
+
+function getVenvEnvironment(): string | undefined {
+    const venvPath = process.env.VIRTUAL_ENV;
+    if (venvPath) {
+        const jacPath = path.join(venvPath, 'bin', 'jac');
+        if (fs.existsSync(jacPath)) {
+            return jacPath;
+        }
     }
     return undefined;
 }
 
 export function activate(context: vscode.ExtensionContext) {
     const condaJac = getCondaEnvironment();
-    const jacCommand = condaJac ? condaJac : 'jac';
+    const venvJac = getVenvEnvironment();
+    const jacCommand = condaJac ? condaJac : (venvJac ? venvJac : 'jac');
 
     let serverOptions: ServerOptions = {
         run: { command: jacCommand, args: ["lsp"] },
