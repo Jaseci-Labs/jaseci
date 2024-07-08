e100df7dc60799edc195c2a0fcc237813ce95d5e
chandralegend <irugalbandarachandra@gmail.com>
2024-07-08 20:15:51 +0530

    chore: Refactor dataset preparation function

--- support/finetune_llm/utils/dataset.py
+++ support/finetune_llm/utils/dataset.py
@@ -10,10 +10,10 @@ def formatted_train(input: str, response: str) -> str:
 
 def prepare_train_data(dataset: str) -> Dataset:
     """Prepare the training data for the MTLLM model."""
-    dataset = load_dataset(dataset)
-    dataset_df = dataset.to_pandas()
+    _dataset = load_dataset(dataset)
+    dataset_df = _dataset.to_pandas()
     dataset_df["text"] = dataset_df[["meaning_in", "meaning_out"]].apply(
         lambda x: formatted_train(x["meaning_in"], x["meaning_out"]), axis=1
     )
-    dataset = Dataset.from_pandas(dataset_df)
-    return dataset
+    _dataset_ = Dataset.from_pandas(dataset_df)
+    return _dataset_
