ded9b1ec80b9d2083536f0e26f646319bc5c6ebd
chandralegend <irugalbandarachandra@gmail.com>
2024-07-26 17:37:11 +0000

    These changes aim to improve the training process and fine-tuning of the MTLLM model.

--- support/finetune_llm/train.py
+++ support/finetune_llm/train.py
@@ -13,16 +13,25 @@ from utils.dataset import prepare_train_data
 from utils.merge_n_push import merge_n_push
 from utils.model import get_model_tokenizer
 
+seed = 42
+
 
 def train(config: argparse.Namespace) -> None:
     """Train the model on the given dataset."""
     train_data = prepare_train_data(config.model["hf_dataset"])
+    train_eval_data = train_data.train_test_split(test_size=0.2, seed=seed)
+    train_data = train_eval_data["train"]
+    eval_data = train_eval_data["test"]
+
     model, tokenizer = get_model_tokenizer(config.model["hf_model"])
+
     peft_config = LoraConfig(**config.lora_config)
-    training_args = TrainingArguments(**config.training_args)
+    training_args = TrainingArguments(output_dir= config.model["output_model"], **config.training_args)
+
     trainer = SFTTrainer(
         model=model,
         train_dataset=train_data,
+        eval_dataset=eval_data,
         peft_config=peft_config,
         args=training_args,
         tokenizer=tokenizer,
@@ -42,9 +51,8 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         "--push_to_hf",
-        type=bool,
-        default=False,
-        help="Push the model to the Hugging Face model hub",
+        action="store_true",
+        help="Push the trained model to the Hugging Face Hub",
     )
     args = parser.parse_args()
     config = load_config(args.config)
--- support/finetune_llm/utils/dataset.py
+++ support/finetune_llm/utils/dataset.py
@@ -2,18 +2,25 @@
 
 from datasets import Dataset, load_dataset
 
+import pandas as pd
+
 
 def formatted_train(input: str, response: str) -> str:
     """Format the input and response into the chat prompt format."""
     return f"{input}{response}"
 
 
-def prepare_train_data(dataset: str) -> Dataset:
+def prepare_train_data(datasets: list[str]) -> Dataset:
     """Prepare the training data for the MTLLM model."""
-    _dataset = load_dataset(dataset)
-    dataset_df = _dataset.to_pandas()
+    dataset_df: pd.DataFrame = None
+    for dataset in datasets:
+        _dataset = load_dataset(dataset)
+        if dataset_df is None:
+            dataset_df = _dataset["train"].to_pandas()
+        else:
+            dataset_df = pd.concat([dataset_df, _dataset["train"].to_pandas()])
     dataset_df["text"] = dataset_df[["input_prompt", "output_prompt"]].apply(
         lambda x: formatted_train(x["input_prompt"], x["output_prompt"]), axis=1
     )
     _dataset_ = Dataset.from_pandas(dataset_df)
-    return _dataset_
+    return _dataset_.shuffle()
