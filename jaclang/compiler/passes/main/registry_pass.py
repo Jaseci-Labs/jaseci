d6b2a8cb3f1aaf962989e598165717e7db403e53
Jayanaka-98 <jayanaka.dantanarayana@jaseci.org>
2024-08-11 18:44:30 +0530

    Update

--- jaclang/compiler/passes/main/registry_pass.py
+++ jaclang/compiler/passes/main/registry_pass.py
@@ -38,8 +38,6 @@ class RegistryPass(Pass):
                 os.path.join(module_dir, f"{module_name}.registry.pkl"), "wb"
             ) as f:
                 pickle.dump(node.registry, f)
-                print(node.registry.pp())
-                # print(node.registry.registry[list(node.registry.registry.keys())[2]])
         except Exception as e:
             self.warning(f"Can't save registry for {module_name}: {e}")
         self.modules_visited.pop()
