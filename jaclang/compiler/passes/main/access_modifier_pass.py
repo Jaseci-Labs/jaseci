c658886c37fe0e7036260dd7b113fd77ab65848f
Thakee Nathees <thakeenathees@gmail.com>
2024-08-16 16:18:04 +0530

    test cases were added and minor codechanges

--- jaclang/compiler/passes/main/access_modifier_pass.py
+++ jaclang/compiler/passes/main/access_modifier_pass.py
@@ -15,6 +15,25 @@ from jaclang.settings import settings
 class AccessCheckPass(Pass):
     """Jac Ast Access Check pass."""
 
+    # NOTE: This method is a hacky way to detect if the drivied class is inherit from base class, it
+    # doesn't work if the base class was provided as an expression (ex. obj Dri :module.Base: {...}).
+    def is_class_inherited_from(
+        self, dri_class: ast.Architype, base_class: ast.Architype
+    ) -> bool:
+        """Return true if the dri_class inherited from base_class."""
+        if dri_class.base_classes is None:
+            return False
+        for expr in dri_class.base_classes.items:
+            if not isinstance(expr, ast.Name):
+                continue
+            if not isinstance(expr.name_of, ast.Architype):
+                continue  # Unlikely.
+            if expr.name_of == base_class:
+                return True
+            if self.is_class_inherited_from(expr.name_of, base_class):
+                return True
+        return False
+
     def report_error(self, message: str, node: Optional[ast.AstNode] = None) -> None:
         """Report error message related to illegal access of attributes and objects."""
         self.error(message, node)
@@ -142,7 +161,15 @@ class AccessCheckPass(Pass):
             return
 
         # Class name, and ability name are declarations and there is no access here as well.
-        if isinstance(node.name_of, (ast.Ability, ast.Architype)):
+        if isinstance(node.name_of, (ast.Ability, ast.Architype, ast.Enum)):
+            return
+
+        # Get the context to check the access.
+        curr_class: Optional[ast.Architype] = Pass.find_parent_of_type(
+            node, ast.Architype
+        )
+        curr_module: Optional[ast.Module] = Pass.find_parent_of_type(node, ast.Module)
+        if curr_module is None:
             return
 
         # Note that currently we can only check for name + symbols, because expressions are not associated with the
@@ -159,11 +186,6 @@ class AccessCheckPass(Pass):
                 return
 
             if access_obj.sym.sym_type == SymbolType.MODULE:
-                curr_module: Optional[ast.Module] = Pass.find_parent_of_type(
-                    node=node, typ=ast.Module
-                )
-                if curr_module is None:
-                    return
                 accessed_module: Optional[ast.Module] = None
                 for mod_dep in curr_module.mod_deps.values():
                     if mod_dep.name == access_obj.sym.sym_name:
@@ -174,7 +196,7 @@ class AccessCheckPass(Pass):
 
                 symbol: Optional[Symbol] = accessed_module.sym_tab.lookup(node.value)
                 if symbol is None:
-                    # TODO: This is a symantic error, assuming that a non
+                    # NOTE: This is a symantic error, assuming that a non
                     # existing member access was reported by some other
                     # semantic analysis pass, as it's not the responsibility
                     # of the current pass.
@@ -191,6 +213,8 @@ class AccessCheckPass(Pass):
             # node without symbol, we're just returning here for now.
             return
 
+        # From here (bellow) the node has a valid symbol.
+
         # Public symbols are fine.
         if node.sym.access == SymbolAccess.PUBLIC:
             return
@@ -199,57 +223,36 @@ class AccessCheckPass(Pass):
         is_portect = node.sym.access == SymbolAccess.PROTECTED
         access_type = "protected" if is_portect else "private"
 
-        # Check if private member variable / ability is accessed outside of the class.
-        if node.sym.sym_type in (SymbolType.HAS_VAR, SymbolType.ABILITY):
+        # The class we're currently in (None if we're not inside any).
+        sym_owner: ast.AstNode = node.sym.parent_tab.owner
 
-            curr_class: Optional[ast.Architype] = Pass.find_parent_of_type(
-                node=node, typ=ast.Architype
-            )
-            member_type: str = (
-                "variable" if node.sym.sym_type == SymbolType.HAS_VAR else "ability"
-            )
+        # If the symbol belongs to a class, we need to check if the access used properly
+        # within the class and in it's inherited classes.
+        if isinstance(sym_owner, ast.Architype):
 
-            # Accessing a private member outside of a class.
+            # Accessing a private/protected member within the top level scope illegal.
             if curr_class is None:
                 return self.report_error(
-                    f"Error: Invalid access of {access_type} member {member_type} variable.",
+                    f"Error: Invalid access of {access_type} member.",
                     node,
                 )
 
-            # Accessing a private member variable from a different or even inherited class.
-            assert isinstance(node.sym.parent_tab.owner, ast.Architype)
             if curr_class != node.sym.parent_tab.owner:
-                if is_portect:
-
-                    # NOTE: This method is a hacky way to detect if the drivied class is inherit from base class, it
-                    # doesn't work if the base class was provided as an expression (ex. obj Dri :module.Base: {...}).
-                    # TODO: This function may be exists somewhere else (I cannot find) or else moved to somewhere
-                    # common.
-                    def is_class_inherited_from(
-                        dri_class: ast.Architype, base_class: ast.Architype
-                    ) -> bool:
-                        if dri_class.base_classes is None:
-                            return False
-                        for expr in dri_class.base_classes.items:
-                            if not isinstance(expr, ast.Name):
-                                continue
-                            if not isinstance(expr.name_of, ast.Architype):
-                                continue  # Unlikely.
-                            if expr.name_of == base_class:
-                                return True
-                            if is_class_inherited_from(expr.name_of, base_class):
-                                return True
-                        return False
-
-                    if not is_class_inherited_from(
-                        curr_class, node.sym.parent_tab.owner
-                    ):
-                        return self.report_error(
-                            f"Error: Invalid access of {access_type} member {member_type}.",
-                            node,
-                        )
-                else:
+                if not is_portect:  # private member accessed in a different class.
                     return self.report_error(
-                        f"Error: Invalid access of {access_type} member {member_type}.",
+                        f"Error: Invalid access of {access_type} member.",
                         node,
                     )
+                else:  # Accessing a protected member, check we're in an inherited class.
+                    if not self.is_class_inherited_from(curr_class, sym_owner):
+                        return self.report_error(
+                            f"Error: Invalid access of {access_type} member.",
+                            node,
+                        )
+
+        elif isinstance(sym_owner, ast.Module) and sym_owner != curr_module:
+            # Accessing a private/public member in a different module.
+            return self.report_error(
+                f"Error: Invalid access of {access_type} member.",
+                node,
+            )
