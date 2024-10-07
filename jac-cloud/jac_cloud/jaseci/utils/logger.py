23f499a8530f94ca1f9eaeac601c91a6a90296f8
Alexie (Boyong) Madolid <maex@tuta.io>
2024-10-07 20:56:52 +0800

    [IMPROVEMENT]: Added namespace and auto deletion based on backup_count

--- jac-cloud/jac_cloud/jaseci/utils/logger.py
+++ jac-cloud/jac_cloud/jaseci/utils/logger.py
@@ -3,21 +3,26 @@
 from datetime import time as dtime
 from enum import IntEnum
 from io import text_encoding
+from itertools import chain
+from json import dumps
 from logging import FileHandler, LogRecord, getLogger
 from logging.handlers import (
     BaseRotatingHandler,
     RotatingFileHandler,
     TimedRotatingFileHandler,
 )
-from os import getenv, remove, rename, stat
-from os.path import exists, isfile
+from os import getenv, remove, stat
+from os.path import exists, getmtime, isfile
 from pathlib import Path
-from re import ASCII, compile
+from re import ASCII, compile, escape
 from stat import ST_MTIME
 from time import gmtime, localtime, strftime, time as ttime
 from typing import Any
 
 from ecs_logging import StdlibFormatter
+
+DEFAULT_PART = [0]
+DEFAULT_SEPARATORS = (",", ":")
 
 
 class MixedTimedRotatingFileHandler(TimedRotatingFileHandler, RotatingFileHandler):
@@ -42,9 +47,14 @@ class MixedTimedRotatingFileHandler(TimedRotatingFileHandler, RotatingFileHandle
         errors: str | None = None,
     ) -> None:
         """Override __init__."""
-        encoding = text_encoding(encoding)
+        Path(filename).parent.mkdir(parents=True, exist_ok=True)
         BaseRotatingHandler.__init__(
-            self, filename, "a", encoding=encoding, delay=delay, errors=errors
+            self,
+            filename,
+            "a",
+            encoding=text_encoding(encoding),
+            delay=delay,
+            errors=errors,
         )
 
         file_path = Path(self.baseFilename)
@@ -57,22 +67,26 @@ class MixedTimedRotatingFileHandler(TimedRotatingFileHandler, RotatingFileHandle
         self.backupCount = backup_count
         self.utc = utc
         self.atTime = at_time
+
+        re_format = escape(self.file_name) + r"-[0-9]+" + escape(self.file_ext) + "$"
         if self.when == "S":
             self.interval = 1
-            self.suffix = "%Y-%m-%d-%H-%M-%S"
-            ext_match = r"(?<!\d)\d{4}-\d{2}-\d{2}_\d{2}-\d{2}-\d{2}(?!\d)"
+            self.prefix = "%Y-%m-%d-%H-%M-%S"
+            ext_match = (
+                r"^(?<!\d)\d{4}-\d{2}-\d{2}-\d{2}-\d{2}-\d{2}(?!\d)-" + re_format
+            )
         elif self.when == "M":
             self.interval = 60
-            self.suffix = "%Y-%m-%d-%H-%M"
-            ext_match = r"(?<!\d)\d{4}-\d{2}-\d{2}_\d{2}-\d{2}(?!\d)"
+            self.prefix = "%Y-%m-%d-%H-%M"
+            ext_match = r"^(?<!\d)\d{4}-\d{2}-\d{2}-\d{2}-\d{2}(?!\d)-" + re_format
         elif self.when == "H":
             self.interval = 60 * 60
-            self.suffix = "%Y-%m-%d-%H"
-            ext_match = r"(?<!\d)\d{4}-\d{2}-\d{2}_\d{2}(?!\d)"
+            self.prefix = "%Y-%m-%d-%H"
+            ext_match = r"^(?<!\d)\d{4}-\d{2}-\d{2}-\d{2}(?!\d)-" + re_format
         elif self.when == "D" or self.when == "MIDNIGHT":
             self.interval = 60 * 60 * 24
-            self.suffix = "%Y-%m-%d"
-            ext_match = r"(?<!\d)\d{4}-\d{2}-\d{2}(?!\d)"
+            self.prefix = "%Y-%m-%d"
+            ext_match = r"^(?<!\d)\d{4}-\d{2}-\d{2}(?!\d)-" + re_format
         elif self.when.startswith("W"):
             self.interval = 60 * 60 * 24 * 7
             if len(self.when) != 2:
@@ -84,8 +98,8 @@ class MixedTimedRotatingFileHandler(TimedRotatingFileHandler, RotatingFileHandle
                     f"Invalid day specified for weekly rollover: {self.when}"
                 )
             self.dayOfWeek = int(self.when[1])
-            self.suffix = "%Y-%m-%d"
-            ext_match = r"(?<!\d)\d{4}-\d{2}-\d{2}(?!\d)"
+            self.prefix = "%Y-%m-%d"
+            ext_match = r"^(?<!\d)\d{4}-\d{2}-\d{2}(?!\d)-" + re_format
         else:
             raise ValueError(f"Invalid rollover interval specified: {self.when}")
 
@@ -96,10 +110,40 @@ class MixedTimedRotatingFileHandler(TimedRotatingFileHandler, RotatingFileHandle
             t = stat(filename)[ST_MTIME]
         else:
             t = ttime()
+        self.has_buffer = False
         self.rolloverAt = self.computeRollover(int(t))
 
-    def build_file_name(self, prefix: str, part: int) -> str:
+    def pull_buffer(self) -> int:
+        """Pull buffer."""
+        if self.has_buffer:
+            self.has_buffer = False
+            return self.interval
+        return 0
+
+    def build_file_name(self, prefix: str) -> str:
         """Override rotation_filename."""
+        part_extractor = compile(
+            r"^"
+            + escape(f"{prefix}-{self.file_name}-")
+            + r"([0-9]+)"
+            + escape(self.file_ext)
+            + r"$"
+        )
+
+        part = (
+            max(
+                chain(
+                    (
+                        int(match.group(1))
+                        for path in self.file_folder.iterdir()
+                        if (match := part_extractor.match(path.name))
+                    ),
+                    DEFAULT_PART,
+                )
+            )
+            + 1
+        )
+
         name = f"{self.file_folder}/{prefix}-{self.file_name}-{part}{self.file_ext}"
 
         if not callable(self.namer):
@@ -107,10 +151,27 @@ class MixedTimedRotatingFileHandler(TimedRotatingFileHandler, RotatingFileHandle
         else:
             return self.namer(name)
 
-    def doRollover(self) -> None:  # noqa: N802
+    def remove_old_backup(self) -> None:
+        """Remove old backup files."""
+        if self.backupCount > 0:
+            backups = sorted(
+                (
+                    path
+                    for path in self.file_folder.iterdir()
+                    if self.extMatch.match(path.name)
+                ),
+                key=getmtime,
+            )
+
+            if self.backupCount < (backup_count := len(backups)):
+                for backup in backups[0 : backup_count - self.backupCount]:
+                    remove(backup)
+
+    def doRollover(self, record: LogRecord) -> None:  # type: ignore[override] # noqa: N802
         """Override doRollover."""
-        current_time = int(ttime())
-        t = self.rolloverAt - self.interval
+        current_time = int(record.created)
+        t = self.rolloverAt - (self.interval + self.pull_buffer())
+
         if self.utc:
             time_tuple = gmtime(t)
         else:
@@ -123,38 +184,21 @@ class MixedTimedRotatingFileHandler(TimedRotatingFileHandler, RotatingFileHandle
                 else:
                     addend = -3600
                 time_tuple = localtime(t + addend)
-        prefix = strftime(self.suffix, time_tuple)
+        prefix = strftime(self.prefix, time_tuple)
 
         if self.stream:
             self.stream.close()
             self.stream = None  # type: ignore[assignment]
-        if self.backupCount > 0:
-            for i in range(self.backupCount - 1, 0, -1):
-                sfn = self.build_file_name(prefix, i)
-                dfn = self.build_file_name(prefix, i + 1)
-                if exists(sfn):
-                    if exists(dfn):
-                        remove(dfn)
-                    rename(sfn, dfn)
-            dfn = self.build_file_name(prefix, 1)
-            if exists(dfn):
-                remove(dfn)
-            self.rotate(self.baseFilename, dfn)
-        else:
-            part = 1
-            while True:
-                dfn = self.build_file_name(prefix, part)
-                if not exists(dfn):
-                    self.rotate(self.baseFilename, dfn)
-                    break
-                part += 1
+
+        self.rotate(self.baseFilename, self.build_file_name(prefix))
+        self.remove_old_backup()
 
         if not self.delay:
             self.stream = self._open()
 
     def shouldRollover(self, record: LogRecord) -> bool:  # noqa: N802
         """Override shouldRollover."""
-        t = int(ttime())
+        t = int(record.created)
         if exists(self.baseFilename) and not isfile(self.baseFilename):
             self.rolloverAt = self.computeRollover(t)
             return False
@@ -166,9 +210,28 @@ class MixedTimedRotatingFileHandler(TimedRotatingFileHandler, RotatingFileHandle
             if self.stream.tell() + len(msg) >= self.maxBytes:
                 return True
         if t >= self.rolloverAt:
+            self.has_buffer = True
             self.rolloverAt = self.computeRollover(t)
             return True
         return False
+
+    def computeRollover(self, currentTime: int) -> int:  # noqa: N802, N803
+        """Override compute roll over to adjust buffer."""
+        return super().computeRollover(currentTime) - int(currentTime % self.interval)
+
+    def emit(self, record: LogRecord) -> None:
+        """
+        Emit a record.
+
+        Output the record to the file, catering for rollover as described
+        in doRollover().
+        """
+        try:
+            if self.shouldRollover(record):
+                self.doRollover(record)
+            FileHandler.emit(self, record)
+        except Exception:
+            self.handleError(record)
 
 
 class Level(IntEnum):
@@ -187,19 +250,15 @@ class Level(IntEnum):
 logger = getLogger(getenv("LOGGER_NAME", "app"))
 logger.setLevel(Level[getenv("LOGGER_LEVEL", "DEBUG")].value)
 
-if getenv("ELASTIC_LOGGER") == "true":
-    handler = FileHandler(getenv("LOGGER_FILE_PATH", "elastic-log.json"))
-    handler.setFormatter(StdlibFormatter())
-    logger.addHandler(handler)
-else:
-    handler = MixedTimedRotatingFileHandler(
-        getenv("LOGGER_FILE_PATH", "jac-cloud.log"),
-        when="d",
-        backup_count=int(getenv("LOGGER_DAILY_MAX_FILE", "-1")),
-        max_bytes=int(getenv("LOGGER_MAX_FILE_SIZE", "10000000")),
-    )
-    handler.setFormatter(StdlibFormatter())
-    logger.addHandler(handler)
+handler = MixedTimedRotatingFileHandler(
+    getenv("LOGGER_FILE_PATH", "/tmp/jac_cloud_logs/jac-cloud.log"),
+    when=getenv("LOGGER_TIME_INTERVAL", "d"),
+    backup_count=int(getenv("LOGGER_MAX_BACKUP", "5")),
+    max_bytes=int(getenv("LOGGER_MAX_FILE_SIZE", "10000000")),
+    utc=getenv("LOGGER_USE_UTC") == "true",
+)
+handler.setFormatter(StdlibFormatter())
+logger.addHandler(handler)
 
 
 def log_entry(
@@ -209,7 +268,7 @@ def log_entry(
     log: dict[str, Any] = {
         "api_name": api,
         "caller_name": caller,
-        "payload": payload,
+        "payload": dumps(payload, separators=DEFAULT_SEPARATORS),
         "entry_node": node,
     }
     msg = str(
@@ -227,7 +286,7 @@ def log_entry(
 def log_exit(response: dict[str, Any], log: dict[str, Any] | None = None) -> None:
     """Log metadata on exit."""
     log = log or {}
-    log["api_response"] = response
+    log["api_response"] = dumps(response, separators=DEFAULT_SEPARATORS)
     log["extra_fields"] = list(log.keys())
     log_msg = str(
         f"Returning call from {log["caller_name"]}"
