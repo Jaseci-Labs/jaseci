8d0d2eb68f34ea805d475c56c524adbccd75f008
marsninja <jason@mars.ninja>
2024-10-09 21:02:20 -0400

    fix: typechecks now fast

--- jac/stubs/jaclang/compiler/passes/tool/__init__.pyi
+++
@@ -1,4 +0,0 @@
-from .fuse_comments_pass import FuseCommentsPass as FuseCommentsPass
-from .jac_formatter_pass import JacFormatPass as JacFormatPass
-
-__all__ = ["FuseCommentsPass", "JacFormatPass"]
--- jac/stubs/jaclang/plugin/__init__.pyi
+++
@@ -1,3 +0,0 @@
-from .default import Architype as Architype, DSFunc as DSFunc, hookimpl as hookimpl
-
-__all__ = ["Architype", "DSFunc", "hookimpl"]
