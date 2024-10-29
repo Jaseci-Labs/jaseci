bc49f3105a2a7c999560d549ae64aea62eecc03d
jayanaka-98 <jayanaka@umich.edu>
2024-10-29 13:14:55 -0400

    machine.py changes to accomadate SemIR in the jacProgram

--- jac/jaclang/runtimelib/machine.py
+++ jac/jaclang/runtimelib/machine.py
@@ -1,5 +1,6 @@
 """Jac Machine module."""
 
+import __future__ import annotations
 import inspect
 import marshal
 import os
@@ -12,6 +13,7 @@ from typing import Optional, Union
 from jaclang.compiler.absyntree import Module
 from jaclang.compiler.compile import compile_jac
 from jaclang.compiler.constant import Constants as Con
+from jaclang.compiler.semtable import SemRegistry
 from jaclang.runtimelib.architype import (
     Architype,
     EdgeArchitype,
@@ -69,6 +71,11 @@ class JacMachine:
                 module_name, full_target, caller_dir, cachable, reload=reload
             )
         return None
+
+    def get_SemIR(self, SemIR: SemRegistry | None) -> None:
+        """Update semtable on the attached JacProgram."""
+        if self.jac_program and SemIR:
+            self.jac_program.SemIR = SemIR
 
     def load_module(self, module_name: str, module: types.ModuleType) -> None:
         """Load a module into the machine."""
@@ -263,14 +270,18 @@ class JacMachine:
 
 
 class JacProgram:
-    """Class to hold the mod_bundle and bytecode for Jac modules."""
+    """Class to hold the mod_bundle bytecode and SemIR for Jac modules."""
 
     def __init__(
-        self, mod_bundle: Optional[Module], bytecode: Optional[dict[str, bytes]]
+        self,
+        mod_bundle: Optional[Module],
+        bytecode: Optional[dict[str, bytes]],
+        SemIR: Optional[SemRegistry]
     ) -> None:
         """Initialize the JacProgram object."""
         self.mod_bundle = mod_bundle
         self.bytecode = bytecode or {}
+        self.SemIR = SemIR
 
     def get_bytecode(
         self,
