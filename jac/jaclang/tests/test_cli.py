02cc3b09501a464acbed4fd7bc678b166d52eba4
marsninja <jason@mars.ninja>
2024-11-01 12:05:38 -0400

    test: update

--- jac/jaclang/tests/test_cli.py
+++ jac/jaclang/tests/test_cli.py
@@ -280,7 +280,7 @@ class JacCliTests(TestCase):
         stdout_value = captured_output.getvalue()
         self.assertEqual(stdout_value.count("type_info.ServerWrapper"), 7)
         self.assertEqual(stdout_value.count("builtins.int"), 3)
-        self.assertEqual(stdout_value.count("builtins.str"), 8)
+        self.assertEqual(stdout_value.count("builtins.str"), 10)
         self.assertIn("Literal['test_server']", stdout_value)
         self.assertIn("Literal['1']", stdout_value)
 
