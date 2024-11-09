7268ee9934a7ba93fae8055716f6c0b0ab5488d6
Jakob Louis Therkelsen <therkels@msu.edu>
2024-11-09 12:35:38 -0500

    updated examples

--- thread_example.py
+++ thread_example.py
@@ -1,25 +1,38 @@
-import threading
-import os
-
-def task1():
-    print("Task 1 assigned to thread: {}".format(threading.current_thread().name))
-    print("ID of process running task 1: {}".format(os.getpid()))
-
-def task2():
-    print("Task 2 assigned to thread: {}".format(threading.current_thread().name))
-    print("ID of process running task 2: {}".format(os.getpid()))
-
-if __name__ == "__main__":
-
-    print("ID of process running main program: {}".format(os.getpid()))
-
-    print("Main thread name: {}".format(threading.current_thread().name))
-
-    t1 = threading.Thread(target=task1, name='t1')
-    t2 = threading.Thread(target=task2, name='t2')
-
-    t1.start()
-    t2.start()
-
-    t1.join()
-    t2.join()
+# program to display the functioning of 
+# settrace() 
+from sys import settrace 
+  
+  
+# local trace function which returns itself 
+def my_tracer(frame, event, arg = None): 
+    # extracts frame code 
+    code = frame.f_code 
+  
+    # extracts calling function name 
+    func_name = code.co_name 
+  
+    # extracts the line number 
+    line_no = frame.f_lineno 
+  
+    print(f"A {event} encountered in {func_name}() at line number {line_no} ") 
+  
+    return my_tracer 
+  
+  
+# global trace function is invoked here and 
+# local trace function is set for fun() 
+def fun(): 
+    return "GFG"
+  
+  
+# global trace function is invoked here and 
+# local trace function is set for check() 
+def check(): 
+    return fun() 
+  
+  
+# returns reference to local 
+# trace function (my_tracer) 
+settrace(my_tracer) 
+  
+check() 
