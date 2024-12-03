2a800e105fde26e074aaad03c43d390849aca299
Thakee Nathees <thakeenathees@gmail.com>
2024-12-03 17:47:28 +0530

    jac2py codegen refactored

--- jac/examples/reference/data_spatial_walker_statements.py
+++ jac/examples/reference/data_spatial_walker_statements.py
@@ -2,8 +2,17 @@ from __future__ import annotations
 from jaclang.plugin.feature import JacFeature as _Jac
 
 
+# Since the Animal class cannot be inherit from object, (cause the base class will be changed at run time)
+# we need a base class.
+#
+# reference: https://stackoverflow.com/a/9639512/10846399
+#
+class Base:
+    pass
+
+
 @_Jac.make_walker(on_entry=[_Jac.DSFunc("self_destruct", None)], on_exit=[])
-class Visitor:
+class Visitor(Base):
     def self_destruct(self, _jac_here_) -> None:
         print("get's here")
         _Jac.disengage(self)
