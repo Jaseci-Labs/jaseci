59a135cdf2834a9eed52ccdd53d837c379c01b46
Johnson He <hejohns@umich.edu>
2024-12-04 03:28:24 -0500

    really stupid and hacked way to "pass" program input to gins
    I lost track of what I should've been doing and did a bunch of random
    stuff that isn't very useful
    
    TODO: there's a weird bug where adding newlines around the PROGRAM_INPUT
    assignment can either cause
      - total_operational_time to not be initialized
      - the print("tracer: PROGRAM_INPUT...") line in the tracer to not print
    right now, it seems to "work" by reinitializing total_operational_time
    again...
    
    Presumably this has something to do with how we skip lines
    (frame.f_lineno += 1) in the tracer to avoid evaluating a (potentially
    side-effectful) assignment twice.
    After all the trouble this caused, we should've just set a flag when we
    assign to PROGRAM_INPUT and grabbed the value on the next breakpoint.

--- jac/jaclang/runtimelib/gins/tracer.py
+++ jac/jaclang/runtimelib/gins/tracer.py
@@ -7,7 +7,12 @@ import threading
 import sys
 import copy
 import os
+import traceback
+import dis
 from collections import deque
+import inspect
+import warnings
+import ast
 
 
 class CfgDeque:
@@ -66,15 +71,14 @@ class CFGTracker:
     def trace_callback(
         self, frame: types.FrameType, event: str, arg: any
     ) -> Optional[Callable]:
-        if event == "call":
-            frame.f_trace_opcodes = True
-
         """Trace function to track executed branches"""
         code = frame.f_code
         if ".jac" not in code.co_filename:
             return self.trace_callback
 
-        if event == "opcode":
+        if event == "call":
+            frame.f_trace_opcodes = True
+        elif event == "opcode":
             # edge case to handle executing code not within a function
             filename = os.path.basename(code.co_filename)
             module = (
@@ -95,5 +99,57 @@ class CFGTracker:
                     variable_dict[var_name] = frame.f_locals[var_name]
                 self.curr_variables[module] = (frame.f_lasti, variable_dict)
                 self.curr_variables_lock.release()
-
+        elif event == "line":
+            ###
+            # this is really circumlocutious, but is also how
+            # [watchpoints](https://github.com/gaogaotiantian/watchpoints/tree/master)
+            # works
+            ###
+            try:
+                #print(inspect.getsourcefile(frame.f_code))
+                #print(frame.f_lineno)
+                # TODO: super inefficient but just for now
+                # inspect.getsource doesn't seem to work like it does for
+                # regular python (see test_tracer.py)
+                # NOTE: we're parsing jac lines as python, fingers crossed
+                with open(inspect.getsourcefile(frame.f_code)) as file:
+                    l = [line.lstrip() for line in file][frame.f_lineno]
+                #print("                       ", l)
+                l.rstrip(';')
+                a = ast.parse(l)
+                line_ast = a.body[0]
+                #print("                       ", ast.unparse(line_ast))
+            except (IndexError, SyntaxError):
+                return self.trace_callback
+            #print(ast.dump(a))
+            #print(len(a.body))
+            assert len(a.body) == 1 # we only parsed one line
+            if isinstance(line_ast, ast.Assign) or isinstance(line_ast, ast.AugAssign):
+                # yes, I know this isn't strictly necessary in python
+                lhs_ast = None
+                rhs_ast = None
+                if isinstance(line_ast, ast.Assign):
+                    assert len(line_ast.targets) == 1, "Only handling single targets right now"
+                    lhs_ast = line_ast.targets[0]
+                elif isinstance(line_ast, ast.AugAssign):
+                    lhs_ast = line_ast.target
+                lhs_var = lhs_ast.id
+                rhs_ast = line_ast.value
+                # NOTE: for some reason, eval(ast.Expression(rhs_ast)) doesn't work
+                rhs_value = eval(ast.unparse(rhs_ast), frame.f_globals, frame.f_locals)
+                if isinstance(line_ast, ast.Assign):
+                    exec(f"{lhs_var} = {rhs_value}\n", frame.f_globals, frame.f_locals)
+                    print(f"{lhs_var} = {rhs_value}")
+                elif isinstance(line_ast, ast.AugAssign):
+                    exec(f"{lhs_var} += {rhs_value}\n", frame.f_globals, frame.f_locals)
+                    print(f"{lhs_var} (+)= {rhs_value}")
+                #print(frame.f_locals)
+                if lhs_var == 'PROGRAM_INPUT':
+                    if isinstance(line_ast, ast.AugAssign):
+                        assert False, "Unimplemented"
+                    print("tracer: PROGRAM_INPUT = ", rhs_value)
+                # this only silences some of the
+                # "RuntimeWarning: assigning None to unbound local" warnings
+                with warnings.catch_warnings(action="ignore"):
+                    frame.f_lineno += 1
         return self.trace_callback
