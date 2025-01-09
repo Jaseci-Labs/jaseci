2ac7f74372dc6ab63e249d33a7fb3dc4be750461
Ashish Mahendra <ashishmahendra@live.com>
2025-01-09 15:32:36 +0000

    Fix error handling in JacProgram compilation process

--- jac/jaclang/runtimelib/machine.py
+++ jac/jaclang/runtimelib/machine.py
@@ -307,12 +307,11 @@ class JacProgram:
                 return marshal.load(f)
 
         result = compile_jac(full_target, cache_result=cachable)
-        if result.errors_had and not result.ir.gen.py_bytecode:
+        if result.errors_had:
             for alrt in result.errors_had:
                 # We're not logging here, it already gets logged as the errors were added to the errors_had list.
                 # Regardless of the logging, this needs to be sent to the end user, so we'll printing it to stderr.
                 logger.error(alrt.pretty_print())
-            return None
         if result.ir.gen.py_bytecode is not None:
             return marshal.loads(result.ir.gen.py_bytecode)
         else:
