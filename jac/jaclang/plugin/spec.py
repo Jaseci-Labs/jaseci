1d6d227dafdaf7976fe80c6a7635afdf848276e4
MalithaPrabhashana <malithaprabhashana@gmail.com>
2025-02-06 17:50:54 +0530

    Revert "edges_only bool fixed"
    This reverts commit a9dc14bb3148e6383dc0d37201b5d1c429689a6a.

--- jac/jaclang/plugin/spec.py
+++ jac/jaclang/plugin/spec.py
@@ -374,7 +374,7 @@ class JacFeatureSpec(
         target_obj: Optional[NodeArchitype | list[NodeArchitype]],
         dir: EdgeDir,
         filter_func: Optional[Callable[[list[EdgeArchitype]], list[EdgeArchitype]]],
-        edges_only: bool,
+        edges_only: Optional[bool],
     ) -> list[NodeArchitype] | list[EdgeArchitype]:
         """Jac's apply_dir stmt feature."""
         raise NotImplementedError
