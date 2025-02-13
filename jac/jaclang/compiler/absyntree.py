a02392ce4e95aa338274492405983d8116e64ca9
kugesan1105 <kugesan.sivasothynathan@jaseci.org>
2025-02-13 17:38:39 +0530

    intial commit

--- jac/jaclang/compiler/absyntree.py
+++ jac/jaclang/compiler/absyntree.py
@@ -756,7 +756,7 @@ class Test(AstSymbolNode, ElementStmt):
     def __init__(
         self,
         name: Name | Token,
-        body: SubNodeList[CodeBlockStmt],
+        body: list[CodeBlockStmt],
         kid: Sequence[AstNode],
         doc: Optional[String] = None,
     ) -> None:
@@ -800,14 +800,17 @@ class Test(AstSymbolNode, ElementStmt):
         res = True
         if deep:
             res = self.name.normalize(deep)
-            res = res and self.body.normalize(deep)
+            for stmt in self.body:
+                res = res and stmt.normalize(deep)
             res = res and self.doc.normalize(deep) if self.doc else res
         new_kid: list[AstNode] = []
         if self.doc:
             new_kid.append(self.doc)
         new_kid.append(self.gen_token(Tok.KW_TEST))
         new_kid.append(self.name)
-        new_kid.append(self.body)
+        new_kid.append(self.gen_token(Tok.LBRACE))
+        new_kid.extend(self.body)
+        new_kid.append(self.gen_token(Tok.RBRACE))
         self.set_kids(nodes=new_kid)
         return res
 
