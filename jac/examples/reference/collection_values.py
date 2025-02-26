dbf344b20ee867d77a7fd559a17bc341e8c2445e
MalithaPrabhashana <malithaprabhashana@gmail.com>
2025-02-26 15:35:39 +0530

    pre-commits checked

--- jac/examples/reference/collection_values.py
+++ jac/examples/reference/collection_values.py
@@ -1,7 +1,7 @@
-squares = {num: num ** 2 for num in range(1, 6)}
-even_squares_set = {num ** 2 for num in range(1, 11) if num % 2 == 0}
-squares_generator = (num ** 2 for num in range(1, 6))
-squares_list = [num ** 2 for num in squares_generator if num != 9]
+squares = {num: num**2 for num in range(1, 6)}
+even_squares_set = {num**2 for num in range(1, 11) if num % 2 == 0}
+squares_generator = (num**2 for num in range(1, 6))
+squares_list = [num**2 for num in squares_generator if num != 9]
 
 print("\n".join([str(squares), str(even_squares_set), str(squares_list)]))
 
