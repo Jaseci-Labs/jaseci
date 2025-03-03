6ac2c7830aa94621dac5b7669effb8a2a7b2edc1
mgtm98 <m.g.t.m.prog@gmail.com>
2025-03-03 20:35:16 +0200

    Calling ImportPass from compile only and add the imported modules to the current JacProgram

--- jac/jaclang/compiler/passes/main/import_pass.py
+++ jac/jaclang/compiler/passes/main/import_pass.py
@@ -37,14 +37,11 @@ class JacImportPass(Pass):
         self.import_table[node.loc.mod_path] = node
         self.annex_impl(node)
         self.terminate()  # Turns off auto traversal for deliberate traversal
-        self.run_again = True
-        while self.run_again:
-            self.run_again = False
-            all_imports = self.get_all_sub_nodes(node, ast.ModulePath)
-            for i in all_imports:
-                self.process_import(i)
-                self.enter_module_path(i)
-            SubNodeTabPass(prior=self, input_ir=node)
+        all_imports = self.get_all_sub_nodes(node, ast.ModulePath)
+        for i in all_imports:
+            self.process_import(i)
+            self.enter_module_path(i)
+        SubNodeTabPass(prior=self, input_ir=node)
 
         node.mod_deps.update(self.import_table)
 
@@ -58,8 +55,11 @@ class JacImportPass(Pass):
         self, node: ast.ModulePath | ast.ModuleItem, mod: ast.Module | None
     ) -> None:
         """Attach a module to a node."""
+        from jaclang.runtimelib.machine import JacMachine
+
         if mod:
-            self.run_again = True
+            JacMachine.get().jac_program.modules[mod.loc.mod_path] = mod
+            JacMachine.get().jac_program.last_imported.append(mod)
             node.sub_module = mod
             self.annex_impl(mod)
             node.add_kids_right([mod], pos_update=False)
@@ -173,7 +173,6 @@ class JacImportPass(Pass):
     def import_jac_mod_from_file(self, target: str) -> ast.Module | None:
         """Import a module from a file."""
         from jaclang.compiler.compile import jac_file_to_pass
-        from jaclang.compiler.passes.main import SubNodeTabPass
 
         if not os.path.exists(target):
             self.error(f"Could not find module {target}")
@@ -181,7 +180,7 @@ class JacImportPass(Pass):
         if target in self.import_table:
             return self.import_table[target]
         try:
-            mod_pass = jac_file_to_pass(file_path=target, target=SubNodeTabPass)
+            mod_pass = jac_file_to_pass(file_path=target, schedule=[])
             self.errors_had += mod_pass.errors_had
             self.warnings_had += mod_pass.warnings_had
             mod = mod_pass.ir
@@ -200,6 +199,23 @@ class JacImportPass(Pass):
 
 class PyImportPass(JacImportPass):
     """Jac statically imports Python modules."""
+
+    def enter_module(self, node: ast.Module) -> None:
+        """Run Importer."""
+        self.cur_node = node
+        self.import_table[node.loc.mod_path] = node
+        self.annex_impl(node)
+        self.terminate()  # Turns off auto traversal for deliberate traversal
+        self.run_again = True
+        while self.run_again:
+            self.run_again = False
+            all_imports = self.get_all_sub_nodes(node, ast.ModulePath)
+            for i in all_imports:
+                self.process_import(i)
+                self.enter_module_path(i)
+            SubNodeTabPass(prior=self, input_ir=node)
+
+        node.mod_deps.update(self.import_table)
 
     def __debug_print(self, msg: str) -> None:
         if settings.py_import_pass_debug:
