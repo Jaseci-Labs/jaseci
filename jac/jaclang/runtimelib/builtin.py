b939925d23429294bdfef7db3ec64cabaa7c38ef
Thakee Nathees <thakeenathees@gmail.com>
2025-03-06 15:41:11 +0530

    A builtin function added to get graph as json

--- jac/jaclang/runtimelib/builtin.py
+++ jac/jaclang/runtimelib/builtin.py
@@ -2,8 +2,11 @@
 
 from __future__ import annotations
 
+import json
 from abc import abstractmethod
 from typing import ClassVar, Optional, override
+
+from sympy import false
 
 from jaclang.runtimelib.constructs import Architype, NodeArchitype
 from jaclang.runtimelib.machine import JacMachine as Jac
@@ -11,24 +14,23 @@ from jaclang.runtimelib.machine import JacMachine as Jac
 
 def dotgen(
     node: Optional[NodeArchitype] = None,
-    depth: Optional[int] = None,
-    traverse: Optional[bool] = None,
+    depth: int = -1,
+    traverse: bool = False,
     edge_type: Optional[list[str]] = None,
-    bfs: Optional[bool] = None,
-    edge_limit: Optional[int] = None,
-    node_limit: Optional[int] = None,
+    bfs: bool = True,
+    edge_limit: int = 512,
+    node_limit: int = 512,
     dot_file: Optional[str] = None,
+    as_json: bool = False,
 ) -> str:
     """Print the dot graph."""
     from jaclang.runtimelib.machine import JacMachine as Jac
 
     root = Jac.root()
-    node = node if node is not None else root
-    depth = depth if depth is not None else -1
-    traverse = traverse if traverse is not None else False
-    bfs = bfs if bfs is not None else True
-    edge_limit = edge_limit if edge_limit is not None else 512
-    node_limit = node_limit if node_limit is not None else 512
+    node = node or root
+
+    if as_json:
+        return(_jac_graph_json())
 
     return Jac.dotgen(
         edge_type=edge_type,
@@ -41,7 +43,6 @@ def dotgen(
         dot_file=dot_file,
     )
 
-
 def jid(obj: Architype) -> str:
     """Get the id of the object."""
     return Jac.object_ref(obj)
@@ -51,6 +52,38 @@ def jobj(id: str) -> Architype | None:
     """Get the object from the id."""
     return Jac.get_object(id)
 
+
+def _jac_graph_json() -> str:
+    """Get the graph in json string."""
+    import jaclang as jl
+    from jaclang import Root, Node
+
+    processed: list[Root | Node] = []
+    nodes: list[dict] = []
+    edges: list[dict] = []
+    working_set: list[tuple] = []
+
+    root = Jac.root()
+    nodes.append({"id": id(root), "label": "root"})
+
+    processed.append(root)
+    working_set = [(root, ref) for ref in root.refs()]
+
+    while working_set:
+        start, end = working_set.pop(0)
+        edges.append({"from": id(start), "to": id(end)})
+        nodes.append({"id": id(end), "label": repr(end)})
+        processed.append(end)
+        for ref in end.refs():
+            if ref not in processed:
+                working_set.append((end, ref))
+    return json.dumps(
+        {
+            "version": "1.0",
+            "nodes": nodes,
+            "edges": edges,
+        }
+    )
 
 __all__ = [
     "abstractmethod",
