7533ebf5d2a0dbe742ad1abf9d845b6fdf51490f
Yiping Kang <yipingkang93@gmail.com>
2025-03-12 11:08:23 -0400

    Tagging up jaclang 0.7.33 and jac-cloud 0.1.22

--- jac-cloud/setup.py
+++ jac-cloud/setup.py
@@ -2,14 +2,14 @@
 
 from setuptools import find_packages, setup
 
-VERSION = "0.1.21"
+VERSION = "0.1.22"
 
 setup(
     name="jac-cloud",
     version=VERSION,
     packages=find_packages(include=["jac_cloud", "jac_cloud.*"]),
     install_requires=[
-        "jaclang==0.7.32",
+        "jaclang==0.7.33",
         "fastapi~=0.111.0",
         "pydantic~=2.8.2",
         "pymongo~=4.8.0",
