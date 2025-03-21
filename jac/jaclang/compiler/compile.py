5c737c1fdb4e966a0f3deaa20114f619b6b48c9c
Mohamed Gamal <m.g.t.m.prog@gmail.com>
2025-03-21 13:42:22 +0200

    Run jac run passes on modules one by one (#1656)

--- jac/jaclang/compiler/compile.py
+++ jac/jaclang/compiler/compile.py
@@ -17,6 +17,8 @@ from jaclang.compiler.passes.tool.schedules import format_pass
 
 def compile_jac(file_path: str, cache_result: bool = False) -> Pass:
     """Start Compile for Jac file and return python code as string."""
+    from jaclang.runtimelib.machine import JacMachine
+
     code = jac_file_to_pass(
         file_path=file_path,
         schedule=pass_schedule,
@@ -25,8 +27,8 @@ def compile_jac(file_path: str, cache_result: bool = False) -> Pass:
     # no more passes were processed, in that case we can ignore it.
     had_syntax_error = isinstance(code, JacParser) and len(code.errors_had) != 0
     if cache_result and (not had_syntax_error) and isinstance(code.ir, ast.Module):
-        print_pass = PyOutPass(input_ir=code.ir, prior=code)
-        return print_pass
+        for _, module in JacMachine.get().jac_program.modules.items():
+            PyOutPass(input_ir=module, prior=code)
     return code
 
 
@@ -65,6 +67,7 @@ def jac_str_to_pass(
         return ast_ret
 
     machine = JacMachine.get()
+    top_mod = ast_ret.ir
     assert isinstance(ast_ret.ir, ast.Module)
     machine.jac_program.last_imported.append(ast_ret.ir)
     machine.jac_program.modules[ast_ret.ir.loc.mod_path] = ast_ret.ir
@@ -77,11 +80,18 @@ def jac_str_to_pass(
     if len(ast_ret.errors_had) != 0:
         return ast_ret
 
-    for i in schedule:
-        if i == target:
-            break
-        ast_ret = i(input_ir=ast_ret.ir, prior=ast_ret)
-    ast_ret = target(input_ir=ast_ret.ir, prior=ast_ret) if target else ast_ret
+    def run_schedule(mod: ast.Module) -> None:
+        nonlocal ast_ret
+        for i in schedule:
+            if i == target:
+                break
+            ast_ret = i(mod, prior=ast_ret)
+        ast_ret = target(mod, prior=ast_ret) if target else ast_ret
+
+    for mod in machine.jac_program.modules.values():
+        run_schedule(mod)
+
+    ast_ret.ir = top_mod
     return ast_ret
 
 
