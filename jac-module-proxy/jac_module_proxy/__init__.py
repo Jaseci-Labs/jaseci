96bcfe96063bc487ee663e462a4033a843040f41
Alexie (Boyong) Madolid <maex@tuta.io>
2025-03-26 02:09:23 +0800

    [BUGFIX]: Adjust dashed args handler and merge jac-module-proxy

--- jac-module-proxy/jac_module_proxy/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Jac Module Proxy Utilities."""
-
-from logging import StreamHandler, getLogger
-from os import getenv
-from sys import stdout
-
-logger = getLogger(__name__)
-logger.setLevel(getenv("LOGGER_LEVEL", "DEBUG"))
-logger.addHandler(StreamHandler(stdout))
