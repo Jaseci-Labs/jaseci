60e0289ebcd75b9c4f9a099217720d0e0d544c75
marsninja <jason@mars.ninja>
2025-04-20 19:08:20 -0400

    clean: dead code

--- jac/jaclang/runtimelib/context.py
+++ jac/jaclang/runtimelib/context.py
@@ -2,10 +2,9 @@
 
 from __future__ import annotations
 
-import unittest
 from contextvars import ContextVar
 from dataclasses import MISSING
-from typing import Any, Callable, Optional, cast
+from typing import Any, Optional, cast
 from uuid import UUID
 
 from .architype import NodeAnchor, Root
@@ -103,87 +102,3 @@ class ExecutionContext:
             sr_anch.persistent = False
             ExecutionContext.system_root = sr_anch
         return sr_anch
-
-
-class JacTestResult(unittest.TextTestResult):
-    """Jac test result class."""
-
-    def __init__(
-        self,
-        stream,  # noqa
-        descriptions,  # noqa
-        verbosity: int,
-        max_failures: Optional[int] = None,
-    ) -> None:
-        """Initialize FailFastTestResult object."""
-        super().__init__(stream, descriptions, verbosity)  # noqa
-        self.failures_count = JacTestCheck.failcount
-        self.max_failures = max_failures
-
-    def addFailure(self, test, err) -> None:  # noqa
-        """Count failures and stop."""
-        super().addFailure(test, err)
-        self.failures_count += 1
-        if self.max_failures is not None and self.failures_count >= self.max_failures:
-            self.stop()
-
-    def stop(self) -> None:
-        """Stop the test execution."""
-        self.shouldStop = True
-
-
-class JacTextTestRunner(unittest.TextTestRunner):
-    """Jac test runner class."""
-
-    def __init__(self, max_failures: Optional[int] = None, **kwargs) -> None:  # noqa
-        """Initialize JacTextTestRunner object."""
-        self.max_failures = max_failures
-        super().__init__(**kwargs)
-
-    def _makeResult(self) -> JacTestResult:  # noqa
-        """Override the method to return an instance of JacTestResult."""
-        return JacTestResult(
-            self.stream,
-            self.descriptions,
-            self.verbosity,
-            max_failures=self.max_failures,
-        )
-
-
-class JacTestCheck:
-    """Jac Testing and Checking."""
-
-    test_case = unittest.TestCase()
-    test_suite = unittest.TestSuite()
-    breaker = False
-    failcount = 0
-
-    @staticmethod
-    def reset() -> None:
-        """Clear the test suite."""
-        JacTestCheck.test_case = unittest.TestCase()
-        JacTestCheck.test_suite = unittest.TestSuite()
-
-    @staticmethod
-    def run_test(xit: bool, maxfail: int | None, verbose: bool) -> None:
-        """Run the test suite."""
-        verb = 2 if verbose else 1
-        runner = JacTextTestRunner(max_failures=maxfail, failfast=xit, verbosity=verb)
-        result = runner.run(JacTestCheck.test_suite)
-        if result.wasSuccessful():
-            print("Passed successfully.")
-        else:
-            fails = len(result.failures)
-            JacTestCheck.failcount += fails
-            JacTestCheck.breaker = (
-                (JacTestCheck.failcount >= maxfail) if maxfail else True
-            )
-
-    @staticmethod
-    def add_test(test_fun: Callable) -> None:
-        """Create a new test."""
-        JacTestCheck.test_suite.addTest(unittest.FunctionTestCase(test_fun))
-
-    def __getattr__(self, name: str) -> object:
-        """Make convenient check.Equal(...) etc."""
-        return getattr(JacTestCheck.test_case, name)
