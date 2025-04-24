1cce8e066cde13927ba9540de0c5a6350d1f3e77
Chathura-Jayasinghe <jayasinghejmcnb.21@uom.lk>
2025-04-24 18:05:28 +0530

    .

--- jac/jaclang/tests/fixtures/py2jac.py
+++ jac/jaclang/tests/fixtures/py2jac.py
@@ -20,11 +20,11 @@ a = 1
 b = 2
 
 match Container(inner=Inner(x=a, y=b)):
-    case Container(inner=Inner(x=val_x, y=0)):
-        print(f"1.Inner.x={val_x}, Inner.y=0")
-    case Container(inner=Inner(x=0, y=val_y)):
-        print(f"2.Inner.x=0, Inner.y={val_y}")
-    case Container(inner=Inner(x=val_x, y=val_y)):
-        print(f"4.Inner.x={val_x}, Inner.y={val_y}")
+    case Container(inner=Inner(x=a, y=0)):
+        print(f"1.Inner.x={a}, Inner.y=0")
+    case Container(inner=Inner(x=0, y=b)):
+        print(f"2.Inner.x=0, Inner.y={b}")
+    case Container(inner=Inner(x=a, y=b)):
+        print(f"4.Inner.x={a}, Inner.y={b}")
     case _:
         print("5.No match")
