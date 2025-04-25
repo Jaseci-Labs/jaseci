316340b05842e5710e6696c1dae8f1a90a14a648
MalithaPrabhashana <malithaprabhashana@gmail.com>
2025-04-25 15:56:05 +0530

    update mkdocs pre-build hook

---
+++ jac/support/jac-lang.org/handle_jac_compile_data.py
@@ -0,0 +1,30 @@
+import os
+import zipfile
+
+TARGET_FOLDER = "../../jaclang"
+EXTRACTED_FOLDER = "docs/playground"
+FINAL_ZIP_PATH = os.path.join(EXTRACTED_FOLDER, "jaclang.zip")
+ZIP_FOLDER_NAME = "jaclang"
+
+
+def pre_build_hook(**kwargs: any) -> None:
+    print("Running pre-build hook...")
+    create_final_zip()
+
+
+def create_final_zip() -> None:
+    print("Creating final zip...")
+
+    if not os.path.exists(TARGET_FOLDER):
+        raise FileNotFoundError(f"Folder not found: {TARGET_FOLDER}")
+
+    with zipfile.ZipFile(FINAL_ZIP_PATH, "w", zipfile.ZIP_DEFLATED) as zipf:
+        for root, _, files in os.walk(TARGET_FOLDER):
+            for file in files:
+                file_path = os.path.join(root, file)
+                arcname = os.path.join(
+                    ZIP_FOLDER_NAME, os.path.relpath(file_path, TARGET_FOLDER)
+                )
+                zipf.write(file_path, arcname)
+
+    print("Zip saved to:", FINAL_ZIP_PATH)
