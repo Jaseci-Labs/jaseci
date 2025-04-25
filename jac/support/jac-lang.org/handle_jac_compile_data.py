e007d5ae29f9108d123ee11183848b979a3a9861
MalithaPrabhashana <malithaprabhashana@gmail.com>
2025-04-25 16:28:19 +0530

    enhance docstrings for clarity and detail in jac compile data handling

--- jac/support/jac-lang.org/handle_jac_compile_data.py
+++ jac/support/jac-lang.org/handle_jac_compile_data.py
@@ -1,4 +1,6 @@
-"""Handle jac compile data for jaclang.org."""
+"""Handle jac compile data for jaclang.org.
+This script is used to handle the jac compile data for jac playground.
+"""
 
 import os
 import zipfile
@@ -10,13 +12,17 @@ ZIP_FOLDER_NAME = "jaclang"
 
 
 def pre_build_hook(**kwargs: any) -> None:
-    """This function is called before the build process starts."""
+    """Run pre-build tasks for preparing files.
+    This function is called before the build process starts.
+    """
     print("Running pre-build hook...")
     create_final_zip()
 
 
 def create_final_zip() -> None:
-    """Create a zip file containing the jaclang folder."""
+    """Create a zip file containing the jaclang folder.
+    The zip file is created in the EXTRACTED_FOLDER directory.
+    """
     print("Creating final zip...")
 
     if not os.path.exists(TARGET_FOLDER):
