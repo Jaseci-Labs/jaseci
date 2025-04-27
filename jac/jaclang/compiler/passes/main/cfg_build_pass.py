d4c278fa404c48de0c9af17816c0bd4bf0dc7a61
jayanaka-98 <jayanaka@umich.edu>
2025-04-27 12:38:58 -0400

    CFG linking done for if statements | need to handle loops and functions

--- jac/jaclang/compiler/passes/main/cfg_build_pass.py
+++ jac/jaclang/compiler/passes/main/cfg_build_pass.py
@@ -17,26 +17,25 @@ class CFGBuildPass(AstPass):
         """Before pass."""
         pass
 
-    # def enter_node(self, node: uni.UniNode) -> None:
-    #     """Enter node."""
-    #     if node.parent:
-    #         if isinstance(node.parent, uni.ModuleCode):
-    #             for bb in node.sub
-    #             print(type(node).__name__)
-
-    def get_parent_bb(self, node: uni.UniNode) -> uni.UniBasicBlock:
+    def get_parent_bb(self, node: uni.UniNode) -> uni.UniBasicBlock | None:
         """Get parent basic block."""
-        parent_bb = node.parent_of_type(uni.UniBasicBlock)
-        if not parent_bb:
-            raise ValueError("Parent basic block not found")
-        return parent_bb
+        if not isinstance(node, uni.Module):
+            parent_bb = node.parent_of_type(uni.UniBasicBlock)
+            return parent_bb
+        else:
+            return None
 
-    def enter_assignment(self, node: uni.Assignment) -> None:
-        """Enter module."""
-        parent_bb = self.get_parent_bb(node)
-        parent_bb.bb_stmts.append(node)
-
-    def enter_if_stmt(self, node: uni.IfStmt) -> None:
-        """Enter module."""
-        # print(f"If statement in basic block {node.condition}")
-        pass
+    def enter_node(self, node: uni.UniNode) -> None:
+        """Enter UniBasicBlock nodes."""
+        if isinstance(node, uni.UniBasicBlock):
+            parent_bb = self.get_parent_bb(node)
+            if isinstance(parent_bb, uni.UniBasicBlock):
+                if isinstance(parent_bb, uni.IfStmt):
+                    node.bb_stmts.append(node)
+                    if node.control_in_bbs:
+                        node.control_in_bbs.append(parent_bb)
+                    if parent_bb.control_out_bbs:
+                        parent_bb.control_out_bbs.append(parent_bb)
+                else:
+                    parent_bb.bb_stmts.append(node)
+                    node.bb_stmts = parent_bb.bb_stmts
