63c1076b7446770ab8d031ee4a97689c4e4fbaf4
marsninja <jason@mars.ninja>
2025-05-09 21:21:54 -0400

    highlighter better

--- scripts/build_vsce.sh
+++ scripts/build_vsce.sh
@@ -1,6 +1,7 @@
-cd ~/jaseci/jac/support/vscode_ext/jac
+cd jac/support/vscode_ext/jac
 npm install
 npm install -g @vscode/vsce
 vsce package
-code --install-extension jaclang-*.vsix
-# cursor --install-extension jaclang-*.vsix
+# code --install-extension jaclang-*.vsix
+cursor --install-extension jaclang-*.vsix
+cd -
