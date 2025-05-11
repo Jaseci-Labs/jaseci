513328f2cb1ce8b74764140339aae73299bfeb5f
Gorgeous Patrick <baichuanli@yahoo.com>
2025-05-11 18:20:14 -0400

    feat: node type and edge type restriction considered

--- jac/jaclang/runtimelib/data_mapper.py
+++ jac/jaclang/runtimelib/data_mapper.py
@@ -16,78 +16,73 @@ class VisitStmtInfo:
     Helps provide some more insights into the visit pattern
     """
 
-    edge_type: uni.EdgeOpRef
+    edge_type: str | None
     async_visit: bool
 
 
-class VisitPredictionPass:
-    """Visit statement detection.
+def _get_from_node_type_of_visit(visit_stmt: uni.VisitStmt) -> str:
+    """Get the node type that the visit is from.
 
-    Helps provide some static information about the visit statement.
+    For example, if a visit is in an ability:
+        can xxx with XXX entry {...}, it will return XXX
     """
+    ability = visit_stmt.parent_of_type(uni.Ability)
+    return (
+        ability.get_all_sub_nodes(uni.EventSignature)[0]
+        .get_all_sub_nodes(uni.Name)[0]
+        .value
+    )
 
-    def _get_edge_list(self, node: uni.VisitStmt) -> uni.EdgeOpRef:
-        op_refs = node.get_all_sub_nodes(uni.EdgeOpRef)
-        return op_refs[0]
 
-    def _get_function(self, node: uni.VisitStmt) -> uni.ArchBlockStmt:
-        func = node.find_parent_of_type(uni.ArchBlockStmt)
-        if func is None:
-            raise RuntimeError()
-        return func
-
-    def _get_walker(self, node: uni.VisitStmt) -> uni.Architype:
-        walker = node.find_parent_of_type(uni.Architype)
-        if walker is None:
-            raise RuntimeError()
-        return walker
-
-    def _get_edge_type_name(self, node: uni.EdgeOpRef) -> str:
-        edge_type_name = ""
-        type_filters = node.get_all_sub_nodes(uni.FilterCompr)
-        if len(type_filters) > 0:
-            type_filter = type_filters[0]
-            name_node = type_filter.get_all_sub_nodes(uni.Name)
-            edge_type_name = name_node[0].value
-        return edge_type_name
-
-    def _get_name(self, node: uni.UniNode) -> str | None:
-        for kid in node.kid:
-            if isinstance(kid, uni.Name):
-                return kid.value
+def _get_to_edge_type_of_visit(visit_stmt: uni.VisitStmt) -> str | None:
+    filters = visit_stmt.get_all_sub_nodes(uni.FilterCompr)
+    if len(filters) == 0:
         return None
+    return filters[0].get_all_sub_nodes(uni.Name)[0].value
 
-    def get_visit_stmt_info(self, node: uni.VisitStmt) -> VisitStmtInfo:
-        """Process a visit statement.
 
-        It will return some information potentially useful to runtime data mapping.
-        """
-        edge_op_ref = self._get_edge_list(node)
-        result = VisitStmtInfo(
-            edge_type=edge_op_ref,
-            async_visit=self._get_edge_type_name(edge_op_ref).startswith("async_"),
+def _get_visit_info(visit_stmts: list[uni.VisitStmt]) -> dict[str, list[VisitStmtInfo]]:
+    res: dict[str, list[VisitStmtInfo]] = {}
+    for visit_stmt in visit_stmts:
+        from_node = _get_from_node_type_of_visit(visit_stmt)
+        if from_node not in res.keys():
+            res[from_node] = []
+        edge_type = _get_to_edge_type_of_visit(visit_stmt)
+        async_visit = edge_type is not None and edge_type.startswith("async_")
+        res[from_node].append(
+            VisitStmtInfo(edge_type=edge_type, async_visit=async_visit)
         )
-        return result
+    return res
 
 
 def _get_next_steps(
-    current_node: NodeAnchor, visited: set[NodeAnchor]
+    current_node: NodeAnchor,
+    visited: set[NodeAnchor],
+    visit_stmt_info: dict[str, list[VisitStmtInfo]],
 ) -> list[list[NodeAnchor]]:
     next_nodes: list[list[NodeAnchor]] = []
     one_level: list[NodeAnchor] = []
 
-    for neighbor in current_node.edges:
-        if neighbor.target in visited:
-            continue
-        print(str(neighbor.architype))
-        # TODO: Change this async to a better representation
-        if str(neighbor.architype).startswith("async_"):
-            one_level.append(neighbor.target)
-        else:
-            if one_level:
-                next_nodes.append(one_level)
-                one_level = []
-            next_nodes.append([neighbor.target])
+    node_type_name = type(current_node.architype).__name__
+    possible_visits = visit_stmt_info.get(node_type_name, [])
+
+    for visit in possible_visits:
+        for neighbor in current_node.edges:
+            if (
+                visit.edge_type is not None
+                and str(neighbor.architype) != visit.edge_type + "()"
+            ):
+                continue
+            if neighbor.target in visited:
+                continue
+            # TODO: Change this async to a better representation
+            if str(neighbor.architype).startswith("async_"):
+                one_level.append(neighbor.target)
+            else:
+                if one_level:
+                    next_nodes.append(one_level)
+                    one_level = []
+                next_nodes.append([neighbor.target])
 
     if one_level:
         next_nodes.append(one_level)
@@ -95,16 +90,18 @@ def _get_next_steps(
     return next_nodes
 
 
-def _traversal(start: NodeAnchor) -> list[list[NodeAnchor]]:
+def _traversal(
+    start: NodeAnchor, visit_stmt_info: dict[str, list[VisitStmtInfo]]
+) -> list[list[NodeAnchor]]:
     queue: list[list[NodeAnchor]] = [[start]]
     traversal: list[list[NodeAnchor]] = []
     visited: set[NodeAnchor] = set()
-    print(type(start))
     while len(queue) > 0:
         node_level = queue.pop()
         traversal.append(node_level)
         next_nodes = [
-            _get_next_steps(current_node, visited) for current_node in node_level
+            _get_next_steps(current_node, visited, visit_stmt_info)
+            for current_node in node_level
         ]
         max_length = max([len(next_node) for next_node in next_nodes])
         for i in range(max_length):
@@ -128,9 +125,7 @@ def generate_data_mapping(
     visit_stmts: list[uni.VisitStmt], start: NodeAnchor
 ) -> dict[NodeAnchor, int]:
     """Generate data mapping."""
-    print(type(start.architype))
-
-    traversal_order = _traversal(start)
+    traversal_order = _traversal(start, _get_visit_info(visit_stmts))
     dpu_node_count = [0] * NUM_DPU
     node_assignments: dict[NodeAnchor, int] = {}
     for layer in traversal_order:
