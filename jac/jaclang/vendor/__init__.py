340458c9b33ac8e93ecbfafaa2b1179550b4b4dc
marsninja <jason@mars.ninja>
2025-05-12 01:55:24 -0400

    tweak

--- jac/jaclang/vendor/__init__.py
+++ jac/jaclang/vendor/__init__.py
@@ -3,10 +3,9 @@ import sys
 
 sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__))))
 
-from . import mypy
 from . import pygls
 from . import lark
 from . import lsprotocol
 from . import pluggy
 
-__all__ = ["mypy", "pygls", "lark", "pluggy", "lsprotocol"]
+__all__ = ["pygls", "lark", "pluggy", "lsprotocol"]
