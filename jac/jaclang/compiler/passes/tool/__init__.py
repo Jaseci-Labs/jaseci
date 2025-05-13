6629b6a4edbb7f8533e10a0854990c5d47baf68e
marsninja <jason@mars.ninja>
2025-05-13 15:24:14 -0400

    feat: new formatter

--- jac/jaclang/compiler/passes/tool/__init__.py
+++ jac/jaclang/compiler/passes/tool/__init__.py
@@ -2,8 +2,10 @@
 
 from .fuse_comments_pass import FuseCommentsPass  # noqa: I100
 from .jac_formatter_pass import JacFormatPass  # noqa: I100
+from .new_formatter_pass import NewJacFormatPass  # noqa: I100
 
 __all__ = [
     "FuseCommentsPass",
     "JacFormatPass",
+    "NewJacFormatPass",
 ]
