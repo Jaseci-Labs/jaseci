b0b20356580e20c3c7b861f2872010b919596ba4
MalithaPrabhashana <malithaprabhashana@gmail.com>
2025-05-25 07:58:51 +0530

    feat: add custom 404 error handling in MkDocs server

--- docs/scripts/mkdocs_serve.py
+++ docs/scripts/mkdocs_serve.py
@@ -25,6 +25,22 @@ class CustomHeaderHandler(http.server.SimpleHTTPRequestHandler):
         self.send_header("Cross-Origin-Embedder-Policy", "require-corp")
         super().end_headers()
 
+    def send_error(self, code, message=None, explain=None):
+        """Serve custom 404.html page for 404 errors."""
+        if code == 404:
+            try:
+                with open("404/index.html", "rb") as f:
+                    content = f.read()
+                self.send_response(404)
+                self.send_header("Content-Type", "text/html")
+                self.send_header("Content-Length", str(len(content)))
+                self.end_headers()
+                self.wfile.write(content)
+            except Exception as e:
+                super().send_error(code, message, explain)
+        else:
+            super().send_error(code, message, explain)
+
 
 class DebouncedRebuildHandler(FileSystemEventHandler):
     """File system event handler for debounced MkDocs site rebuilding."""
